4d37a8e31e28d940733888395eeba81d49cd11c9
keelim <kimh00335@gmail.com>
2019-08-18 20:30:24 +0900

    2019 08 18

--- app/src/main/java/com/keelim/cnubus/ui/Broot/BFragment.java
+++ app/src/main/java/com/keelim/cnubus/ui/Broot/BFragment.java
@@ -5,11 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import androidx.annotation.Nullable;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
 import com.keelim.cnubus.R;
 
 public class BFragment extends Fragment {
@@ -17,7 +19,7 @@ public class BFragment extends Fragment {
     private BViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(BViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
--- app/src/main/java/com/keelim/cnubus/ui/Croot/CFragment.java
+++ app/src/main/java/com/keelim/cnubus/ui/Croot/CFragment.java
@@ -5,11 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import androidx.annotation.Nullable;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
 import com.keelim.cnubus.R;
 
 public class CFragment extends Fragment {
@@ -17,7 +19,7 @@ public class CFragment extends Fragment {
     private CViewModel notificationsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(CViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);

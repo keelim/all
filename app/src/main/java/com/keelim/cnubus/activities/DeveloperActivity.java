ecaca3663ef05ac7bb2109ef80735a1faf04e2b2
keelim <kimh00335@gmail.com>
2019-08-18 23:31:46 +0900

    2019 08 18

---
+++ app/src/main/java/com/keelim/cnubus/activities/DeveloperActivity.java
@@ -0,0 +1,18 @@
+package com.keelim.cnubus.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.keelim.cnubus.R;
+import com.keelim.cnubus.utils.MaterialHelper;
+
+public class DeveloperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer);
+        MaterialHelper.with(this).init().loadAbout();
+    }
+}
--- app/src/main/java/com/keelim/cnubus/activities/MainActivity.java
+++ app/src/main/java/com/keelim/cnubus/activities/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_aroot, R.id.navigation_broot, R.id.navigation_croot)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);

db2ecaf36d6aed323231af9ab7079de85e366228
keelim <kimh00335@gmail.com>
2019-08-18 23:01:26 +0900

    2019 08 18

---
+++ app/src/main/java/com/keelim/cnubus/ui/Setting/SettingViewModel.java
@@ -0,0 +1,7 @@
+package com.keelim.cnubus.ui.Setting;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/keelim/cnubus/ui/Setting/setting.java
@@ -0,0 +1,41 @@
+package com.keelim.cnubus.ui.Setting;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.keelim.cnubus.R;
+
+public class setting extends Fragment {
+
+    private SettingViewModel mViewModel;
+
+    public static setting newInstance() {
+        return new setting();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mViewModel =
+                ViewModelProviders.of(this).get(SettingViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_croot, container, false);
+
+        return root;
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(SettingViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}

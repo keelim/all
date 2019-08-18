22549b301ff33f45f3f19f1c978c250738d445c1
keelim <kimh00335@gmail.com>
2019-08-19 06:55:24 +0900

    glide image

--- app/src/main/java/com/keelim/cnubus/activities/BRootActivity.java
+++ app/src/main/java/com/keelim/cnubus/activities/BRootActivity.java
@@ -1,16 +1,21 @@
 package com.keelim.cnubus.activities;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.bumptech.glide.Glide;
 import com.keelim.cnubus.R;
 
 public class BRootActivity extends AppCompatActivity {
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broot);
+        Glide.with(this).load("https://raw.githubusercontent.com/keelim/Keelim.github.io/master/assets/a.png").into(imageView);
+
     }
 }
--- app/src/main/java/com/keelim/cnubus/activities/CRootActivity.java
+++ app/src/main/java/com/keelim/cnubus/activities/CRootActivity.java
@@ -1,16 +1,20 @@
 package com.keelim.cnubus.activities;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.bumptech.glide.Glide;
 import com.keelim.cnubus.R;
 
 public class CRootActivity extends AppCompatActivity {
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_croot);
+        Glide.with(this).load("https://raw.githubusercontent.com/keelim/Keelim.github.io/master/assets/a.png").into(imageView);
     }
 }

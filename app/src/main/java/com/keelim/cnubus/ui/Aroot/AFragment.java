0c330a01d62418e76a6d85087036bce7931e37a2
keelim <kimh00335@gmail.com>
2019-08-28 00:00:02 +0900

    fragment setting

--- app/src/main/java/com/keelim/cnubus/ui/Aroot/AFragment.java
+++ app/src/main/java/com/keelim/cnubus/ui/Aroot/AFragment.java
@@ -4,24 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.keelim.cnubus.R;
 
 public class AFragment extends Fragment {
 
-    private AViewModel dashboardViewModel;
+    private ListView listView;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         AViewModel aViewModel = ViewModelProviders.of(this).get(AViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_aroot, container, false);
+        listView = root.findViewById(R.id.a_listview);
+
+        ArrayAdapter<CharSequence> arrayAdapterA = ArrayAdapter.createFromResource(getActivity(), R.array.aList,
+                android.R.layout.simple_list_item_1);
+
+        listView.setAdapter(arrayAdapterA);
+
 
         return root;
     }

9ad1132654ebef064584f67e4d3f8a479dad19cf
keelim <kimh00335@gmail.com>
2019-08-29 15:20:17 +0900

    time table setting

--- app/src/main/java/com/keelim/cnubus/activities/CRootActivity.java
+++ app/src/main/java/com/keelim/cnubus/activities/CRootActivity.java
@@ -9,13 +9,12 @@ import com.bumptech.glide.Glide;
 import com.keelim.cnubus.R;
 
 public class CRootActivity extends AppCompatActivity {
-    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_croot);
-        imageView = findViewById(R.id.iv_croot);
-        Glide.with(this).load("https://raw.githubusercontent.com/keelim/Keelim.github.io/master/assets/c.png").into(imageView);
+
     }
 }

2ca39a25b5b9a9284a2dbf678b49693681c666f3
keelim <kimh00335@gmail.com>
2019-08-29 15:11:53 +0900

    change night root

---
+++ app/src/main/java/com/keelim/cnubus/activities/NightRootActivity.java
@@ -0,0 +1,18 @@
+package com.keelim.cnubus.activities;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.keelim.cnubus.R;
+
+public class NightRootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_night_root);
+    }
+}

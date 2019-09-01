8aae1557724e8d2f3b64655a9ae88c14a79f5ca9
keelim <kimh00335@gmail.com>
2019-09-01 14:16:28 +0900

    2019 09 01

--- app/src/main/java/com/keelim/cnubus/activities/BRootActivity.java
+++ app/src/main/java/com/keelim/cnubus/activities/BRootActivity.java
@@ -9,13 +9,12 @@ import com.bumptech.glide.Glide;
 import com.keelim.cnubus.R;
 
 public class BRootActivity extends AppCompatActivity {
-    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broot);
-        imageView = findViewById(R.id.iv_broot);
+        ImageView imageView = findViewById(R.id.iv_broot);
         Glide.with(this).load("https://raw.githubusercontent.com/keelim/Keelim.github.io/master/assets/b.png").into(imageView);
 
     }

102d40d0f418edaa080c26c686bf50356622e9e1
keelim <kimh00335@gmail.com>
2019-09-07 13:45:54 +0900

    2019 09 07

--- app/src/main/java/com/keelim/cnubus/ui/Aroot/AFragment.java
+++ app/src/main/java/com/keelim/cnubus/ui/Aroot/AFragment.java
@@ -1,21 +1,30 @@
 package com.keelim.cnubus.ui.Aroot;
 
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.keelim.cnubus.R;
+
+import java.util.Date;
 
 public class AFragment extends Fragment {
 
-    private ListView listView;
+    private TextView currenTime;
+    private String basic_string;
+
+
+
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,14 +32,37 @@ public class AFragment extends Fragment {
         AViewModel aViewModel = ViewModelProviders.of(this).get(AViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_aroot, container, false);
-        listView = root.findViewById(R.id.a_listview);
+        ListView listView = root.findViewById(R.id.a_listview);
 
         ArrayAdapter<CharSequence> arrayAdapterA = ArrayAdapter.createFromResource(getActivity(), R.array.aList,
                 android.R.layout.simple_list_item_1);
 
         listView.setAdapter(arrayAdapterA);
 
+        currenTime = root.findViewById(R.id.current_time_a);
+        basic_string = currenTime.getText().toString();
+        FloatingActionButton floatingActionButton = root.findViewById(R.id.floatingActionButtona);
+
+        floatingActionButton.setOnClickListener(view -> {
+            String add_date = getDate();
+            currenTime.setText(String.format("%s%s", basic_string, add_date));
+        });
 
         return root;
     }
+
+    public String getDate(){
+        // 현재시간을 msec 으로 구한다.
+        long now = System.currentTimeMillis();
+        // 현재시간을 date 변수에 저장한다.
+        Date date = new Date(now);
+        // 시간을 나타냇 포맷을 정한다 ( yyyy/MM/dd 같은 형태로 변형 가능 )
+        SimpleDateFormat sdfNow = new SimpleDateFormat("HH:mm:ss");
+        // nowDate 변수에 값을 저장한다.
+        return sdfNow.format(date);
+    }
+    // inner function
+
+
+
 }

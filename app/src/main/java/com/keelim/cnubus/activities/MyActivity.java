6806157a2bdc7093281c4a673b81d71e0d2bc7ac
keelim <kimh00335@gmail.com>
2019-09-08 20:46:22 +0900

    2019 09 08

---
+++ app/src/main/java/com/keelim/cnubus/activities/MyActivity.java
@@ -0,0 +1,23 @@
+package com.keelim.cnubus.activities;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.keelim.cnubus.R;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("설정");
+
+
+
+    }
+}

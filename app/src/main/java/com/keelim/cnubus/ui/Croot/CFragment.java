de1e67e43dd73cdfe15ea1217910c4ecebf3f915
keelim <kimh00335@gmail.com>
2019-09-14 02:50:23 +0900

    databinding source change

--- app/src/main/java/com/keelim/cnubus/ui/Croot/CFragment.java
+++ app/src/main/java/com/keelim/cnubus/ui/Croot/CFragment.java
@@ -5,34 +5,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.keelim.cnubus.R;
-import com.keelim.cnubus.ui.Broot.BViewModel;
-
-import java.util.List;
+import com.keelim.cnubus.databinding.FragmentCrootBinding;
 
 public class CFragment extends Fragment {
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         CViewModel homeViewModel = ViewModelProviders.of(this).get(CViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_croot, container, false);
-
-        ListView listView = root.findViewById(R.id.c_listview);
+        com.keelim.cnubus.databinding.FragmentCrootBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_croot, container, false);
 
         ArrayAdapter<CharSequence> arrayAdapterA = ArrayAdapter.createFromResource(getActivity(), R.array.cList,
                 android.R.layout.simple_list_item_1);
 
-        listView.setAdapter(arrayAdapterA);
-        return root;
+        binding.cListview.setAdapter(arrayAdapterA);
+        return binding.getRoot();
     }
 }

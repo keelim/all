af17fb327a0ca57e26f4831b3dda39d8ba1ff004
keelim <kimh00335@gmail.com>
2019-12-09 22:23:15 +0900

    2019 12 09

---
+++ app/src/main/java/com/keelim/cnubus/activities/SettingsActivity.java
@@ -0,0 +1,47 @@
+package com.keelim.cnubus.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.keelim.cnubus.R;
+import com.keelim.cnubus.activities.developer.DeveloperActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.settings_preferences);
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(Preference preference) { //preferebce 클릭 리스너
+            String key = preference.getKey();
+            if (key.equals("developer")) {
+                Intent intent_developer = new Intent(getContext(), DeveloperActivity.class);
+                startActivity(intent_developer);
+                return true;
+            }
+            return false;
+        }
+    }
+}

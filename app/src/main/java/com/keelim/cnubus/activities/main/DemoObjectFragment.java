d2a3e8ab1783b81f7d1aa1bb85d5faccfe61e1a3
keelim <kimh00335@gmail.com>
2019-12-12 14:24:24 +0900

    View pager 구성 완료

---
+++ app/src/main/java/com/keelim/cnubus/activities/main/DemoObjectFragment.java
@@ -0,0 +1,34 @@
+package com.keelim.cnubus.activities.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.keelim.cnubus.R;
+
+// Instances of this class are fragments representing a single
+// object in our collection.
+public class DemoObjectFragment extends Fragment {
+    public static final String ARG_OBJECT = "object";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_collection_object, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        Toast.makeText(getActivity(), "Toast", Toast.LENGTH_SHORT).show();
+    }
+
+}

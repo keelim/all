61eda245e0ff5b68dd1ed17d865bad79290629c8
keelim <kimh00335@gmail.com>
2019-12-13 02:21:24 +0900

    2019 12 13

--- app/src/main/java/com/keelim/cnubus/activities/main/RootListViewAdapter.java
+++ app/src/main/java/com/keelim/cnubus/activities/main/RootListViewAdapter.java
@@ -1,4 +1,38 @@
 package com.keelim.cnubus.activities.main;
 
-public class RootListViewAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class RootListViewAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<String> arrayList;
+
+    public RootListViewAdapter(Context context, ArrayList<String> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }

32ae729f3c0e68b49e016408fb151ca21f26bebb
keelim <kimh00335@gmail.com>
2019-12-12 20:23:02 +0900

    viewpager + tablayout complete

---
+++ app/src/main/java/com/keelim/cnubus/activities/main/ViewPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.keelim.cnubus.activities.main;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = new RootFragment();
+        Bundle args = new Bundle();
+        args.putInt("position", position);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "A노선";
+            case 1:
+                return "B노선";
+            case 2:
+                return "야간노선";
+            default:
+                return "";
+        }
+    }
+}

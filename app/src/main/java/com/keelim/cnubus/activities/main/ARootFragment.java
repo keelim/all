902591e6120b9eca84a588178984dde2a5f3522d
keelim <kimh00335@gmail.com>
2019-12-19 22:21:02 +0900

    2019 12 19

--- app/src/main/java/com/keelim/cnubus/activities/main/ARootFragment.java
+++ app/src/main/java/com/keelim/cnubus/activities/main/ARootFragment.java
@@ -1,67 +1,35 @@
 package com.keelim.cnubus.activities.main;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.keelim.cnubus.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ARootFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ARootFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ARootFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ARootFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ARootFragment newInstance(String param1, String param2) {
-        ARootFragment fragment = new ARootFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        Toast.makeText(getContext(), "ARoot", Toast.LENGTH_SHORT).show();
+        listView = getView().findViewById(R.id.lv_aroot);
+        applyList(getResources().getStringArray(R.array.aroot));
         return inflater.inflate(R.layout.fragment_a_root, container, false);
+    }
+
+    private void applyList(String[] root) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, root);
+        listView.setAdapter(adapter);
     }
 }

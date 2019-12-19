01b2b4e1b8d4cd9d001ee0099abce006e5a0a9f4
keelim <kimh00335@gmail.com>
2019-12-19 22:04:54 +0900

    2019 12 19

--- app/src/main/java/com/keelim/cnubus/activities/main/ViewPagerAdapter.java
+++ app/src/main/java/com/keelim/cnubus/activities/main/ViewPagerAdapter.java
@@ -1,28 +1,28 @@
 package com.keelim.cnubus.activities.main;
 
-import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
-public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+import java.util.ArrayList;
 
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    ArrayList<Fragment> fragments = new ArrayList<>();
 
     public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
         super(fm, behavior);
+        fragments.add(new ARootFragment());
+        fragments.add(new BRootFragment());
+        fragments.add(new CRootFragment());
+        fragments.add(new NightRootFragment());
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        Fragment fragment = new RootFragment();
-        Bundle args = new Bundle();
-        args.putInt("position", position);
-        fragment.setArguments(args);
-        return fragment;
+        return fragments.get(position);
     }
 
     @Override

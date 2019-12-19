8398431bd704d96d2469137b3977eba46011d60b
keelim <kimh00335@gmail.com>
2019-12-19 22:21:02 +0900

    2019 12 19

--- app/src/main/java/com/keelim/cnubus/activities/main/BRootFragment.java
+++ app/src/main/java/com/keelim/cnubus/activities/main/BRootFragment.java
@@ -1,67 +1,30 @@
 package com.keelim.cnubus.activities.main;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
 
 import com.keelim.cnubus.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BRootFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BRootFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ListView listView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public BRootFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BRootFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BRootFragment newInstance(String param1, String param2) {
-        BRootFragment fragment = new BRootFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        Toast.makeText(getActivity(), "BRoot", Toast.LENGTH_SHORT).show();
+        listView = getView().findViewById(R.id.lv_broot);
+        applyList(getResources().getStringArray(R.array.broot));
         return inflater.inflate(R.layout.fragment_b_root, container, false);
+    }
+
+    private void applyList(String[] root) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, root);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/keelim/cnubus/activities/main/CRootFragment.java
+++ app/src/main/java/com/keelim/cnubus/activities/main/CRootFragment.java
@@ -7,59 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.keelim.cnubus.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CRootFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CRootFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CRootFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CRootFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CRootFragment newInstance(String param1, String param2) {
-        CRootFragment fragment = new CRootFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        listView = listView.findViewById(R.id.lv_croot);
+        applyList(getResources().getStringArray(R.array.croot));
         return inflater.inflate(R.layout.fragment_c_root, container, false);
+    }
+
+    private void applyList(String[] root) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, root);
+        listView.setAdapter(adapter);
     }
 }

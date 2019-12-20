7b97bc8a02223f8c4693a9d76f46627bcf14a89b
keelim <kimh00335@gmail.com>
2019-12-20 12:32:22 +0900

    2019 12 20

--- app/src/main/java/com/keelim/cnubus/activities/MapsActivity.java
+++ app/src/main/java/com/keelim/cnubus/activities/MapsActivity.java
@@ -32,7 +32,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        checkPermission();
     }
 
     /**

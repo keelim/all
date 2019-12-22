83e1443631a4bf6202ba2b1e97af23ec4e7e0c95
keelim <kimh00335@gmail.com>
2019-12-22 19:33:26 +0900

    2019 12 20

---
+++ app/src/main/java/com/keelim/cnubus/activities/OpenSourceActivity.kt
@@ -0,0 +1,14 @@
+package com.keelim.cnubus.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.keelim.cnubus.R
+
+class OpenSourceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_source)
+    }
+}
---
+++ app/src/main/java/com/keelim/cnubus/activities/PopupActivity.kt
@@ -0,0 +1,30 @@
+package com.keelim.cnubus.activities
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import com.keelim.cnubus.R
+
+class PopupActivity : AppCompatActivity() {
+    //원하는 부분을 팝업으로 만들 수 있다.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE) // window 창 닫기
+        setContentView(R.layout.activity_popup)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return event.action != MotionEvent.ACTION_OUTSIDE
+    }
+
+    fun mOnClose(view: View?) { //activity result
+        val intent = Intent()
+        intent.putExtra("result", "Close Popup")
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
+}

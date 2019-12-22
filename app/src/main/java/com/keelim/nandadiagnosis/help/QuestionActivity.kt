f8e3e947802922a708a84c0f03697192d3cb8838
keelim <kimh00335@gmail.com>
2019-12-22 19:29:52 +0900

    2019 12 22
    Former-commit-id: 9163db0714b1f0c7e20153864e6c7ae9b485a941

---
+++ app/src/main/java/com/keelim/nandadiagnosis/help/QuestionActivity.kt
@@ -0,0 +1,12 @@
+package com.keelim.nandadiagnosis.help
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.keelim.nandadiagnosis.R
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}

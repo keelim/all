3a2e31a2cc6365892dceb36bd72bc6643c1ea7b2
keelim <kimh00335@gmail.com>
2019-12-24 14:18:30 +0900

    구글 맵 연동 완료

--- app/src/main/java/com/keelim/cnubus/activities/MapsActivity.java
+++ app/src/main/java/com/keelim/cnubus/activities/MapsActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.keelim.cnubus.R;
 
@@ -25,7 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        Toast.makeText(this, "버스 정류장을 표시 합니다.", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -43,16 +44,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // 구글 맵 객체를 불러온다.
         mMap = googleMap;
         // 서울 여의도에 대한 위치 설정
-        LatLng seoul = new LatLng(37.52487, 126.92723);
+        LatLng m1 = new LatLng(36.363895, 127.345148);
+        LatLng m2 = new LatLng(36.367002, 127.342782);
+        LatLng m3 = new LatLng(36.368622, 127.341531);
+        LatLng m4 = new LatLng(36.374241, 127.343924);
+        LatLng m5 = new LatLng(36.376406, 127.344168);
+        LatLng m6 = new LatLng(36.372513, 127.343118);
+        LatLng m7 = new LatLng(36.370587, 127.343520);
+        LatLng m8 = new LatLng(36.369522, 127.346725);
+        LatLng m9 = new LatLng(36.369119, 127.351884);
+        LatLng m10 = new LatLng(36.367465, 127.352190);
+        LatLng m11 = new LatLng(36.372480, 127.346155);
+        LatLng m12 = new LatLng(36.369780, 127.346901);
+        LatLng m13 = new LatLng(36.367404, 127.345517);
+        LatLng m14 = new LatLng(36.365505, 127.345159);
+        LatLng m15 = new LatLng(36.367564, 127.345800);
+
+
         // 구글 맵에 표시할 마커에 대한 옵션 설정
+
         MarkerOptions makerOptions = new MarkerOptions();
-        makerOptions
-                .position(seoul)
-                .title("원하는 위치(위도, 경도)에 마커를 표시했습니다.");
-        // 마커를 생성한다.
+        makerOptions.position(m1);
         mMap.addMarker(makerOptions);
-        //카메라를 여의도 위치로 옮긴다.
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m2);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m3);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m4);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m5);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m6);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m7);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m8);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m9);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m10);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m11);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m12);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m13);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m14);
+        mMap.addMarker(makerOptions);
+
+        makerOptions = new MarkerOptions();
+        makerOptions.position(m15);
+        mMap.addMarker(makerOptions);
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(m1));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 
 

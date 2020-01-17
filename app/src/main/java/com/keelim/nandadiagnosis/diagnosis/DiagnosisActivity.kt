9723f3eda2d9e28beaa044893fc0722389679d2d
keelim <kimh00335@gmail.com>
2020-01-17 14:47:19 +0900

    2020 01 17
    Former-commit-id: c1a936978c3fb17b356fc13696415915f50cbdf9

--- app/src/main/java/com/keelim/nandadiagnosis/diagnosis/DiagnosisActivity.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/diagnosis/DiagnosisActivity.kt
@@ -14,6 +14,7 @@ import kotlin.collections.ArrayList
 
 class DiagnosisActivity : AppCompatActivity() {
     private var arrayList: ArrayList<DiagnosisItem>? = ArrayList()
+    private var nav = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diagnosislist)
@@ -36,43 +37,56 @@ class DiagnosisActivity : AppCompatActivity() {
         val pointer = intent.getStringExtra("extra")
         when (Objects.requireNonNull(pointer)) {
             "1" -> {
-                customAdd(0, 11, array1) //ok
+                customAdd(0, 11, array1) //ok 1~12
+                nav = 0
             }
             "2" -> {
                 customAdd(12, 22, array1)
+                nav = 12
             }
             "3" -> {
                 customAdd(23, 32, array1)
+                nav = 23
             }
             "4" -> {
                 customAdd(33, 51, array1)
+                nav = 33
             }
             "5" -> {
                 customAdd(52, 86, array1)
+                nav = 52
             }
             "6" -> {
                 customAdd(87, 97, array1)
+                nav = 87
             }
             "7" -> {
                 customAdd(98, 108, array1)
+                nav = 98
             }
             "8" -> {
                 customAdd(109, 123, array1)
+                nav = 109
             }
             "9" -> {
                 customAdd(124, 129, array1)
+                nav = 124
             }
             "10" -> {
                 customAdd(130, 168, array1)
+                nav = 130
             }
             "11" -> {
                 customAdd(169, 179, array1)
+                nav = 169
             }
             "12" -> {
                 customAdd(180, 207, array1)
+                nav = 180
             }
             "13" -> {
                 customAdd(208, 236, array1)
+                nav = 208
             }
         }
     }

8eef8e9dd2a3c78a23be3027e84c0a155ae84a5b
keelim <kimh00335@gmail.com>
2020-01-31 02:56:11 +0900

    2020 01 31

--- app/src/main/java/com/keelim/cnubus/services/FirebaseInstanceIDService.kt
+++ app/src/main/java/com/keelim/cnubus/services/FirebaseInstanceIDService.kt
@@ -30,20 +30,20 @@ class FirebaseInstanceIDService : FirebaseMessagingService() {
     private fun sendNotification(remoteMessage: RemoteMessage) {
         val message = remoteMessage.data["data"]
         // 수신되는 푸시 메시지
-
         val messageDivider = message!!.indexOf("|")
         // 구분자를 통해 어떤 종류의 알람인지를 구별합니다.
 
         val pushType = message.substring(messageDivider + 1) // 구분자 뒤에 나오는 메시지
         val resultIntent = Intent(this, MainActivity::class.java)
         resultIntent.putExtra("pushType", pushType)
+
         val pendingIntent =
             PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // 버전 제약을 두는 것
             val channel = "채널"
             val channelName = "채널 이름" // 앱 설정에서 알림 이름으로 뜸.
-            val notichannel =
+            val notiChannel =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             val channelMessage = NotificationChannel(
                 channel, channelName,
@@ -58,7 +58,7 @@ class FirebaseInstanceIDService : FirebaseMessagingService() {
                 vibrationPattern = longArrayOf(100, 200, 100, 200)
             }
 
-            notichannel.createNotificationChannel(channelMessage)
+            notiChannel.createNotificationChannel(channelMessage)
             val notificationBuilder =
                 NotificationCompat.Builder(this, channel)
                     .setSmallIcon(R.mipmap.ic_launcher)
@@ -70,10 +70,9 @@ class FirebaseInstanceIDService : FirebaseMessagingService() {
                     .setContentIntent(pendingIntent)
                     .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
 
-            val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.notify(9999, notificationBuilder.build())
+            
         } else {
             val notificationBuilder =
                 NotificationCompat.Builder(this, "")
@@ -84,7 +83,6 @@ class FirebaseInstanceIDService : FirebaseMessagingService() {
                     .setColor(Color.parseColor("#0ec874")) // 푸시 색상
                     .setContentIntent(pendingIntent)
                     .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
-
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.notify(9999, notificationBuilder.build())
         }

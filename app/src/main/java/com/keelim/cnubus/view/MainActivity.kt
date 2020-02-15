93739b6e65700bc34a443770e88a7a0016467696
h0033 <kimh00335@gmail.com>
2020-02-16 00:28:24 +0900

    2020 02 16

--- app/src/main/java/com/keelim/cnubus/view/MainActivity.kt
+++ app/src/main/java/com/keelim/cnubus/view/MainActivity.kt
@@ -9,6 +9,14 @@ import androidx.core.view.GravityCompat
 import androidx.fragment.app.FragmentPagerAdapter
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.ActivityResult
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.keelim.cnubus.R
 import com.keelim.cnubus.model.ViewPagerAdapter
 import com.keelim.cnubus.view.recycler.RecyclerActivity
@@ -18,6 +26,7 @@ import kotlinx.android.synthetic.main.layout_drawer.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var pagerAdapter: ViewPagerAdapter
+    private lateinit var appUpdateManager: AppUpdateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +36,15 @@ class MainActivity : AppCompatActivity() {
             AdRequest.Builder().build()
 
         adView.loadAd(adRequest)
-        pagerAdapter = ViewPagerAdapter(
-            supportFragmentManager,
-            FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
-        )
+        pagerAdapter = ViewPagerAdapter(supportFragmentManager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT)
         viewpager.adapter = pagerAdapter
         tabLayout.setupWithViewPager(viewpager)
 
+
         main_drawer_button.setOnClickListener {
-            if(!drawer_container.isDrawerOpen(GravityCompat.END)) drawer_container.openDrawer(GravityCompat.END)
+            if (!drawer_container.isDrawerOpen(GravityCompat.END)) drawer_container.openDrawer(
+                GravityCompat.END
+            )
         }
         drawer_close.setOnClickListener {
             if (drawer_container.isDrawerOpen(GravityCompat.END)) {
@@ -52,10 +61,39 @@ class MainActivity : AppCompatActivity() {
         }
 
         drawer_lab1.setOnClickListener {
-            startActivity(Intent(this,RecyclerActivity::class.java))
+            startActivity(Intent(this, RecyclerActivity::class.java))
         }
-    }
 
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE && appUpdateInfo.isUpdateTypeAllowed(
+                    AppUpdateType.FLEXIBLE
+                )
+            ) {
+                appUpdateManager.startUpdateFlowForResult(
+                    // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                    appUpdateInfo,
+                    // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+                    AppUpdateType.FLEXIBLE,
+                    // The current activity making the update request.
+                    this,
+                    // Include a request code to later monitor this update request.
+                    2
+                )
+                Snackbar.make(drawer_container, "업데이트를 시작합니다.", Snackbar.LENGTH_SHORT).show()
+            } else Snackbar.make(drawer_container, "최신 버전 어플리케이션 사용해주셔서 감사합니다.", Snackbar.LENGTH_SHORT).show()
+        }
+
+        val listener = InstallStateUpdatedListener { state ->
+            if (state.installStatus() == InstallStatus.DOWNLOADED)
+                popupSnackbarForCompleteUpdate()
+        }
+        appUpdateManager.registerListener(listener)
+
+    }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -64,6 +102,29 @@ class MainActivity : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK) {
                 val result = data!!.getStringExtra("result")
             }
+        } else if (requestCode == 2) {
+            when (resultCode) {
+                RESULT_OK -> {
+                    Snackbar.make(drawer_container, "업데이트를 성공적으로 완료했습니다.", Snackbar.LENGTH_LONG)
+                }
+                Activity.RESULT_CANCELED -> {
+                    Snackbar.make(drawer_container, "업데이트를 취소하였습니다.", Snackbar.LENGTH_LONG)
+                }
+                ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
+                    Snackbar.make(drawer_container, "시스템 오류가 발생했습니다.", Snackbar.LENGTH_LONG)
+                }
+            }
         }
     }
+
+    private fun popupSnackbarForCompleteUpdate() {
+        Snackbar.make(
+            drawer_container, "업데이트를 다운로드 하고 있습니다.", Snackbar.LENGTH_INDEFINITE
+        ).apply {
+            setAction("RESTART") { appUpdateManager.completeUpdate() }
+            setActionTextColor(resources.getColor(R.color.colorAccent, this@MainActivity.theme))
+            show()
+        }
+    }
+
 }

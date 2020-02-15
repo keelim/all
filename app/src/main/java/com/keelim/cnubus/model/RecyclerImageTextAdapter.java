358c1e030dfc9329015f4a8205e6898f3f922ac7
h0033 <kimh00335@gmail.com>
2020-02-15 22:25:32 +0900

    2020 02 15

---
+++ app/src/main/java/com/keelim/cnubus/model/RecyclerImageTextAdapter.java
@@ -0,0 +1,79 @@
+package com.keelim.cnubus.model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.keelim.cnubus.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class RecyclerImageTextAdapter extends RecyclerView.Adapter<RecyclerImageTextAdapter.ViewHolder> {
+    private ArrayList<RecyclerItems> mData;
+
+    // 생성자에서 데이터 리스트 객체를 전달받음.
+    public RecyclerImageTextAdapter(ArrayList<RecyclerItems> list) {
+        mData = list;
+    }
+
+    // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴.
+    @Override
+    public RecyclerImageTextAdapter.ViewHolder onCreateViewHolder(@NotNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.items_recycler, parent, false);
+        RecyclerImageTextAdapter.ViewHolder vh = new ViewHolder(view);
+
+        return vh;
+    }
+
+    // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerImageTextAdapter.ViewHolder holder, int position) {
+
+        RecyclerItems item = mData.get(position);
+        holder.icon.setImageDrawable(item.getIconDrawable());
+        holder.title.setText(item.getTitleStr());
+        holder.desc.setText(item.getDesStr());
+    }
+
+    // getItemCount() - 전체 데이터 갯수 리턴.
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView icon;
+        TextView title;
+        TextView desc;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            // 뷰 객체에 대한 참조. (hold strong reference)
+            icon = itemView.findViewById(R.id.recycler_icon);
+            title = itemView.findViewById(R.id.recycler_title);
+            desc = itemView.findViewById(R.id.recycler_desc);
+
+            itemView.setOnClickListener(v -> {
+                int pos = getAdapterPosition();
+                if (pos != RecyclerView.NO_POSITION) {
+                    RecyclerItems item = mData.get(pos);
+
+                }
+            });
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/keelim/cnubus/model/RecyclerItems.kt
@@ -0,0 +1,9 @@
+package com.keelim.cnubus.model
+
+import android.graphics.drawable.Drawable
+
+data class RecyclerItems(
+        var iconDrawable: Drawable,
+        var titleStr: String,
+        var desStr: String
+)

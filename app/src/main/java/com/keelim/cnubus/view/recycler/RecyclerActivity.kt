800ae13426f7aabd0be7e5f0a0f7a5ab95728650
h0033 <kimh00335@gmail.com>
2020-02-15 22:41:51 +0900

    2020 02 15

---
+++ app/src/main/java/com/keelim/cnubus/view/recycler/RecyclerActivity.kt
@@ -0,0 +1,36 @@
+package com.keelim.cnubus.view.recycler
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.keelim.cnubus.R
+
+
+class RecyclerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler)
+
+        // 리사이클러뷰에 표시할 데이터 리스트 생성.
+        // 리사이클러뷰에 표시할 데이터 리스트 생성.
+        val list: ArrayList<String> = ArrayList()
+        for (i in 0..99) {
+            list.add(String.format("TEXT %d", i))
+        }
+
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler1)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        val adapter = RecyclerAdapter(list)
+        recyclerView.adapter = adapter
+    }
+
+}
+
+

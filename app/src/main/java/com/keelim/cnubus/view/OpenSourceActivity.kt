abdce943d004c8082a82cb1e71ec92b64da303f4
h0033 <kimh00335@gmail.com>
2020-08-10 00:42:54 +0900

    2020 08 10

--- app/src/main/java/com/keelim/cnubus/view/OpenSourceActivity.kt
+++ app/src/main/java/com/keelim/cnubus/view/OpenSourceActivity.kt
@@ -2,20 +2,12 @@ package com.keelim.cnubus.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import com.keelim.cnubus.R
-import kotlinx.android.synthetic.main.activity_open_source.*
 
-class OpenSourceActivity : AppCompatActivity(R.layout.activity_open_source) {
+class OpenSourceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setSupportActionBar(toolbar)
-        toolbar_layout.title = title
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setContentView(R.layout.activity_opensource)
     }
 }
--- app/src/main/java/com/keelim/cnubus/view/SettingActivity.kt
+++ app/src/main/java/com/keelim/cnubus/view/SettingActivity.kt
@@ -1,7 +1,5 @@
 package com.keelim.cnubus.view
 
-import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
@@ -9,10 +7,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import com.keelim.cnubus.R
-import com.keelim.cnubus.model.ModeCode
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingActivity : AppCompatActivity(R.layout.activity_settings) {
@@ -44,10 +40,6 @@ class SettingActivity : AppCompatActivity(R.layout.activity_settings) {
 
         override fun onPreferenceTreeClick(preference: Preference): Boolean { //preferebce 클릭 리스너
             when (preference.key) {
-                "app_share" -> {
-                    Toast.makeText(activity, "기능 준비 중 입니다. ", Toast.LENGTH_SHORT).show()
-                    return true
-                }
 
                 "opensource" -> {
                     Intent(context, OpenSourceActivity::class.java).apply {
@@ -69,33 +61,13 @@ class SettingActivity : AppCompatActivity(R.layout.activity_settings) {
                     }
                     return true
                 }
-
-                "lab1" -> {
-                    MaterialAlertDialogBuilder(requireContext())
-                        .setTitle("실험기능")
-                        .setMessage("실험기능으로 앱이 갑자기 정지 할 수 있습니다.\n 그래도 실행하시겠습니까")
-                        .setPositiveButton(
-                            "예"
-                        ) { _: DialogInterface?, _: Int ->
-                            val lab = Intent(context, MapsActivity::class.java)
-                            lab.putExtra("lab1", ModeCode.LAB1.ordinal)
-                            startActivity(lab)
-                        }
-                        .setNegativeButton(
-                            "아니오"
-                        ) { _: DialogInterface?, _: Int -> }
-                        .show()
-                    return true
-                }
             }
             return false
         }
 
         private fun getVersionInfo(): String? { // 버전을 확인을 하는 메소드
             return requireActivity().packageManager.getPackageInfo(
-                requireContext().packageName,
-                0
-            ).versionName
+                requireContext().packageName, 0).versionName
         }
     }
 }

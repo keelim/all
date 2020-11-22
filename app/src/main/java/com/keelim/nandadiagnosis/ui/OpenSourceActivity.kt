9d0e30725a4d14ba97060b2ad19695c1ca643133
keelim <kimh00335@gmail.com>
2020-11-22 23:30:04 +0900

    gitflow-feature-stash: 2020_11_22 Former-commit-id: 23091f4e278385dfbb175a919a57bfa11fd5b5b6

--- app/src/main/java/com/keelim/nandadiagnosis/ui/OpenSourceActivity.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/OpenSourceActivity.kt
@@ -2,14 +2,19 @@ package com.keelim.nandadiagnosis.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.keelim.nandadiagnosis.R
-import kotlinx.android.synthetic.main.activity_open_source.*
+import com.keelim.nandadiagnosis.databinding.ActivityOpenSourceBinding
+import kotlinx.android.synthetic.main.activity_open_source.view.*
 
-class OpenSourceActivity : AppCompatActivity(R.layout.activity_open_source) {
+class OpenSourceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOpenSourceBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setSupportActionBar(toolbar)
-        toolbar_layout.title = title
+        binding = ActivityOpenSourceBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setSupportActionBar(view.toolbar)
+        view.toolbar_layout.title = title
     }
 }

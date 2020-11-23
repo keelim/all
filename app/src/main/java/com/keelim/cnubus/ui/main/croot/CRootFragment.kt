5ac965276193cb3872bfaebd2a5dc51e78e72bfa
keelim <kimh00335@gmail.com>
2020-11-23 20:30:06 +0900

    gitflow-feature-stash: 20201123 Former-commit-id: 517cf0ea8a5882b416445d26c91101c37ac3ff92

--- app/src/main/java/com/keelim/cnubus/ui/main/croot/CRootFragment.kt
+++ app/src/main/java/com/keelim/cnubus/ui/main/croot/CRootFragment.kt
@@ -1,26 +1,33 @@
 package com.keelim.cnubus.ui.main.croot
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import com.keelim.cnubus.R
+import com.keelim.cnubus.databinding.FragmentCRootBinding
 import kotlinx.android.synthetic.main.fragment_c_root.*
 import kotlinx.android.synthetic.main.fragment_c_root.view.*
 
 class CRootFragment : Fragment(R.layout.fragment_c_root) {
-
+    private var fragmentCRootBinding: FragmentCRootBinding? = null
     private lateinit var rootList: Array<String>
 
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        requireView().lv_croot.setOnItemClickListener { _, _, _, _ ->
-            Snackbar.make(crootLayout, "C 노선 지도 업데이트 준비 중입니다. ", Snackbar.LENGTH_LONG).show()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentCRootBinding.bind(view)
+        fragmentCRootBinding = binding
+        binding.lvCroot.setOnItemClickListener { _, _, _, _ ->
+            Snackbar.make(binding.crootLayout, "C 노선 지도 업데이트 준비 중입니다. ", Snackbar.LENGTH_LONG).show()
         }
-
         rootList = resources.getStringArray(R.array.croot)
-        requireView().lv_croot.adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_list_item_1, rootList)
+        binding.lvCroot.adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_list_item_1, rootList)
+    }
 
+    override fun onDestroyView() {
+        fragmentCRootBinding = null
+        super.onDestroyView()
     }
 }

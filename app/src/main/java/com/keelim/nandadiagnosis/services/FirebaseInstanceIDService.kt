c00b855629162b345e6f0cdfcafbf5f7191320a6
keelim <kimh00335@gmail.com>
2020-11-26 18:52:55 +0900

    gitflow-feature-stash: 24--- Former-commit-id: 84363980d81c00f31df179f7718a924910ebb679

--- app/src/main/java/com/keelim/nandadiagnosis/services/FirebaseInstanceIDService.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/services/FirebaseInstanceIDService.kt
@@ -12,7 +12,6 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-
 import com.keelim.nandadiagnosis.R
 import com.keelim.nandadiagnosis.ui.main.MainActivity
 
@@ -29,18 +28,17 @@ class FirebaseInstanceIDService : FirebaseMessagingService() {
 
     private fun sendNotification(remoteMessage: RemoteMessage) {
         val message = remoteMessage.data["data"]
-        // 수신되는 푸시 메시지
-
-        // 구분자를 통해 어떤 종류의 알람인지를 구별합니다.
 
         val resultIntent = Intent(this, MainActivity::class.java).apply {
             putExtra("pushType", "pushType")
         }
         val pendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val channel = "채널"
+        val channelName = "채널 이름" // 앱 설정에서 알림 이름으로 뜸.
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = "채널"
-            val channelName = "채널 이름" // 앱 설정에서 알림 이름으로 뜸.
-            val notiChannel = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             val channelMessage = NotificationChannel(channel, channelName,
                     NotificationManager.IMPORTANCE_DEFAULT).apply {
                 description = "채널에 대한 설명입니다."
@@ -49,32 +47,18 @@ class FirebaseInstanceIDService : FirebaseMessagingService() {
                 setShowBadge(false)
                 vibrationPattern = longArrayOf(100, 200, 100, 200)
             }
-
-            notiChannel.createNotificationChannel(channelMessage)
-            val notificationBuilder = NotificationCompat.Builder(this, channel)
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setContentTitle("알림이 도착을 했습니다. ")
-                    .setChannelId(channel)
-                    .setAutoCancel(true)
-                    .setColor(Color.parseColor("#0ec874"))
-                    .setContentIntent(pendingIntent)
-                    .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
-
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(9999, notificationBuilder.build())
-
-        } else {
-            val notificationBuilder = NotificationCompat.Builder(this, "")
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setContentTitle("푸시 타이틀")
-                    .setContentText(message)
-                    .setAutoCancel(true)
-                    .setColor(Color.parseColor("#0ec874")) // 푸시 색상
-                    .setContentIntent(pendingIntent)
-                    .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
-
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(9999, notificationBuilder.build())
+            notificationManager.createNotificationChannel(channelMessage)
         }
+
+        val notificationBuilder = NotificationCompat.Builder(this, channel)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("알림이 도착을 했습니다. ")
+                .setChannelId(channel)
+                .setAutoCancel(true)
+                .setColor(Color.parseColor("#0ec874"))
+                .setContentIntent(pendingIntent)
+                .setDefaults(Notification.DEFAULT_SOUND or Notification.DEFAULT_VIBRATE)
+
+        notificationManager.notify(9999, notificationBuilder.build())
     }
 }

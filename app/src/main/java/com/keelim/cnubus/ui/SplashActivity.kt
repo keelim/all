022e3ce0af677deebf1d5c2a66f3904a321c713e
keelim <kimh00335@gmail.com>
2020-12-13 18:33:23 +0900

    gitflow-feature-stash: 20201213 Former-commit-id: 8fcdf8046d4e9cb5ea8ddfcb51d1d3794df39d3a

--- app/src/main/java/com/keelim/cnubus/ui/SplashActivity.kt
+++ app/src/main/java/com/keelim/cnubus/ui/SplashActivity.kt
@@ -5,40 +5,64 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
 import com.google.android.material.snackbar.Snackbar
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
-import com.keelim.cnubus.R
+import com.keelim.cnubus.BuildConfig
 import com.keelim.cnubus.databinding.ActivitySplashBinding
 import com.keelim.cnubus.ui.main.MainActivity
-import kotlinx.android.synthetic.main.activity_splash.*
 import java.util.*
 
 
-class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
+class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
-
+    private lateinit var interstitialAd: InterstitialAd
+    private val test = "ca -app-pub-3940256099942544/1033173712"
+    private infix fun String.or(that: String): String = if (BuildConfig.DEBUG) this else that
     private var listener = object : PermissionListener {
         override fun onPermissionGranted() {
-            Toast.makeText(this@SplashActivity, "모든 권한이 승인 되었습니다. ", Toast.LENGTH_SHORT).show()
-            Handler(Looper.getMainLooper()).postDelayed({
-                Intent(this@SplashActivity, MainActivity::class.java).apply {
-                    startActivity(this)
-                    finish()
+            Snackbar.make(binding.root, "모든 권한이 승인 되었습니다. ", Snackbar.LENGTH_SHORT).show()
+
+            interstitialAd = InterstitialAd(this@SplashActivity)
+            interstitialAd.adUnitId = test or BuildConfig.API_KEY2
+            interstitialAd.adListener = object : AdListener() {
+                override fun onAdLoaded() {
+                    interstitialAd.show()
                 }
-            }, 3000) //권한이 전부 승인되면 실행
+
+                override fun onAdClosed() {}
+
+                override fun onAdFailedToLoad(errorCode: Int) {
+                    Toast.makeText(this@SplashActivity, "ad load fail", Toast.LENGTH_SHORT).show()
+                    Log.e("ADMOB", errorCode.toString())
+                }
+            } //전면광고 셋팅
+            interstitialAd.loadAd(AdRequest.Builder().build())
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish() //앱을 종료한다.
+            }, 300)
         }
 
         override fun onPermissionDenied(deniedPermissions: ArrayList<String>?) {
-            Toast.makeText(this@SplashActivity, deniedPermissions.toString(), Toast.LENGTH_SHORT).show()
+            Handler(Looper.getMainLooper()).postDelayed({
+                Toast.makeText(this@SplashActivity, deniedPermissions.toString(), Toast.LENGTH_SHORT).show()
+                finish()
+            }, 3000)
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         TedPermission.with(this)
             .setPermissionListener(listener)

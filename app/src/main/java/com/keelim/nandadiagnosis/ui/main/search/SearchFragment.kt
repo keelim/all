09bce78ab405235752f43c6ae1dee99713459637
keelim <kimh00335@gmail.com>
2020-12-19 02:59:58 +0900

    gitflow-feature-stash: 20201219 Former-commit-id: dc69e5f2eeef47a50922bd998e0537496a2b2e5f

--- app/src/main/java/com/keelim/nandadiagnosis/ui/main/search/SearchFragment.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/main/search/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.keelim.nandadiagnosis.ui.main.search
 
+
 import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
@@ -12,6 +13,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.keelim.nandadiagnosis.R
 import com.keelim.nandadiagnosis.databinding.FragmentSearchBinding
+
 import com.keelim.nandadiagnosis.db.AppDatabase
 import com.keelim.nandadiagnosis.db.NandaEntity
 import io.reactivex.Observable
@@ -21,7 +23,6 @@ import java.util.concurrent.TimeUnit
 class SearchFragment : Fragment(R.layout.fragment_search) {
     private var fragmentSearchBinding: FragmentSearchBinding? = null
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
@@ -29,39 +30,17 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         val binding = FragmentSearchBinding.bind(view)
         fragmentSearchBinding = binding
 
-        binding.searchLv.adapter = DatabaseAdapter(requireContext(), arrayListOf())
-        binding.searchLv.setOnItemClickListener { adapterView, _, i, _ ->
-            val db = adapterView.adapter.getItem(i) as NandaEntity
-            Toast.makeText(activity, "클래스 영역: " + db.class_name + "도매인 영역" + db.domain_name, Toast.LENGTH_SHORT).show() // 무슨 화면이 나와야 하는가?
-            contentControl(db.nanda_id)
-        }
+        /*binding.list.layoutManager = LinearLayoutManager(requireActivity())
+        binding.list.adapter = SearchRecyclerViewAdapter(arrayListOf())*/
     }
 
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.search_menu, menu)
         val item = menu.findItem(R.id.menu_search)
+
         val searchManager = requireActivity().getSystemService(Context.SEARCH_SERVICE) as SearchManager
         val searchView = item.actionView as SearchView
-
-        /*searchView.apply {
-            setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
-            isSubmitButtonEnabled = true
-
-            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                //검색을 할 수 있게 하는 것
-                override fun onQueryTextSubmit(query: String): Boolean {
-                    val items = searchDiagnosis(query) //검색을 한다.
-
-                    fragmentSearchBinding!!.searchLv.adapter = DatabaseAdapter(activity!!, items).apply {
-                        notifyDataSetChanged()
-                    }
-                    return true
-                }
-
-                override fun onQueryTextChange(newText: String): Boolean { return false }
-            })
-        }*/
 
         Observable.create<CharSequence> { emitter ->
             searchView.apply {
@@ -72,7 +51,7 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
                     override fun onQueryTextSubmit(query: String): Boolean {
                         val items = searchDiagnosis(query) //검색을 한다.
 
-                        fragmentSearchBinding!!.searchLv.adapter = DatabaseAdapter(activity!!, items).apply {
+                        fragmentSearchBinding!!.list.adapter = SearchRecyclerViewAdapter(items).apply {
                             notifyDataSetChanged()
                         }
                         return true
@@ -98,19 +77,12 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         super.onCreateOptionsMenu(menu, inflater)
     }
 
-    private fun searchDiagnosis(keyword: String): List<NandaEntity> { //여기까지는 이상이 없는 것 같다.
-        return AppDatabase.getInstance(requireActivity())!!.dataDao().search(keyword)
-    }
-
-    private fun contentControl(position: Int) { // 넘겨 받은 난다 아이디를 통하여 구분
-        when (position) {
-            in 0..10 -> {
-            }
-        }
-    }
-
     override fun onDestroyView() {
         fragmentSearchBinding = null
         super.onDestroyView()
+    }
+
+    private fun searchDiagnosis(keyword: String): List<NandaEntity> { //여기까지는 이상이 없는 것 같다.
+        return AppDatabase.getInstance(requireActivity())!!.dataDao().search(keyword)
     }
 }
---
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/main/search/SearchRecyclerViewAdapter.kt
@@ -0,0 +1,45 @@
+package com.keelim.nandadiagnosis.ui.main.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.keelim.nandadiagnosis.databinding.ItemListviewBinding
+import com.keelim.nandadiagnosis.db.NandaEntity
+
+class SearchRecyclerViewAdapter(private val values: List<NandaEntity>) : RecyclerView.Adapter<SearchRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemListviewBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.itemTextView.text = item.diagnosis
+        holder.desView.text = item.reason
+        holder.classView.text = item.class_name
+        holder.domainView.text = item.domain_name
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: ItemListviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        val itemTextView: TextView = binding.diagnosisItem
+        val desView: TextView = binding.diagnosisDes
+        val classView = binding.className
+        val domainView = binding.domainName
+
+        init {
+            binding.root.setOnClickListener {
+                val pos = adapterPosition
+                val db = values[pos]
+                /*Snackbar.make(binding.root, "클래스 영역: ${db.class_name}도매인 영역${db.domain_name}", Snackbar.LENGTH_SHORT).show()*/
+            }
+        }
+
+        override fun toString(): String {
+            return super.toString() + " '" + desView.text + "'"
+        }
+    }
+
+}

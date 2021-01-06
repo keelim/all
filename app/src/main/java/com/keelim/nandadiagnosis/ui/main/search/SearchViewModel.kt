8bf41a1572cdbb478eda53413e6f04f394a581f6
keelim <kimh00335@gmail.com>
2021-01-07 01:43:26 +0900

    gitflow-feature-stash: 20210107 Former-commit-id: 7921a1ee702d1796ca488ce20cd2a8e456a5c9e4

---
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/main/search/SearchViewModel.kt
@@ -0,0 +1,8 @@
+package com.keelim.nandadiagnosis.ui.main.search
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SearchViewModel(application: Application) : AndroidViewModel(application) {
+    
+}

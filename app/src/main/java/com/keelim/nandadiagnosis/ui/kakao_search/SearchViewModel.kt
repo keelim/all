b14e01554b758628cf30b62627aadb031ba8cd3e
keelim <kimh00335@gmail.com>
2021-02-12 17:00:51 +0900

    gitflow-feature-stash: 20210212 Former-commit-id: 97f2f83079068558155cd0de8b22f2ecf68b1817

--- app/src/main/java/com/keelim/nandadiagnosis/ui/kakao_search/SearchViewModel.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/kakao_search/SearchViewModel.kt
@@ -1,6 +1,5 @@
 package com.keelim.nandadiagnosis.ui.kakao_search
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel

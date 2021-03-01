ef415f0e6ec54aaafcb9c3054963e627bcaeaa7f
keelim <kimh00335@gmail.com>
2021-03-01 12:49:23 +0900

    gitflow-feature-stash: 20210301 Former-commit-id: a50dc0aabda19d966515e733d2feb1e63431bed9

---
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.keelim.nandadiagnosis.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val PrimaryColor = Color(0xFFAB00)
+val ColorAccent = Color(0xFFDD4B)
+val ColorPrimaryDark = Color(0xc67c00)
+val Error = Color(0x121212)
+val MainBgColor = Color(0xFFFFFF)
+val PreviewEditButtonParentBg = Color(0xFFFFFF)
+val IconNormalColor = Color(0x121212)
+val BgForAlphaImage = Color(0xEEEEEE)
+val UcropToolbarTextColor = Color(0x121212)
+val Divider = Color(0xCCCCCC)
+val IcLauncher = Color(0x304FFE)
+val DcolorPrimary = Color(0xFFDD4B)
+val DcolorAccent = Color(0xFFECB3)
+val DcolorPrimaryDark = Color(0xFFE082)

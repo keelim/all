894edc4d5da8b22659ba988f961a06cb07268ff9
keelim <kimh00335@gmail.com>
2021-03-06 23:55:14 +0900

    introducing viewpager2+indicator + mvvm
    Former-commit-id: 4cc3bcc25a14c26598186b1b5e20a2bfbbfadd04

--- app/src/main/java/com/keelim/cnubus/data/Event.kt
+++ app/src/main/java/com/keelim/cnubus/data/Event.kt
@@ -13,28 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.keelim.common
+package com.keelim.cnubus.data
 
-import androidx.lifecycle.Observer
-
-open class Event<out T> (private val content: T) {
-    var flag = false
+open class Event<out T>(private val content: T) {
+    var hasBeenHandled = false
+        private set
 
     fun getContentIfNotHandled(): T? {
-        return if (flag) null
-        else {
-            flag = true
-            content
+        return if (hasBeenHandled) { // 이벤트가 이미 처리 되었다면
+            null // null을 반환하고,
+        } else { // 그렇지 않다면
+            hasBeenHandled = true // 이벤트가 처리되었다고 표시한 후에
+            content // 값을 반환합니다.
         }
     }
 
+    /**
+     * 이벤트의 처리 여부에 상관 없이 값을 반환합니다.
+     */
     fun peekContent(): T = content
-
-    class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
-        override fun onChanged(t: Event<T>?) {
-            t?.getContentIfNotHandled()?.let { value ->
-                onEventUnhandledContent(value)
-            }
-        }
-    }
 }

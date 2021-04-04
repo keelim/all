fb570a3760d67fc60194ea4739f6cd273b043788
keelim <kimh00335@gmail.com>
2021-04-04 21:04:00 +0900

    2021 04 04
    Former-commit-id: ded9b9c68722c4cdd3a3c27e05603621499a158d

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -1,10 +1,13 @@
 import org.gradle.api.JavaVersion
 
 object ProjectConfigurations {
+    const val applicationId = "com.keelim.cnubus"
     const val compileSdk = 30
     const val minSdk = 24
     const val targetSdk = 30
     const val buildTools = "31.0.0 rc2"
+    const val versionCode = 37
+    const val versionName = "2.0.37"
 
     val javaVer = JavaVersion.VERSION_1_8
     const val javaVerString = "1.8"

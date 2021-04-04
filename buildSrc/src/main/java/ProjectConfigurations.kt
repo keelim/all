b2a4b71274ce7d6b69e8e3750ec45cbc7f9ba493
keelim <kimh00335@gmail.com>
2021-04-04 15:03:53 +0900

    gitflow-feature-stash: 20210404 Former-commit-id: 2aa851c6dee6eef35b706391351dfba387a5d7c5

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -1,10 +1,13 @@
 import org.gradle.api.JavaVersion
 
 object ProjectConfigurations {
+    const val applicationId = "com.keelim.nandadiagnosis"
     const val compileSdk = 30
     const val minSdk = 24
     const val targetSdk = 30
     const val buildTools = "31.0.0 rc2"
+    const val versionCode = 38
+    const val versionName = "1.0.38"
 
     val javaVer = JavaVersion.VERSION_1_8
     const val javaVerString = "1.8"

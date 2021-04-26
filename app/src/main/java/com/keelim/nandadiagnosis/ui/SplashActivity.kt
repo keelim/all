7465960b115ff97c26c87256e2e63c6acd191ae8
keelim <kimh00335@gmail.com>
2021-04-26 22:38:18 +0900

    modify indent
    Former-commit-id: f39112567ccd667590ce38675e3359754ae88cd2

--- app/src/main/java/com/keelim/nandadiagnosis/ui/SplashActivity.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/SplashActivity.kt
@@ -46,153 +46,151 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class SplashActivity : BaseActivity() {
-    private var mInterstitialAd: InterstitialAd? = null
-    private val binding: ActivitySplashBinding by binding(R.layout.activity_splash)
-    private val test = "ca-app-pub-3940256099942544/1033173712"
-    private lateinit var settings: SharedPreferences
-    private infix fun String.or(that: String): String = if (BuildConfig.DEBUG) this else that
+  private var mInterstitialAd: InterstitialAd? = null
+  private val binding: ActivitySplashBinding by binding(R.layout.activity_splash)
+  private val test = "ca-app-pub-3940256099942544/1033173712"
+  private lateinit var settings: SharedPreferences
+  private infix fun String.or(that: String): String = if (BuildConfig.DEBUG) this else that
 
+  private val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+    arrayOf(
+      Manifest.permission.WRITE_EXTERNAL_STORAGE,
+      Manifest.permission.INSTALL_SHORTCUT,
+      Manifest.permission.INTERNET,
+      Manifest.permission.READ_EXTERNAL_STORAGE,
+      Manifest.permission.FOREGROUND_SERVICE
+    )
+  } else {
+    arrayOf(
+      Manifest.permission.WRITE_EXTERNAL_STORAGE,
+      Manifest.permission.INSTALL_SHORTCUT,
+      Manifest.permission.INTERNET,
+      Manifest.permission.READ_EXTERNAL_STORAGE
+    )
+  }
 
-    private val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-        arrayOf(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.INSTALL_SHORTCUT,
-            Manifest.permission.INTERNET,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.FOREGROUND_SERVICE
-        )
+  companion object {
+    const val PREF_FIRST_START = "AppFirstLaunch"
+    const val MULTIPLE_PERMISSIONS = 8888
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    addShortcut()
+
+    if (hasPermissions(permissions)) { // 권한이 있는 경우
+      goNext()
     } else {
-        arrayOf(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.INSTALL_SHORTCUT,
-            Manifest.permission.INTERNET,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        )
+      ActivityCompat.requestPermissions(this, permissions, MULTIPLE_PERMISSIONS)
     }
+  }
 
-    companion object {
-        const val PREF_FIRST_START = "AppFirstLaunch"
-        const val MULTIPLE_PERMISSIONS = 8888
-    }
+  private fun addShortcut() {
+    settings = getSharedPreferences(PREF_FIRST_START, 0)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        addShortcut()
+    if (settings.getBoolean("AppFirstLaunch", true)) { // 아이콘이 두번 추가 안되도록 하기 위해서 필요한 체크입니다.
+      settings.edit().putBoolean("AppFirstLaunch", false).apply()
 
-        if (hasPermissions(permissions)) { // 권한이 있는 경우
-            goNext()
-        } else {
-            ActivityCompat.requestPermissions(this, permissions, MULTIPLE_PERMISSIONS)
-        }
-    }
-
-    private fun addShortcut() {
-        settings = getSharedPreferences(PREF_FIRST_START, 0)
-
-        if (settings.getBoolean("AppFirstLaunch", true)) { // 아이콘이 두번 추가 안되도록 하기 위해서 필요한 체크입니다.
-            settings.edit().putBoolean("AppFirstLaunch", false).apply()
-
-            if (ShortcutManagerCompat.isRequestPinShortcutSupported(this)) {
-                val shortcutInfo = ShortcutInfoCompat.Builder(this, "#1")
-                    .setIntent(
-                        Intent(
-                            this,
-                            SplashActivity::class.java
-                        ).setAction(Intent.ACTION_MAIN)
-                    )
-                    .setShortLabel(getString(R.string.app_name)) //  아이콘에 같이 보여질 이름
-                    .setIcon(
-                        IconCompat.createWithResource(
-                            this,
-                            R.mipmap.ic_launcher
-                        )
-                    ) // 아이콘에 보여질 이미지
-                    .build()
-
-                ShortcutManagerCompat.requestPinShortcut(this, shortcutInfo, null)
-                Toast.makeText(this, "홈 화면에 바로가기를 추가하였습니다. ", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun showAd() {
-        val adRequest = AdRequest.Builder().build()
-        InterstitialAd.load(
-            this, test or "ca-app-pub-3115620439518585/4013096159", adRequest,
-            object : InterstitialAdLoadCallback() {
-                override fun onAdFailedToLoad(adError: LoadAdError) {
-                    Timber.d(adError.message)
-                    mInterstitialAd = null
-                }
-
-                override fun onAdLoaded(interstitialAd: InterstitialAd) {
-                    Timber.d("Ad was loaded.")
-                    mInterstitialAd = interstitialAd
-
-                    if (mInterstitialAd != null) {
-                        mInterstitialAd!!.show(this@SplashActivity)
-                    } else {
-                        Timber.d("The interstitial ad wasn't ready yet.")
-                    }
-                }
-            }
-        )
-    }
-
-
-    private fun hasPermissions(permissions: Array<String>): Boolean {
-        permissions.forEach { permission ->
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    permission
-                ) != PackageManager.PERMISSION_GRANTED
+      if (ShortcutManagerCompat.isRequestPinShortcutSupported(this)) {
+        val shortcutInfo = ShortcutInfoCompat.Builder(this, "#1")
+          .setIntent(
+            Intent(
+              this,
+              SplashActivity::class.java
+            ).setAction(Intent.ACTION_MAIN)
+          )
+          .setShortLabel(getString(R.string.app_name)) //  아이콘에 같이 보여질 이름
+          .setIcon(
+            IconCompat.createWithResource(
+              this,
+              R.mipmap.ic_launcher
             )
-                return false
-        }
-        return true
+          ) // 아이콘에 보여질 이미지
+          .build()
+
+        ShortcutManagerCompat.requestPinShortcut(this, shortcutInfo, null)
+        Toast.makeText(this, "홈 화면에 바로가기를 추가하였습니다. ", Toast.LENGTH_SHORT).show()
+      }
     }
+  }
 
-    // 권한 요청에 대한 결과 처리
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+  private fun showAd() {
+    val adRequest = AdRequest.Builder().build()
+    InterstitialAd.load(
+      this, test or "ca-app-pub-3115620439518585/4013096159", adRequest,
+      object : InterstitialAdLoadCallback() {
+        override fun onAdFailedToLoad(adError: LoadAdError) {
+          Timber.d(adError.message)
+          mInterstitialAd = null
+        }
 
-        when (requestCode) {
-            MULTIPLE_PERMISSIONS -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Snackbar.make(binding.root, "모든 권한이 승인 되었습니다. ", Snackbar.LENGTH_SHORT).show()
+        override fun onAdLoaded(interstitialAd: InterstitialAd) {
+          Timber.d("Ad was loaded.")
+          mInterstitialAd = interstitialAd
 
-                    showAd()
-                    goNext()
-                } else {
-                    // 하나라도 거부한다면.
-                    AlertDialog.Builder(this)
-                        .setTitle("앱 권한")
-                        .setMessage("해당 앱의 원할한 기능을 이용하시려면 애플리케이션 정보>권한> 에서 모든 권한을 허용해 주십시오")
-                        .setPositiveButton("권한설정") { dialog, which ->
-                            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                                data = Uri.parse("package:" + applicationContext.packageName)
-                                startActivity(this)
-                                dialog.cancel()
-                            }
-                        }
-                        .setNegativeButton("취소") { dialog, which -> dialog.cancel() }
-                        .show()
-                }
+          if (mInterstitialAd != null) {
+            mInterstitialAd!!.show(this@SplashActivity)
+          } else {
+            Timber.d("The interstitial ad wasn't ready yet.")
+          }
+        }
+      }
+    )
+  }
+
+  private fun hasPermissions(permissions: Array<String>): Boolean {
+    permissions.forEach { permission ->
+      if (ActivityCompat.checkSelfPermission(
+          this,
+          permission
+        ) != PackageManager.PERMISSION_GRANTED
+      )
+        return false
+    }
+    return true
+  }
+
+  // 권한 요청에 대한 결과 처리
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    when (requestCode) {
+      MULTIPLE_PERMISSIONS -> {
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          Snackbar.make(binding.root, "모든 권한이 승인 되었습니다. ", Snackbar.LENGTH_SHORT).show()
+
+          showAd()
+          goNext()
+        } else {
+          // 하나라도 거부한다면.
+          AlertDialog.Builder(this)
+            .setTitle("앱 권한")
+            .setMessage("해당 앱의 원할한 기능을 이용하시려면 애플리케이션 정보>권한> 에서 모든 권한을 허용해 주십시오")
+            .setPositiveButton("권한설정") { dialog, which ->
+              Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.parse("package:" + applicationContext.packageName)
+                startActivity(this)
+                dialog.cancel()
+              }
             }
+            .setNegativeButton("취소") { dialog, which -> dialog.cancel() }
+            .show()
         }
+      }
     }
+  }
 
-    private fun goNext() {
-        CoroutineScope(Dispatchers.IO).launch {
-            delay(1000)
-            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-            finish() // 앱을 종료한다.
-        }
+  private fun goNext() {
+    CoroutineScope(Dispatchers.IO).launch {
+      delay(1000)
+      startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+      finish() // 앱을 종료한다.
     }
+  }
 
-    override fun onBackPressed() {}
+  override fun onBackPressed() {}
 }
--- common/src/main/java/com/keelim/common/BackPressCloseHandler.kt
+++ common/src/main/java/com/keelim/common/BackPressCloseHandler.kt
@@ -19,26 +19,26 @@ import android.app.Activity
 import android.widget.Toast
 
 class BackPressCloseHandler(private val activity: Activity) {
-    private var backKeyPressedTime: Long = 0
-    private lateinit var toast: Toast
+  private var backKeyPressedTime: Long = 0
+  private lateinit var toast: Toast
 
-    fun onBackPressed() {
-        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
-            backKeyPressedTime = System.currentTimeMillis()
-            showGuide()
-            return
-        }
-        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
-            activity.finish()
-            toast.cancel()
-        }
+  fun onBackPressed() {
+    if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+      backKeyPressedTime = System.currentTimeMillis()
+      showGuide()
+      return
     }
+    if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+      activity.finish()
+      toast.cancel()
+    }
+  }
 
-    private fun showGuide() {
-        toast = Toast.makeText(
-            activity, "'뒤로'버튼을 한번 더 누르시면 종료됩니다.",
-            Toast.LENGTH_SHORT
-        )
-        toast.show()
-    }
+  private fun showGuide() {
+    toast = Toast.makeText(
+      activity, "'뒤로'버튼을 한번 더 누르시면 종료됩니다.",
+      Toast.LENGTH_SHORT
+    )
+    toast.show()
+  }
 }

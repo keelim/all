b6479573be9b4bef2ca8921b48ea23df873c93e8
keelim <kimh00335@gmail.com>
2021-04-26 22:23:07 +0900

    gitflow-feature-stash: 20210426 Former-commit-id: e70de83816dde00db9ba8e1f7a754556d3770eb7

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -6,8 +6,8 @@ object ProjectConfigurations {
     const val minSdk = 24
     const val targetSdk = 30
     const val buildTools = "30.0.3"
-    const val versionCode = 39
-    const val versionName = "1.0.39d"
+    const val versionCode = 40
+    const val versionName = "1.0.40"
 
     val javaVer = JavaVersion.VERSION_11
     const val javaVerString = "1.8"

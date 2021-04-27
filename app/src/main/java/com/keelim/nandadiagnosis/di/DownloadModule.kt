f382b7d2072a0fb6d403d0fda8ae1d355ea335c1
keelim <kimh00335@gmail.com>
2021-04-27 09:44:07 +0900

    modify download module
    Former-commit-id: 41696abc3e0885700ecf20d399169ce3c2409304

--- app/src/main/java/com/keelim/nandadiagnosis/di/DownloadModule.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/di/DownloadModule.kt
@@ -21,13 +21,13 @@ import org.koin.dsl.module
 import java.io.File
 
 val downloadModule = module {
-    single { (url: String, file: File) ->
-        DownloadManager.Request(Uri.parse(url))
-            .setTitle("Downloading")
-            .setDescription("Downloading Database file")
-            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            .setDestinationUri(Uri.fromFile(file))
-            .setAllowedOverMetered(true)
-            .setAllowedOverRoaming(true)
-    }
+  single { (url: String, file: File) ->
+    DownloadManager.Request(Uri.parse(url))
+      .setTitle("Downloading")
+      .setDescription("Downloading Database file")
+      .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+      .setDestinationUri(Uri.fromFile(file))
+      .setAllowedOverMetered(true)
+      .setAllowedOverRoaming(true)
+  }
 }

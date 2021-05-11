0b4fc7db13f601034a06e08b5f96f484e08b5329
keelim <kimh00335@gmail.com>
2021-05-11 21:31:42 +0900

    gitflow-feature-stash: 0502 Former-commit-id: 7df2f3108ce553f553f76d9c864a6921f6ac0807

---
+++ data/db/src/main/java/com/keelim/nandadiagnosis/data/db/history/HistoryDao.kt
@@ -0,0 +1,32 @@
+/*
+ * Designed and developed by 2020 keelim (Jaehyun Kim)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.keelim.nandadiagnosis.data.db.history
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface HistoryDao {
+  @Query("select * from history")
+  fun getAll(): List<History>
+
+  @Insert
+  fun insertHisotry(history: History)
+
+  @Query("delete from history where keyword==:keyword")
+  fun delete(keyword: String)
+}

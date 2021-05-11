44518016ff6ccb858d1f16aadf2521c0cf00ba3e
keelim <kimh00335@gmail.com>
2021-05-11 20:55:58 +0900

    modify indent and introduce buildSrc
    Former-commit-id: 555c5f7cc6b7694d6cfc3f936f1ba6aa4a0374be

--- app/src/main/java/com/keelim/cnubus/base/BaseActivity.kt
+++ app/src/main/java/com/keelim/cnubus/base/BaseActivity.kt
@@ -20,7 +20,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-open class BaseActivity: AppCompatActivity(){
+open class BaseActivity : AppCompatActivity() {
     protected inline fun <reified T : ViewDataBinding> binding(@LayoutRes resId: Int): Lazy<T> =
         lazy { DataBindingUtil.setContentView<T>(this, resId) }
 }

18c40d2bdbb62502259f50d841d18e37adf1998d
keelim <kimh00335@gmail.com>
2021-06-14 15:00:25 +0900

    introduce error module and modify project
    Former-commit-id: d03fc9fec21087c7b53d14766d1f3f677d19adce

--- feature/error/src/main/java/com/keelim/nandaDiagnosis/feature/error/ErrorActivity.kt
+++ feature/error/src/main/java/com/keelim/nandaDiagnosis/feature/error/ErrorActivity.kt
@@ -1,18 +1,3 @@
-/*
- * Designed and developed by 2020 keelim (Jaehyun Kim)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.keelim.nandaDiagnosis.feature.error
 
 import android.content.Intent
@@ -20,23 +5,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.keelim.nandaDiagnosis.feature.error.databinding.ActivityErrorBinding
 
-class ErrorActivity : AppCompatActivity(R.layout.activity_error) {
-  private lateinit var binding: ActivityErrorBinding
-
-  private val lastActivityIntent by lazy {
-    intent.getParcelableExtra<Intent>(EXTRA_INTENT)
-  }
-
-  private val errorText by lazy {
-    intent.getStringExtra(EXTRA_ERROR_TEXT)
-  }
+class ErrorActivity : AppCompatActivity() {
+  private val binding by lazy{ ActivityErrorBinding.inflate(layoutInflater)}
+  private val lastActivityIntent by lazy { intent.getParcelableExtra<Intent>(EXTRA_INTENT) }
+  private val errorText by lazy { intent.getStringExtra(EXTRA_ERROR_TEXT) }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    binding = ActivityErrorBinding.inflate(layoutInflater)
 
     binding.tvErrorLog.text = errorText
-
     binding.btnReload.setOnClickListener {
       startActivity(lastActivityIntent)
       finish()

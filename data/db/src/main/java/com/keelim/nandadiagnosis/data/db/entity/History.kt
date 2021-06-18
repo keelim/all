95c4c7e0b3266ade896bd8371b1b6de004590955
keelim <kimh00335@gmail.com>
2021-06-18 11:52:20 +0900

    modify database
    Former-commit-id: c270afb29caf0d04491f3afe31e7a10c6bff423d

--- data/db/src/main/java/com/keelim/nandadiagnosis/data/db/entity/History.kt
+++ data/db/src/main/java/com/keelim/nandadiagnosis/data/db/entity/History.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.keelim.nandadiagnosis.data.db.history
+package com.keelim.nandadiagnosis.data.db.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity

af999aaa82823f06c0870c7bc71e0eab5eb71305
kimh00335@gmail.com <kimjaehyun08!>
2021-06-21 11:14:03 +0900

    2021 06 21
    Former-commit-id: 1f690729c59aa8fc25309a988160e44a8315c305

--- app/src/main/java/com/keelim/nandadiagnosis/ui/auth/profile/ProfileFragment2.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/auth/profile/ProfileFragment2.kt
@@ -15,18 +15,99 @@
  */
 package com.keelim.nandadiagnosis.ui.auth.profile
 
+import android.app.Activity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.keelim.nandadiagnosis.R
 import com.keelim.nandadiagnosis.base.BaseFragment
 import com.keelim.nandadiagnosis.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
-internal class ProfileFragment2 : BaseFragment<ProfileViewModel, FragmentProfileBinding>() {
+internal class ProfileFragment2 @Inject constructor(
+  private val auth: FirebaseAuth,
+) : BaseFragment<ProfileViewModel, FragmentProfileBinding>() {
   override val viewModel: ProfileViewModel by viewModels()
 
-  override fun getViewBinding(): FragmentProfileBinding = FragmentProfileBinding.inflate(layoutInflater)
+  override fun getViewBinding(): FragmentProfileBinding =
+    FragmentProfileBinding.inflate(layoutInflater)
 
-  override fun observeData() {
-    TODO("Not yet implemented")
+  private val gso by lazy {
+    GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+      .requestIdToken(getString(R.string.default_web_client_id))
+      .requestEmail()
+      .build()
+  }
+  private val gsc by lazy { GoogleSignIn.getClient(requireActivity(), gso) }
+
+  private val loginLauncher =
+    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+      if (result.resultCode == Activity.RESULT_OK) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+
+        try {
+          task.getResult(ApiException::class.java)?.let { account ->
+            Timber.d("firebase with google ${account.id}")
+          } ?: throw Exception()
+        } catch (e: Exception) {
+          e.printStackTrace()
+        }
+      }
+    }
+
+  override fun observeData() = viewModel.profileState.observe(this) {
+    when (it) {
+      is ProfileState.UnInitialized -> initViews()
+      is ProfileState.Loading -> handleLoading()
+      is ProfileState.Login -> TODO()
+      is ProfileState.Success -> handleSuccess(it)
+      is ProfileState.Error -> handleError()
+    }
+  }
+
+
+  private fun initViews() = with(binding) {
+    loginButton.setOnClickListener {
+      signInGoogle()
+    }
+
+    logoutButton.setOnClickListener {
+
+    }
+  }
+
+  private fun signInGoogle() {
+    val signInIntent = gsc.signInIntent
+    loginLauncher.launch(signInIntent)
+  }
+
+  private fun handleLoading() = with(binding) {
+    progressBar.isVisible = true
+    loginRequiredGroup.isGone = true
+  }
+
+  private fun handleSuccess(state: ProfileState.Success) = with(binding) {
+    progressBar.isGone = true
+    when (state) {
+      ProfileState.Success.NotRegistered -> {
+
+      }
+      is ProfileState.Success.Registered -> {
+        profileGroup.isGone = true
+        loginRequiredGroup.isVisible = true
+      }
+    }
+  }
+
+  private fun handleError() {
+
   }
 }

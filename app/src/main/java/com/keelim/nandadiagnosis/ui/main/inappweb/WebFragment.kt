dd1f5c274211e3c39dc2a3c7b7bcd7c4fe19e91c
kimh00335@gmail.com <kimjaehyun08!>
2021-06-24 10:03:05 +0900

    modify AboutFragment.kt
    Former-commit-id: 307ef3a3352224623beb44400cfca1ed8bfdf757

--- app/src/main/java/com/keelim/nandadiagnosis/ui/main/inappweb/WebFragment.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/main/inappweb/WebFragment.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.keelim.nandadiagnosis.ui
+package com.keelim.nandadiagnosis.ui.main.inappweb
 
 import android.annotation.SuppressLint
 import android.content.Context

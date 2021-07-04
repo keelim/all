f46a5131861d67499d2f1aab6c90202b21eb7f8f
keelim <kimh00335@hmail.com>
2021-07-04 22:38:01 +0900

    2021 07 04
    Former-commit-id: e5c586a0f4664779ad15bb91a029c5b80a32661b

--- compose/src/main/java/com/keelim/nandadiagnosis/compose/ui/theme/Theme.kt
+++ compose/src/main/java/com/keelim/nandadiagnosis/compose/ui/theme/Theme.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed by 2020 keelim (Jaehyun Kim)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.keelim.nandadiagnosis.compose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -7,15 +22,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+  primary = Purple200,
+  primaryVariant = Purple700,
+  secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+  primary = Purple500,
+  primaryVariant = Purple700,
+  secondary = Teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -29,19 +44,19 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun NandaDiagnosisTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+  val colors = if (darkTheme) {
+    DarkColorPalette
+  } else {
+    LightColorPalette
+  }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+  MaterialTheme(
+    colors = colors,
+    typography = Typography,
+    shapes = Shapes,
+    content = content
+  )
 }

2455763c6286c2725ddff24f2c1c0f190a8071d6
keelim <kimh00335@gmail.com>
2021-07-09 23:14:29 +0900

    introduce domain module
    Former-commit-id: 70f0124a88c927543c67d54fe01a562edf0ca8c9

---
+++ domain/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("kotlin-parcelize")
+    id("dagger.hilt.android.plugin")
+}
+
+listOf(
+    "android.gradle",
+).forEach { file ->
+    apply(from = "${rootDir}/gradle/${file}")
+}
+
+dependencies {
+    implementation(project(":data"))
+    implementation(AndroidX.core_ktx)
+    implementation(Hilt.android)
+    kapt(Hilt.hilt_compiler)
+
+    implementation(SquareUp.timber)
+    implementation(Kotlin.Coroutines.android)
+
+    implementation(Kotlin.stdlibJvm)
+    testImplementation(AppTest.junit)
+    androidTestImplementation(AppTest.androidJunit)
+    androidTestImplementation(AppTest.espressoCore)
+    androidTestImplementation(Kotlin.Coroutines.test)
+}

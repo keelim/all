322cf297ebebcdaed0828f04ce08e5d2f5156803
김재현 <h00335@naver.com>
2021-08-12 20:49:29 +0900

    write hilt setting
    Former-commit-id: 885b6354ebedef6c5f47b9b20b03179232a65986

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -2,9 +2,9 @@ import org.gradle.api.JavaVersion
 
 object ProjectConfigurations {
     const val applicationId = "com.keelim.cnubus"
-    const val compileSdk = 30
+    const val compileSdk = 31
     const val minSdk = 24
-    const val targetSdk = 30
+    const val targetSdk = 31
     const val buildTools = "30.0.3"
     const val versionCode = 50
     const val versionName = "2.0.50"

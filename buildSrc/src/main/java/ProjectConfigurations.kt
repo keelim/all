5486643c8d24ef7629f5271787bc5cf26962d04e
김재현 <h00335@naver.com>
2021-08-12 20:55:50 +0900

    upgrade app version
    Former-commit-id: db57364dadbb70d9c1e96c1955bbeadcd1130a9b

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -6,8 +6,8 @@ object ProjectConfigurations {
     const val minSdk = 24
     const val targetSdk = 31
     const val buildTools = "30.0.3"
-    const val versionCode = 50
-    const val versionName = "2.0.50"
+    const val versionCode = 51
+    const val versionName = "2.0.51"
 
     val javaVer = JavaVersion.VERSION_11
     const val javaVerString = "11"

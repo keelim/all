a340fd97b07dfbf62d0a8e822c18d88c84ba4415
김재현 <h00335@naver.com>
2021-08-20 18:25:26 +0900

    [version] modify gradle.kts
    Former-commit-id: 31a1088e6724e43ab7287e1d8ff60999197f80df

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -1,19 +1,13 @@
-object Coil {
-    const val coil = "io.coil-kt:coil:1.2.1"
-}
-
-object DataStore {
-    const val dataStorePreferences = "1.0.0-beta01"
-    const val preferences =
-        "androidx.datastore:datastore-preferences:${dataStorePreferences}"
-}
-
-
 object Play {
     const val play_location = "com.google.android.gms:play-services-location:18.0.0"
-    const val play_map = "com.google.android.gms:play-services-maps:17.0.0"
-    const val play_ads = "com.google.android.gms:play-services-ads:20.0.0"
+    const val play_map = "com.google.android.gms:play-services-maps:17.0.1"
+    const val play_ads = "com.google.android.gms:play-services-ads:20.1.0"
     const val play_core = "com.google.android.play:core:1.10.0"
+    const val maps_sdk = "com.google.maps.android:maps-ktx:3.0.1"
+}
+
+object Coil {
+    const val coil = "io.coil-kt:coil:1.2.0"
 }
 
 object Rx {
@@ -22,8 +16,22 @@ object Rx {
     const val rxkotlin = "io.reactivex.rxjava2:rxkotlin:2.4.0"
 }
 
+object Kotlin {
+    const val version = "1.5.21"
+    const val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+
+    object Coroutines {
+        private const val coroutinesVersion = "1.5.0"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
+        const val android =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion"
+    }
+    const val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0"
+}
+
 object SquareUp {
-    private const val version = "4.9.1"
+    private const val version = "4.9.0"
     private const val rversion = "2.9.0"
     const val timber = "com.jakewharton.timber:timber:4.7.1"
     const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.5"
@@ -35,7 +43,7 @@ object SquareUp {
 }
 
 object Room {
-    private const val roomVersion = "2.3.0"
+    private const val roomVersion = "2.3.0-alpha02"
     const val runtime = "androidx.room:room-runtime:$roomVersion"
     const val compiler = "androidx.room:room-compiler:$roomVersion"
     const val ktx = "androidx.room:room-ktx:$roomVersion"
@@ -48,10 +56,16 @@ object LifeCycle {
     const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion"
     const val runtime = "androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion"
     const val extensions = "androidx.lifecycle:lifecycle-extensions:2.2.0"
+    const val services = "androidx.lifecycle:lifecycle-services:$lifecycleVersion"
+}
+
+object DataStore {
+    const val dataStorePreferences = "1.0.0-alpha07"
+    const val preferences = "androidx.datastore:datastore-preferences:${dataStorePreferences}"
 }
 
 object Hilt {
-    const val version = "2.38"
+    const val version = "2.36"
     const val android = "com.google.dagger:hilt-android:${version}"
     const val hilt_compiler = "com.google.dagger:hilt-android-compiler:${version}"
     const val hilt_common = "androidx.hilt:hilt-common:$version"
@@ -61,57 +75,47 @@ object Hilt {
     const val android_hilt_compiler = "androidx.hilt:hilt-compiler:${version}"
 }
 
-object Compose {
-    private const val compose_version = "1.0.0-rc01"
-    const val compose_ui = "androidx.compose.ui:ui:$compose_version"
-    const val compose_material = "androidx.compose.material:material:$compose_version"
-    const val compose_ui_tooling = "androidx.compose.ui:ui-tooling:$compose_version"
-}
-
-object Kotlin {
-    const val version = "1.5.10"
-    const val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
-
-    object Coroutines {
-        private const val coroutinesVersion = "1.5.0"
-        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
-        const val android =
-            "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
-        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion"
-    }
-
-    const val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0"
-}
-
 object AndroidX {
     const val annotation = "androidx.annotation:annotation:1.2.0-alpha01"
 
-    private const val activityVersion = "1.2.3"
+    private const val activityVersion = "1.2.2"
     const val activity = "androidx.activity:activity:$activityVersion"
     const val activity_ktx = "androidx.activity:activity-ktx:$activityVersion"
 
-    private const val fragmentVersion = "1.3.4"
-    const val fragment = "androidx.fragment:fragment:$fragmentVersion"
-    const val fragment_ktx = "androidx.fragment:fragment-ktx:$fragmentVersion"
-
     const val appcompat = "androidx.appcompat:appcompat:1.3.0-rc01"
-    const val core_ktx = "androidx.core:core-ktx:1.6.0-alpha03"
+    const val core_ktx = "androidx.core:core-ktx:1.6.0-alpha01"
 
     const val navigation_fragment = "androidx.navigation:navigation-fragment-ktx:2.3.5"
     const val navigation_ui = "androidx.navigation:navigation-ui-ktx:2.3.5"
+
+    const val browser = "androidx.browser:browser:1.3.0"
+    private const val fragmentVersion = "1.3.0-beta01"
+    const val fragment_ktx = "androidx.fragment:fragment-ktx:$fragmentVersion"
 }
 
 object UI {
     const val browser = "androidx.browser:browser:1.3.0-alpha06"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4"
     const val drawerlayout = "androidx.drawerlayout:drawerlayout:1.1.1"
-    const val material = "com.google.android.material:material:1.4.0-alpha02"
+    const val material = "com.google.android.material:material:1.4.0"
     const val swiperefreshlayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01"
     const val palette = "androidx.palette:palette:1.0.0"
     const val preference_ktx = "androidx.preference:preference-ktx:1.1.1"
     const val recyclerview = "androidx.recyclerview:recyclerview:1.2.0"
-    const val recycler_selection = "androidx.recyclerview:recyclerview-selection:1.1.0"
     const val viewPager = "androidx.viewpager2:viewpager2:1.1.0-alpha01"
+    const val recycler_selection = "androidx.recyclerview:recyclerview-selection:1.1.0"
+}
+
+object Compose {
+    private const val compose_version = "1.0.1"
+    const val compose_ui = "androidx.compose.ui:ui:$compose_version"
+    const val compose_material = "androidx.compose.material:material:$compose_version"
+    const val compose_ui_tooling = "androidx.compose.ui:ui-tooling:$compose_version"
+    const val foundation = "androidx.compose.foundation:foundation:$compose_version"
+    const val compose_icon = "androidx.compose.material:material-icons-core:$compose_version"
+    const val expand_icon = "androidx.compose.material:material-icons-extended:$compose_version"
+    const val runtime_livedata = "androidx.compose.runtime:runtime-livedata:$compose_version"
+    const val compose_junit = "androidx.compose.ui:ui-test-junit4:$compose_version"
 }
 
 object AppTest {
@@ -122,37 +126,8 @@ object AppTest {
     const val espressoCore = "androidx.test.espresso:espresso-core:3.3.0"
 }
 
-
 object Glide {
     private const val version = "4.12.0"
     const val core = "com.github.bumptech.glide:glide:$version"
     const val compiler = "com.github.bumptech.glide:compiler:$version"
-
-
-    object Dep {
-        object GradlePlugin {
-            const val androidStudioVersion = "4.2.0-alpha14"
-            const val android = "com.android.tools.build:gradle:$androidStudioVersion"
-            const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
-            const val kotlinSerialization =
-                "org.jetbrains.kotlin:kotlin-serialization:${Kotlin.version}"
-            const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.4.0"
-            const val hilt = "com.google.dagger:hilt-android-gradle-plugin:${Hilt.version}"
-        }
-
-
-        object Dagger {
-            private const val version = "2.29.1"
-            const val dagger = "com.google.dagger:dagger:$version"
-            const val compiler = "com.google.dagger:dagger-compiler:$version"
-        }
-
-
-
-
-        const val jsoup = "org.jsoup:jsoup:1.13.1"
-
-    }
-
-
 }

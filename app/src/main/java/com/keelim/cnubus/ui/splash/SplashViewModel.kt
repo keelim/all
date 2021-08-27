3f3a9b5c1cd313cbbd91f24823cd2a197f8e81ee
김재현 <h00335@naver.com>
2021-08-27 13:41:03 +0900

    [develop] modify splash
    Former-commit-id: 597c32f589078aa0fe8b77bc19d11940988c04d6

--- app/src/main/java/com/keelim/cnubus/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/keelim/cnubus/ui/splash/SplashViewModel.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.keelim.cnubus.ui.saplsh
+package com.keelim.cnubus.ui.splash
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope

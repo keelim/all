7dd49ec8744eb64aee15fb8384e2dc99166148d3
김재현 <h00335@naver.com>
2021-09-03 14:30:46 +0900

    [version] 2.0.53
    Former-commit-id: dad7fa15ec9eb3b400c36e9608925a214296954b

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -6,8 +6,8 @@ object ProjectConfigurations {
     const val minSdk = 24
     const val targetSdk = 31
     const val buildTools = "30.0.3"
-    const val versionCode = 52
-    const val versionName = "2.0.52"
+    const val versionCode = 53
+    const val versionName = "2.0.53"
 
     val javaVer = JavaVersion.VERSION_11
     const val javaVerString = "11"

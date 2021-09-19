1412b91af1867f0ae533a07700d85df7d3ce6bc8
keelim <kimh00335@gmail.com>
2021-09-20 01:07:50 +0900

    [modify] common module
    Former-commit-id: 8a43a1943f353b796845f98e221bfbc16e89831d

---
+++ common/src/main/java/com/keelim/common/di/CoroutinesQualifiers.kt
@@ -0,0 +1,20 @@
+package com.keelim.common.di
+
+import javax.inject.Qualifier
+
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainImmediateDispatcher

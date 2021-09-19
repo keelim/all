31cbdd446841621095a9b02aa766e1ca7db5ebd2
keelim <kimh00335@gmail.com>
2021-09-20 01:08:05 +0900

    [modify] app module
    Former-commit-id: 4ff926ddc29f1dc5c455d3aabe0920572b1b999f

---
+++ app/src/main/java/com/keelim/cnubus/di/CoroutinesModule.kt
@@ -0,0 +1,32 @@
+package com.keelim.cnubus.di
+
+import com.keelim.common.di.DefaultDispatcher
+import com.keelim.common.di.IoDispatcher
+import com.keelim.common.di.MainDispatcher
+import com.keelim.common.di.MainImmediateDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal object CoroutinesModule {
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @MainImmediateDispatcher
+    @Provides
+    fun providesMainImmediateDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
+}

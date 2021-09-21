84e5c78b74a2e27480ff03d29b368642a50eb13c
keelim <kimh00335@gmail.com>
2021-09-22 00:32:28 +0900

    [version] project version up
    Former-commit-id: be904921607fbe9c3f1b5ae03e15c5468c03ce01

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -6,8 +6,8 @@ object ProjectConfigurations {
     const val minSdk = 24
     const val targetSdk = 31
     const val buildTools = "30.0.3"
-    const val versionCode = 48
-    const val versionName = "1.0.48"
+    const val versionCode = 50
+    const val versionName = "1.0.50"
 
     val javaVer = JavaVersion.VERSION_11
     const val javaVerString = "1.8"

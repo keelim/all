d5e8a2d77b30efa224a34a389c18cee07ddffe88
keelim <kimh00335@gmail.com>
2021-10-13 22:44:56 +0900

    [release] apply lint
    Former-commit-id: c376a7b58a56f2227a807e22eb6f49f2de7fa60d

--- app/src/main/java/com/keelim/cnubus/ui/onBoarding/OnBoardingOneFragment.kt
+++ app/src/main/java/com/keelim/cnubus/ui/onBoarding/OnBoardingOneFragment.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed by 2021 keelim (Jaehyun Kim)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.keelim.cnubus.ui.onBoarding
 
 import android.content.Intent
@@ -10,15 +25,15 @@ import coil.load
 import com.keelim.cnubus.databinding.FragmentOnBoardingBinding
 import com.keelim.cnubus.ui.main.MainActivity
 
-
 class OnBoardingOneFragment(
-    private val section:Int,
+    private val section: Int,
     private val imageUrl: String,
 ) : Fragment() {
     private var _binding: FragmentOnBoardingBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
@@ -35,7 +50,7 @@ class OnBoardingOneFragment(
         _binding = null
     }
 
-    private fun initViews() = with(binding){
+    private fun initViews() = with(binding) {
         btnSkips.setOnClickListener {
             startActivity(Intent(requireActivity(), MainActivity::class.java))
         }
--- features/ui-setting/src/main/java/com/keelim/ui_setting/ui/Clock.kt
+++ features/ui-setting/src/main/java/com/keelim/ui_setting/ui/Clock.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed by 2021 keelim (Jaehyun Kim)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.keelim.ui_setting.ui
 
 import androidx.compose.animation.animateColorAsState
@@ -79,7 +94,6 @@ fun Clock(time: Time) {
 
         NumberColumn(time.seconds / 10, 0..5, padding)
         NumberColumn(time.seconds % 10, 0..9, padding)
-
     }
 }
 

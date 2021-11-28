03afb75ace0e5097482425810ae1856b605c22a7
keelim <kimh00335@gmail.com>
2021-11-28 21:58:08 +0900

    [UPDATE] UPDATE VERSION UP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.keelim.mygrade"
         minSdk = 24
         targetSdk = 31
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     val key: String = gradleLocalProperties(rootDir).getProperty("UNIT")

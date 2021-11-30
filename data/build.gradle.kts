e50a56f5493e98273d88a8ad8d87be2ed87cd506
keelim <kimh00335@gmail.com>
2021-11-30 12:33:06 +0900

    [UPDATE] UPDATE PROJECT SETTING
    Former-commit-id: ed15f0b89232a39e3c47bf58a416b784f36c7802

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -34,6 +34,16 @@ dependencies {
     androidTestImplementation(AppTest.espressoCore)
     androidTestImplementation(Coroutines.test)
     implementation(Play.maps_sdk)
+
+    implementation("androidx.room:room-runtime:2.3.0")
+    implementation("androidx.room:room-ktx:2.3.0")
+    kapt("androidx.room:room-compiler:2.3.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:27.1.0"))
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.0")
 }
 
 kapt {

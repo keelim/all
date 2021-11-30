13bef8cdbe3223550e4c25ba657b7adaba7c1b80
keelim <kimh00335@gmail.com>
2021-11-30 16:03:33 +0900

    [UPDATE] UPDATE SETTING MODULE TO COMPOSE
    Former-commit-id: 001b02984298cb49b25be0580a5053d7c262860d

---
+++ app/src/main/java/com/keelim/cnubus/ui/setting/compose/ScreenAction.kt
@@ -0,0 +1,12 @@
+package com.keelim.cnubus.ui.setting.compose
+
+internal enum class ScreenAction {
+    Homepage,
+    Map,
+    Update,
+    Theme,
+    OpenSource,
+    Lab,
+    Developer,
+    Subway
+}

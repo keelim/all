eb92baba3711d4c331e1037d513bc6e98d53ef40
keelim <kimh00335@gmail.com>
2021-12-13 15:15:59 +0900

    [UPDATE] UPDATE SEARCH FEATURE
    Former-commit-id: 956087b01e8d86e5389264f1d90fd836e284ce55

--- app/src/main/java/com/keelim/nandadiagnosis/ui/main/search/SearchFragment.kt
+++ app/src/main/java/com/keelim/nandadiagnosis/ui/main/search/SearchFragment.kt
@@ -24,7 +24,6 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -32,7 +31,6 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.selection.Selection
 import androidx.recyclerview.selection.SelectionPredicates
 import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.selection.StorageStrategy
@@ -44,6 +42,7 @@ import com.keelim.nandadiagnosis.ui.main.search.selection.MyItemDetailsLookup
 import com.keelim.nandadiagnosis.ui.main.search.selection.MyItemKeyProvider
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -62,7 +61,7 @@ class SearchFragment : Fragment() {
     },
     textSelectListener = {},
   )
-  private val searchRecyclerViewAdapter2 = SearchRecyclerViewAdapter2(
+  private val searchRecyclerViewAdapter3 = SearchRecyclerViewAdapter3(
     favoriteListener = { favorite, id ->
       favoriteUpdate(favorite, id)
     }
@@ -114,7 +113,7 @@ class SearchFragment : Fragment() {
     recyclerView.apply {
       setHasFixedSize(true)
       addItemDecoration(RecyclerViewDecoration(0, 10))
-      adapter = searchRecyclerViewAdapter2
+      adapter = searchRecyclerViewAdapter3
     }
     initTracker()
   }
@@ -137,6 +136,7 @@ class SearchFragment : Fragment() {
           saveSearchKeyword(query)
           Timber.d("데이터베이스 $query")
           viewModel.search2(query.replace("\\s", ""))
+          search(query.replace("\\s", ""))
           return true
         }
         override fun onQueryTextChange(newText: String): Boolean = true
@@ -157,23 +157,9 @@ class SearchFragment : Fragment() {
       .withSelectionPredicate(SelectionPredicates.createSelectAnything())
       .build()
 
-    searchRecyclerViewAdapter2.tracker = trackers
+    searchRecyclerViewAdapter3.tracker = trackers
     binding.floating.setOnClickListener {
-      multiSelection(trackers?.selection!!)
-    }
-  }
-
-  private fun multiSelection(selection: Selection<Long>) {
-    val list2 = selection
-      .map { searchRecyclerViewAdapter2.getItem(it.toInt()) }
-
-    if (list2.isEmpty()) {
-      Toast.makeText(requireActivity(), "데이터를 선택해주세요", Toast.LENGTH_SHORT).show()
-    } else {
-      shareInformation(
-        list2
-          .fold("") { acc, entity -> acc + entity.toString() }
-      )
+//      multiSelection(trackers?.selection!!)
     }
   }
 
@@ -215,7 +201,13 @@ class SearchFragment : Fragment() {
     if (state.searchList.isEmpty()) {
       Timber.d("결과 값이 비어 있습니다.")
     } else {
-      searchRecyclerViewAdapter2.submitList(state.searchList)
+//      searchRecyclerViewAdapter2.submitList(state.searchList)
+    }
+  }
+
+  private fun search(query:String) = viewLifecycleOwner.lifecycleScope.launch {
+    viewModel.getContent(query).collectLatest {
+      searchRecyclerViewAdapter3.submitData(it)
     }
   }
 }

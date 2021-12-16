de98068834d2c954a03aad5dc9952166ced10764
keelim <kimh00335@gmail.com>
2021-12-16 09:20:53 +0900

    [RELEASE] VERSION UP
    Former-commit-id: c9544420d51680b5a82ebb5caef5682332d3838f

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -5,6 +5,6 @@ object ProjectConfigurations {
     const val minSdk = 24
     const val targetSdk = 31
     const val buildTools = "30.0.3"
-    const val versionCode = 50
-    const val versionName = "1.0.50"
+    const val versionCode = 51
+    const val versionName = "1.0.51"
 }

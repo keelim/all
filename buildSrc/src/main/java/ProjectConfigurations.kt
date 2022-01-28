79cdef8b8b75fb560acd4ae7dd22a635df7567bd
keelim <kimh00335@gmail.com>
2022-01-28 22:48:23 +0900

    [RELEASE] VERSION UP 2104
    Former-commit-id: 3aad6510a958ec6d99303fb2d4596f963039eac6

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -7,8 +7,8 @@ ProjectConfigurations {
     const val minSdk = 24
     const val targetSdk = 31
     const val buildTools = "30.0.3"
-    const val versionCode = 2103
-    const val versionName = "2.1.03"
+    const val versionCode = 2104
+    const val versionName = "2.1.04"
 
     val javaVer = JavaVersion.VERSION_11
     const val javaVerString = "11"

7c85b194cacd1e33b4f11c658751039d37078630
keelim <kimh00335@gmail.com>
2022-01-28 20:31:08 +0900

    [FEAT] UPDATE CLOCK SCREEN
    Former-commit-id: e9fb853bbe3ab9a0e445b7baeb3606e9987ec54a

--- features/ui-setting/src/main/java/com/keelim/ui_setting/Clock.kt
+++ features/ui-setting/src/main/java/com/keelim/ui_setting/Clock.kt
@@ -17,21 +17,16 @@ package com.keelim.ui_setting
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import com.keelim.compose.ui.setThemeContent
 import com.keelim.ui_setting.ui.components.ClockScreen
+import com.keelim.ui_setting.ui.theme.CnubusTheme
 
 class ClockActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setThemeContent {
-            MaterialTheme {
-                Surface(
-                    color = MaterialTheme.colors.background
-                ) {
-                    ClockScreen()
-                }
+            CnubusTheme {
+                ClockScreen()
             }
         }
     }

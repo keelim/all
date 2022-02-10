98b8f3c9d4771e5af6c0133f550a9b31f562765d
keelim <kimh00335@gmail.com>
2022-02-10 09:42:16 +0900

    [UPDATE] UPDATE THE FEATURE DATASTORE AND LIBRARY VERSION
    Former-commit-id: 9b3ccc2c5952d9feef444896120d6b4937c8f39d

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
     val agpVersion = "7.2.0-beta01"
-    val kotlinVersion = "1.6.10"
+    val kotlinVersion = "1.5.31"
 
     repositories {
         gradlePluginPortal()
@@ -11,15 +11,15 @@ pluginManagement {
     plugins {
         id("com.android.application") version agpVersion
         id("com.android.library") version agpVersion
-        id("org.jetbrains.kotlin.android") version kotlinVersion
+        id("org.jetbrains.kotlin.android") version agpVersion
         id("org.jetbrains.kotlin.jvm") version kotlinVersion
         id("org.jetbrains.kotlin.kapt") version kotlinVersion
-        id("com.google.devtools.ksp") version "${kotlinVersion}-1.0.2"
+        id("com.google.devtools.ksp") version "${kotlinVersion}-1.0.0"
     }
     resolutionStrategy {
         eachPlugin {
             when (val id = requested.id.id) {
-                "dagger.hilt.android.plugin" -> useModule("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
+                "dagger.hilt.android.plugin" -> useModule("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
                 "org.jetbrains.kotlin.plugin.serialization" -> useModule("${id}:${id}.gradle.plugin:${kotlinVersion}")
             }
         }

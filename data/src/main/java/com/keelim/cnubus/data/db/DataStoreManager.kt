c8a8731c749a3d3eb0fe6ce86977b48e247a0490
keelim <kimh00335@gmail.com>
2022-02-11 10:25:34 +0900

    [UPDATE] UPDATE AND USED DATASTORE
    Former-commit-id: 183fbccc837687f78221cb44b10e954623a910b2

--- data/src/main/java/com/keelim/cnubus/data/db/DataStoreManager.kt
+++ data/src/main/java/com/keelim/cnubus/data/db/DataStoreManager.kt
@@ -1,11 +1,9 @@
 package com.keelim.cnubus.data.db
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.longPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.keelim.cnubus.data.db.DataStoreManager.PreferencesKeys.KEY_LAST_DATABASE_UPDATED_TIME_MILLIS
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
@@ -13,15 +11,14 @@ import kotlinx.coroutines.flow.map
 
 
 class DataStoreManager @Inject constructor(
-    private val ctx: Context
+    private val dataStore: DataStore<Preferences>
 ) {
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "preference")
-    fun getLong(): Flow<Long> = ctx.dataStore.data.map { preferences ->
+    fun getLong(): Flow<Long> = dataStore.data.map { preferences ->
         preferences[KEY_LAST_DATABASE_UPDATED_TIME_MILLIS] ?: 0L
     }
 
     suspend fun setLong(time: Long) {
-        ctx.dataStore.edit { preferences ->
+        dataStore.edit { preferences ->
             preferences[KEY_LAST_DATABASE_UPDATED_TIME_MILLIS] = time
         }
     }

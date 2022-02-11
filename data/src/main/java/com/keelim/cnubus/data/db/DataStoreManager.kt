71c912194e349036eaae0f41855cbb07174d0cb5
keelim <kimh00335@gmail.com>
2022-02-11 10:30:09 +0900

    [UPDATE] UPDATE THE DATASTORE MANAGER
    Former-commit-id: dd51d9342e589ef64884217a05f8e7742ced64df

--- data/src/main/java/com/keelim/cnubus/data/db/DataStoreManager.kt
+++ data/src/main/java/com/keelim/cnubus/data/db/DataStoreManager.kt
@@ -4,7 +4,6 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.longPreferencesKey
-import com.keelim.cnubus.data.db.DataStoreManager.PreferencesKeys.KEY_LAST_DATABASE_UPDATED_TIME_MILLIS
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -13,21 +12,15 @@ import kotlinx.coroutines.flow.map
 class DataStoreManager @Inject constructor(
     private val dataStore: DataStore<Preferences>
 ) {
-    fun getLong(): Flow<Long> = dataStore.data.map { preferences ->
-        preferences[KEY_LAST_DATABASE_UPDATED_TIME_MILLIS] ?: 0L
+    fun getLong(keyStone: String): Flow<Long> = dataStore.data.map { preferences ->
+        val key = longPreferencesKey(keyStone)
+        preferences[key] ?: 0L
     }
 
-    suspend fun setLong(time: Long) {
+    suspend fun setLong(keyStone: String, time: Long) {
+        val key = longPreferencesKey(keyStone)
         dataStore.edit { preferences ->
-            preferences[KEY_LAST_DATABASE_UPDATED_TIME_MILLIS] = time
+            preferences[key] = time
         }
-    }
-
-    private object PreferencesKeys {
-        val KEY_LAST_DATABASE_UPDATED_TIME_MILLIS = longPreferencesKey(LAST_DATABASE_ACCESS_KEY)
-    }
-
-    companion object {
-        private const val LAST_DATABASE_ACCESS_KEY = "KEY_LAST_DATABASE_UPDATED_TIME_MILLIS"
     }
 }

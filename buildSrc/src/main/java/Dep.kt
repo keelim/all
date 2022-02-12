511d1ed60eaad865105d6a724bf00fd6d10fc6b1
keelim <kimh00335@gmail.com>
2022-02-13 01:31:49 +0900

    [UPDATE] UPDATE THE TEST CODE
    Former-commit-id: 6cea06dbb2123791ae02fa65e4e8e073309c2c73

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -148,6 +148,7 @@ object Dep {
         object Hilt {
             const val android = "com.google.dagger:hilt-android:$version"
             const val compiler = "com.google.dagger:hilt-compiler:$version"
+            const val test = "com.google.dagger:hilt-android-testing:$version"
         }
     }
 

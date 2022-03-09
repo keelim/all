2465b5e01f1defe560e283d498ffaf74f2d519f6
keelim <kimh00335@gmail.com>
2022-03-09 22:42:18 +0900

    [UPDATE] UPDATE THE CUSTOM VIEW
    Former-commit-id: 39766bd12a54c45d2eba5408e06ff77b7665d58b

--- app/src/main/java/com/keelim/cnubus/ui/setting/LabActivity.kt
+++ app/src/main/java/com/keelim/cnubus/ui/setting/LabActivity.kt
@@ -18,8 +18,9 @@ package com.keelim.cnubus.ui.setting
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.keelim.cnubus.databinding.ActivityLabBinding
+import com.keelim.common.extensions.toast
 
-class LabActivity() : AppCompatActivity() {
+class LabActivity : AppCompatActivity() {
     private val binding by lazy { ActivityLabBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +29,14 @@ class LabActivity() : AppCompatActivity() {
     }
 
     private fun initViews() = with(binding) {
-        root.setOnClickListener {
-            binding.ratingView.upTheRating()
+        btnStar.setOnClickListener {
+            var selected = ratingView.selected
+            toast(selected.toString())
+            selected += (selected + 1)
+            if (selected > 4) {
+                selected = 0
+            }
+            ratingView.selected = selected
         }
     }
 }

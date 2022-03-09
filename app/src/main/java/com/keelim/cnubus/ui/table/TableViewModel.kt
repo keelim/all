88e1cb77623bd756e6ca8cacec4208767a5e86a1
keelim <kimh00335@gmail.com>
2022-03-09 14:27:21 +0900

    [UPDATE] UPDATE THE TABLE FRAGMENT AND VIEWPAGER
    Former-commit-id: 375584b0e66462f1ae5d9d71be37cc5556908f80

---
+++ app/src/main/java/com/keelim/cnubus/ui/table/TableViewModel.kt
@@ -0,0 +1,8 @@
+package com.keelim.cnubus.ui.table
+
+import com.keelim.common.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class TableViewModel: BaseViewModel() {
+}

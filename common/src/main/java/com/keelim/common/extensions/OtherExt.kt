e6b2e88b6f6aaa622d3fc2fe3c1e9434ad16d6f3
keelim <kimh00335@gmail.com>
2022-03-13 15:40:36 +0900

    [UPDATE] UPDATE THE WORKMANAGER AND COROUTINE SCOPE
    Former-commit-id: 88bf86844c9989b0e89dac46af5ad2cbff86267f

--- common/src/main/java/com/keelim/common/extensions/OtherExt.kt
+++ common/src/main/java/com/keelim/common/extensions/OtherExt.kt
@@ -14,15 +14,3 @@
  * limitations under the License.
  */
 package com.keelim.common.extensions
-
-import android.content.Context
-import android.widget.Toast
-import androidx.annotation.StringRes
-
-fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, message, duration).show()
-}
-
-fun Context.toast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(this, this.resources.getText(resId), duration).show()
-}

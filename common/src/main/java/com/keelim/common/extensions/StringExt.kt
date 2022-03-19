4a221680cf5e1c71770bdb57145f2c22a87f56a0
keelim <kimh00335@gmail.com>
2022-03-19 23:19:39 +0900

    [UPDATE] UPDATE EXTENSION
    Former-commit-id: 03ea72395f3446d4cc22a7cacc94df49ce03d99b

--- common/src/main/java/com/keelim/common/extensions/StringExt.kt
+++ common/src/main/java/com/keelim/common/extensions/StringExt.kt
@@ -14,3 +14,13 @@
  * limitations under the License.
  */
 package com.keelim.common.extensions
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+fun String.toDate(dateFormat: String = "yyyy-MM-dd'T'HH:mm:ss", tz: TimeZone = TimeZone.getDefault()): Date? {
+    val parser = SimpleDateFormat(dateFormat, Locale.getDefault()).apply { timeZone = tz }
+    return parser.parse(this)
+}

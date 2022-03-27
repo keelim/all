ea2c1e4f392147ac3b36650733dc80fddb6fe691
keelim <kimh00335@gmail.com>
2022-03-28 00:11:02 +0900

    [VERSION] VERSION UP 2112
    Former-commit-id: dded0d6cae1931e3cad6a2691f854a8e2ef7f33e

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -7,8 +7,8 @@ ProjectConfigurations {
     const val minSdk = 24
     const val targetSdk = 31
     const val buildTools = "30.0.3"
-    const val versionCode = 2110
-    const val versionName = "2.1.10"
+    const val versionCode = 2111
+    const val versionName = "2.1.11"
 
     val javaVer = JavaVersion.VERSION_11
     const val javaVerString = "11"

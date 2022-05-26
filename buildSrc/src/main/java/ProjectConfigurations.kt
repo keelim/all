abd2a4bdecd594f602a1618a3897feac27d63ef5
keelim <kimh00335@gmail.com>
2022-05-26 22:55:06 +0900

    version up
    Former-commit-id: 2c6ad93f5d427e9d00a358ffde7ea69460762318

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -6,7 +6,7 @@ object ProjectConfigurations {
     const val targetSdk = 31
     const val buildTools = "31.0.0"
     const val applicationID = "com.keelim.mygrade"
-    const val versionCode = 4
-    const val versionName = "0.0.4"
+    const val versionCode = 9
+    const val versionName = "0.0.9"
     val javaVer = JavaVersion.VERSION_11
 }

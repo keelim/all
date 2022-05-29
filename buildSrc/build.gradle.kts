7f82bc47b90097a2f541e0ff4e7bcfdaf64f99ae
keelim <kimh00335@gmail.com>
2022-05-29 15:16:09 +0900

    add library
    Former-commit-id: f89827d761178b59ac70e6bfd9ab061dcf4afbcb

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,4 +10,5 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:7.1.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+    implementation("com.squareup:javapoet:1.13.0")
 }

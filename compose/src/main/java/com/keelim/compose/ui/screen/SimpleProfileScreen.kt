dba9a1e37e5d38edb6c414121690bff5ff537493
keelim <kimh00335@gmail.com>
2022-05-29 17:02:48 +0900

    spotless update
    Former-commit-id: 46cdeca9366279ac3899d8f9f8b0a3ecfbea70b3

--- compose/src/main/java/com/keelim/compose/ui/screen/SimpleProfileScreen.kt
+++ compose/src/main/java/com/keelim/compose/ui/screen/SimpleProfileScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed by 2021 keelim (Jaehyun Kim)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.keelim.compose.ui.screen
 
 import androidx.compose.foundation.BorderStroke
@@ -39,7 +54,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.keelim.compose.R
-
 
 @Composable
 fun ImageProfile(modifier: Modifier = Modifier) {
@@ -115,9 +129,11 @@ fun ProfileCard() {
                 ImageProfile()
                 Divider()
                 Info()
-                Button(onClick = {
-                    isClicked.value = !isClicked.value
-                }) {
+                Button(
+                    onClick = {
+                        isClicked.value = !isClicked.value
+                    }
+                ) {
                     Text(
                         text = "Portfolio",
                         style = MaterialTheme.typography.button
@@ -141,9 +157,11 @@ fun PreviewProfileCard() {
 
 @Composable
 fun Content() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .padding(5.dp)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
         Surface(
             modifier = Modifier
                 .padding(3.dp)
@@ -186,9 +204,10 @@ fun Portfolio(data: List<String>) {
                         .padding(7.dp)
                 ) {
                     ImageProfile(modifier = Modifier.size(50.dp))
-                    Column(modifier = Modifier
-                        .padding(7.dp)
-                        .align(alignment = CenterVertically)
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = CenterVertically)
                     ) {
                         Text(text = item, fontWeight = FontWeight.Bold)
                         Text(text = "Good Project", style = MaterialTheme.typography.h2)

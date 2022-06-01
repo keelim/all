13226186b7fedae70ab980b6d214607869f36288
keelim <kimh00335@gmail.com>
2022-06-01 23:56:01 +0900

    update root viewmodel
    Former-commit-id: 6bb87a9625b4e340f96531d5a4b3eb2dc9725333

--- app/src/main/java/com/keelim/cnubus/ui/root/basic/RootViewModel.kt
+++ app/src/main/java/com/keelim/cnubus/ui/root/basic/RootViewModel.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.keelim.cnubus.ui.root
+package com.keelim.cnubus.ui.root.basic
 
 import androidx.lifecycle.viewModelScope
 import com.keelim.cnubus.data.db.entity.History
@@ -36,7 +36,7 @@ import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
 @HiltViewModel
-class RootCommonViewModel @Inject constructor(
+class RootViewModel @Inject constructor(
     private val stationRepository: StationRepository,
     private val userUseCase: UserUseCase,
 ) : BaseViewModel() {

ef1a6cc187be1d2e3584940633dcbc1a1fe4b670
keelim <kimh00335@gmail.com>
2022-06-09 01:52:30 +0900

    change moshi to kotlin serializable
    Former-commit-id: f60cfa8cde5ecb9d79f903e529f66745c96bfad0

--- data/src/main/java/com/keelim/data/api/ApiRequestFactory.kt
+++ data/src/main/java/com/keelim/data/api/ApiRequestFactory.kt
@@ -1,23 +1,20 @@
 package com.keelim.data.api
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import javax.inject.Inject
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 
 class ApiRequestFactory @Inject constructor(
 ) {
     private val baseUrl = "https://sample.com"
-    private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
 
     val retrofit: ApiService = Retrofit.Builder()
         .baseUrl(baseUrl)
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaTypeOrNull()!!))
         .client(
             OkHttpClient.Builder()
                 .addInterceptor(

bfccd5b88a1de6e3e76c27eec632ce81f950372b
keelim <kimh00335@gmail.com>
2022-06-09 01:52:30 +0900

    change moshi to kotlin serializable
    Former-commit-id: c83f15c841cf963fa831b1e536cd3994cf41c828

--- data/src/main/java/com/keelim/data/api/response/ResponseNotification.kt
+++ data/src/main/java/com/keelim/data/api/response/ResponseNotification.kt
@@ -2,17 +2,12 @@ package com.keelim.data.api.response
 
 
 import android.os.Parcelable
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
-@JsonClass(generateAdapter = true)
+@kotlinx.serialization.Serializable
 @Parcelize
 data class ResponseNotification(
-    @Json(name = "majorDimension")
     var majorDimension: String?, // ROWS
-    @Json(name = "range")
     var range: String?, // main!A1:Z1000
-    @Json(name = "values")
     var values: List<List<String>>
 ) : Parcelable

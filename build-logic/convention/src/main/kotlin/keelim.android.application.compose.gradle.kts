75f3523b994378289e6653fa775d388a206544fb
keelim <kimh00335@gmail.com>
2022-06-09 01:51:27 +0900

    add build-logic
    Former-commit-id: 85bbf349c62e1157c024b50e109886e1c51ddcf5

---
+++ build-logic/convention/src/main/kotlin/keelim.android.application.compose.gradle.kts
@@ -0,0 +1,10 @@
+
+import com.keelim.builds.configureAndroidCompose
+
+plugins {
+    id("com.android.application")
+}
+
+android {
+    configureAndroidCompose(this)
+}
--- build-logic/convention/src/main/kotlin/keelim.android.library.gradle.kts
+++ build-logic/convention/src/main/kotlin/keelim.android.library.gradle.kts
@@ -1,11 +1,11 @@
 /*
- * Designed and developed by 2021 keelim (Jaehyun Kim)
+ * Copyright 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,16 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.keelim.compose.screen;
 
-internal enum class SettingAction {
-    Content,
-    Homepage,
-    Map,
-    Update,
-    Theme,
-    OpenSource,
-    Lab,
-    Developer,
-    Subway
+import com.keelim.builds.ProjectConfigurations
+import com.keelim.builds.configureKotlinAndroid
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    configureKotlinAndroid(this)
+
+    defaultConfig {
+        targetSdk = ProjectConfigurations.targetSdk
+    }
 }

9397209b090abac78f3029d9cf24e0dce96481fa
keelim <kimh00335@gmail.com>
2022-06-09 01:50:51 +0900

    delete buildSrc
    Former-commit-id: 2eddd666e428f8309b4e0ae55332d0bc716d5074

--- buildSrc/src/main/java/compose-plugin.gradle.kts
+++
@@ -1,9 +0,0 @@
-import com.android.build.gradle.BaseExtension
-
-configure<BaseExtension> {
-    buildFeatures.compose = true
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.1.0"
-    }
-}

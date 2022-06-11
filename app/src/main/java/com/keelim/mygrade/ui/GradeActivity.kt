7a945f314d9badd604c0ecea03e4030664b88477
keelim <kimh00335@gmail.com>
2022-06-11 11:34:33 +0900

    update grade screen
    Former-commit-id: ba53489b797f38295a4e0e3be1614bb9b1f9778e

--- app/src/main/java/com/keelim/mygrade/ui/GradeActivity.kt
+++ app/src/main/java/com/keelim/mygrade/ui/GradeActivity.kt
@@ -2,32 +2,62 @@ package com.keelim.mygrade.ui
 
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.os.Bundle
-import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import androidx.core.view.drawToBitmap
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
-import com.keelim.mygrade.BuildConfig
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import com.keelim.data.db.entity.SimpleHistory
 import com.keelim.data.model.Result
+import com.keelim.data.repository.IoRepository
 import com.keelim.mygrade.R
 import com.keelim.mygrade.databinding.ActivityGradeBinding
-import com.keelim.mygrade.ui.center.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 import java.io.File
 import java.io.FileOutputStream
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
+@HiltViewModel
+class GradeViewModel @Inject constructor(
+    private val ioRepository: IoRepository,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(GradeUiState())
+    val uiState: StateFlow<GradeUiState> = _uiState.asStateFlow()
+
+    fun saveSimpleHistory(grade: String, rank: String) = viewModelScope.launch {
+        ioRepository.insertSimpleHistories(
+            SimpleHistory(
+                grade = grade,
+                rank = rank
+            ))
+    }
+
+    fun changeSaveAction(isSave: Boolean) {
+        _uiState.update { it.copy(isSave = isSave) }
+    }
+    data class GradeUiState(
+        val isSave: Boolean = false,
+    )
+}
 
 @AndroidEntryPoint
 class GradeActivity : AppCompatActivity() {
     private val data: Result? by lazy { intent.getParcelableExtra("data") }
-    private val viewModel by viewModels<MainViewModel>()
     private lateinit var binding: ActivityGradeBinding
+    private val viewModel by viewModels<GradeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,19 +65,31 @@ class GradeActivity : AppCompatActivity() {
             this,
             R.layout.activity_grade
         ).apply {
-            grade.text = data?.grade.orEmpty()
-            level.text = data?.point.orEmpty()
+            val (_grade, _level) = data?.grade.orEmpty() to data?.point.orEmpty()
+            grade.text = _grade
+            level.text = _level
             btnCopy.setOnClickListener {
                 saveAndCopy()
+            }
+            btnSave.setOnClickListener {
+                viewModel.saveSimpleHistory(_grade, _level)
+                viewModel.changeSaveAction(true)
             }
         }.also {
             binding = it
         }
+        initFlow()
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        finish()
+    private fun initFlow(){
+        viewModel.uiState
+            .flowWithLifecycle(lifecycle)
+            .onEach { state ->
+                if(state.isSave){
+                    snack(binding.root, "저장이 완료되었습니다. ")
+                }
+            }
+            .launchIn(lifecycleScope)
     }
 
     private fun saveAndCopy() {

31b460ec6586407b09172ac3d1aa0121936976df
keelim <kimh00335@gmail.com>
2022-06-11 11:43:25 +0900

    update simple hsitory
    Former-commit-id: 5814895154f7d9c65bde8b5203a14556e4662b7d

--- data/src/main/java/com/keelim/data/db/entity/SimpleHistory.kt
+++ data/src/main/java/com/keelim/data/db/entity/SimpleHistory.kt
@@ -3,11 +3,10 @@ package com.keelim.data.db.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.Calendar
-import java.util.Date
 
 @Entity(tableName = "simple_history")
 data class SimpleHistory(
-    val date: Date = Calendar.getInstance().time,
+    val date: String = Calendar.getInstance().time.toString(),
     val grade: String,
     val rank: String,
     @PrimaryKey(autoGenerate = true)

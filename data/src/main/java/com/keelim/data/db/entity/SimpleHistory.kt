d65d9726d1de4d76db1c56e4ea6a4b993a6893d4
keelim <kimh00335@gmail.com>
2022-06-11 11:34:33 +0900

    update grade screen
    Former-commit-id: 47376043287a273faf12bb270c24c33a2822769c

--- data/src/main/java/com/keelim/data/db/entity/SimpleHistory.kt
+++ data/src/main/java/com/keelim/data/db/entity/SimpleHistory.kt
@@ -2,11 +2,12 @@ package com.keelim.data.db.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.Calendar
 import java.util.Date
 
 @Entity(tableName = "simple_history")
 data class SimpleHistory(
-    val date: Date,
+    val date: Date = Calendar.getInstance().time,
     val grade: String,
     val rank: String,
     @PrimaryKey(autoGenerate = true)

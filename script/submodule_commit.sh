a21ab3e08417164585d0398c6d70c463bbcb90ec
keelim <kimh00335@gmail.com>
2022-06-18 17:22:02 +0900

    update script
    Former-commit-id: c4243a5d283dc2fac65c921e0c4f3dff632da7c4

--- script/submodule_commit.sh
+++ script/submodule_commit.sh
@@ -1,4 +1,7 @@
-git submodule foreach git checkout master
+git submodule foreach git checkout -b newfeature
 git submodule foreach git add -A .
 git submodule foreach git commit -am "update common module"
+git submodule foreach git checkout master
+git submodule foreach git merge newfeature
 git submodule foreach git push
+git submodule foreach git branch -d newfeature

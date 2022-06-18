746c855e942115e4b098797679b45e2b5db1e096
keelim <kimh00335@gmail.com>
2022-06-18 17:12:52 +0900

    update submodule script
    Former-commit-id: 99702458f3e9737eeb14b223457b9d1fefe89f7b

--- script/submodule_commit.sh
+++ script/submodule_commit.sh
@@ -1,5 +1,4 @@
 git submodule foreach git checkout -b newfeature
-git submodule update --remote
 git submodule foreach git add -A .
 git submodule foreach git commit -am "update common module"
 git submodule foreach git checkout master

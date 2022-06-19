507e287b70a6ae426e24e032e80865ac0a2a72b1
keelim <kimh00335@gmail.com>
2022-06-20 00:35:12 +0900

    update spotless
    Former-commit-id: 836c7fca6793ed74249f9430c798466b1a7aaa27

--- app/src/main/java/com/keelim/cnubus/ui/root/swipe/SwipeActivity.kt
+++ app/src/main/java/com/keelim/cnubus/ui/root/swipe/SwipeActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Designed and developed by 2021 keelim (Jaehyun Kim)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.keelim.cnubus.ui.root.swipe
 
 import android.os.Bundle
@@ -19,7 +34,6 @@ class SwipeActivity : AppCompatActivity() {
             this,
             R.layout.activity_swipe
         ).apply {
-
         }.also {
             binding = it
         }
@@ -27,18 +41,14 @@ class SwipeActivity : AppCompatActivity() {
         initData()
     }
 
-    private fun initFlow(){
-
+    private fun initFlow() {
     }
-    private fun initData(){
-
+    private fun initData() {
     }
 
     private fun handleMigrateSuccess(data: List<Location>) {
-        if(data.isEmpty()) {
-
+        if (data.isEmpty()) {
         } else {
-
         }
     }
 }

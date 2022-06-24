3f5061a8595b3396eccdc32db6dfcbcfa9310dfa
Keel_im <kimh00335@gmail.com>
2022-06-25 01:33:25 +0900

    version up
    Former-commit-id: fc9cff367f874a581bbc3e896821814c4fdf42e4

--- build-logic/convention/src/main/kotlin/com/keelim/builds/ProjectConfigurations.kt
+++ build-logic/convention/src/main/kotlin/com/keelim/builds/ProjectConfigurations.kt
@@ -8,7 +8,7 @@ object ProjectConfigurations {
     const val targetSdk = 32
     const val buildTools = "31.0.0"
     const val applicationID = "com.keelim.mygrade"
-    const val versionCode = 12
-    const val versionName = "0.0.12"
+    const val versionCode = 13
+    const val versionName = "0.0.13"
     val javaVer = JavaVersion.VERSION_11
 }

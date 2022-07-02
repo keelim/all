47a4d0e851a6ed02774e8c48b3be26bb95328e02
keelim <kimh00335@gmail.com>
2022-07-02 13:21:23 +0900

    update inappupdate code
    Former-commit-id: 49641f06ba1397e5ec8de561b66cb4da898b0c9a

--- app/src/main/java/com/keelim/mygrade/MyApplication.kt
+++ app/src/main/java/com/keelim/mygrade/MyApplication.kt
@@ -1,10 +1,13 @@
 package com.keelim.mygrade
 
 import android.app.Application
-import com.google.android.gms.ads.MobileAds
 import com.google.android.material.color.DynamicColors
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import com.keelim.mygrade.notification.NotificationChannels
 import com.keelim.mygrade.utils.AppOpenManager
+import com.keelim.mygrade.utils.Keys
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -16,6 +19,11 @@ class MyApplication: Application() {
         super.onCreate()
 //        MobileAds.initialize(this)
 //        appOpenManager.initialize(this)
+        val remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = Keys.fetchTime
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
         NotificationChannels.initialize(this)
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
---
+++ app/src/main/java/com/keelim/mygrade/utils/Keys.kt
@@ -0,0 +1,6 @@
+package com.keelim.mygrade.utils
+
+object Keys {
+    const val IN_APP_UPDATE_REQUEST_CODE = 1
+    const val fetchTime: Long = 3600L
+}

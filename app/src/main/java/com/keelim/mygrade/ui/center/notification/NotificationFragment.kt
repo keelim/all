7fa6942ccb0a4ba9599d153c5f021df4594dab1e
keelim <kimh00335@gmail.com>
2022-07-24 23:14:24 +0900

    update data layer
    - add notion interceptor
    - update repository to flow pattern
    
    Former-commit-id: 7db2598f6274dbaf1803a2ea3341d57293712cf6

--- app/src/main/java/com/keelim/mygrade/ui/center/notification/NotificationFragment.kt
+++ app/src/main/java/com/keelim/mygrade/ui/center/notification/NotificationFragment.kt
@@ -22,31 +22,33 @@ import com.keelim.mygrade.databinding.FragmentNotificationBinding
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class NotificationViewModel @Inject constructor(
     private val ioRepository: IoRepository,
 ) : ViewModel() {
-    private var _state: MutableStateFlow<NotificationState> =
-        MutableStateFlow(NotificationState.UnInitialized)
-    val state: StateFlow<NotificationState> = _state
-
-    init {
-        viewModelScope.launch {
-            _state.emit(NotificationState.Loading)
-            runCatching {
-                ioRepository.getNotification()
-            }.onSuccess {
-                _state.emit(NotificationState.Success(it))
-            }.onFailure {
-                _state.emit(NotificationState.Error("에러가 발생하였습니다. 다시 시도해주세요."))
+    val state: StateFlow<NotificationState> = ioRepository.getNotification()
+        .map {
+            if (it.isEmpty()) {
+                NotificationState.Loading
+            } else {
+                NotificationState.Success(it)
             }
-        }
-    }
+        }.catch {
+            NotificationState.Error("에러가 발생했습니다. 잠시 후 다시 시도해주세요.")
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000L),
+            initialValue = NotificationState.UnInitialized
+        )
 }
+
 
 @AndroidEntryPoint
 class NotificationFragment : Fragment() {

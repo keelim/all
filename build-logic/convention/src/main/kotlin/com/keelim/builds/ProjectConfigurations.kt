5b5e8a4b31957674e3787fa6e5682c1972990825
Keel_im <kimh00335@gmail.com>
2022-08-15 16:37:15 +0900

    Update ProjectConfigurations.kt
    Former-commit-id: 2abb64cd4ea6a8489afff656f9904d76488c3da5

--- build-logic/convention/src/main/kotlin/com/keelim/builds/ProjectConfigurations.kt
+++ build-logic/convention/src/main/kotlin/com/keelim/builds/ProjectConfigurations.kt
@@ -8,7 +8,7 @@ object ProjectConfigurations {
     const val targetSdk = 33
     const val buildTools = "31.0.0"
     const val applicationID = "com.keelim.mygrade"
-    const val versionCode = 17
-    const val versionName = "0.0.17"
+    const val versionCode = 18
+    const val versionName = "0.0.18"
     val javaVer = JavaVersion.VERSION_11
 }

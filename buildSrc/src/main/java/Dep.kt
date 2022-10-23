5d43da5f518ae781ddb247633fcfd7cfb914b7d1
Keel_im <kimh00335@gmail.com>
2022-10-24 00:19:09 +0900

    update library
    Former-commit-id: f9f2694da6ddf3710e439150617a82e5945743d4

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -37,7 +37,7 @@ object SquareUp {
 }
 
 object Room {
-    private const val roomVersion = "2.3.0"
+    private const val roomVersion = "2.4.3"
     const val runtime = "androidx.room:room-runtime:$roomVersion"
     const val compiler = "androidx.room:room-compiler:$roomVersion"
     const val ktx = "androidx.room:room-ktx:$roomVersion"

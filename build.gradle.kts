44023c159011ac8b04cbfb24883ae7d9b8870af1
keelim <kimh00335@gmail.com>
2022-10-24 23:23:06 +0900

    [delete] oss lincense
    Former-commit-id: f9d4c529d443a57fdf870cd1ab99b7401f835578

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
         classpath("com.diffplug.spotless:spotless-plugin-gradle:6.1.2")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")

3b0942c14bbf8d8ca11a7ea94d36ab26f4204204
keelim <kimh00335@gmail.com>
2022-10-25 00:54:41 +0900

    [update] 24 <= android <= 33
    Former-commit-id: 28d432c523764bdb517e98e8e5a2f12226968b12

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -2,9 +2,9 @@ import org.gradle.api.JavaVersion
 
 object ProjectConfigurations {
     const val applicationId = "com.keelim.comssa"
-    const val compileSdk = 31
+    const val compileSdk = 33
     const val minSdk = 24
-    const val targetSdk = 31
+    const val targetSdk = 33
     const val buildTools = "32.0.0-rc-1"
     const val versionCode = 6
     const val versionName = "1.0.6"

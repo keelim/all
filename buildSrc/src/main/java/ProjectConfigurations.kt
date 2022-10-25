eaaeeaba4829a4e494306a15b0f0fe4a37fbcfb5
keelim <kimh00335@gmail.com>
2022-10-26 01:04:28 +0900

    udpate app version
    Former-commit-id: 366d5584dbb86753f3922de722c5a5e3e0d3f595

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -6,8 +6,8 @@ object ProjectConfigurations {
     const val minSdk = 24
     const val targetSdk = 33
     const val buildTools = "32.0.0-rc-1"
-    const val versionCode = 6
-    const val versionName = "1.0.6"
+    const val versionCode = 7
+    const val versionName = "1.0.7"
 
     val javaVer = JavaVersion.VERSION_11
     const val javaVerString = "1.8"

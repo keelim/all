c06d0431607ded6f869b8021589d2ffad537f326
keelim <kimh00335@gmail.com>
2022-11-21 02:52:51 +0900

    build.gradle to plugin dsl
    Former-commit-id: 51a0a03ea45e6dfdfaf1998e5cd1da2afd98c7f9

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     `kotlin-dsl`
 }
 
+group = "com.keelim.buildSrc"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 repositories {
     google()
     mavenCentral()
@@ -11,4 +18,13 @@ dependencies {
     implementation("com.android.tools.build:gradle:7.3.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     implementation("com.squareup:javapoet:1.13.0")
+}
+
+gradlePlugin {
+    plugins {
+        register("firebase-set") {
+            id = "keelim.firebase-set"
+            implementationClass = "FirebaseSetConventionPlugin"
+        }
+    }
 }

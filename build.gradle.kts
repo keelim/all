e5ced3dd977841c707eb39c2b5df3738c8db6b57
Keel_im <kimh00335@gmail.com>
2022-11-20 11:35:00 +0900

    remove  spotless
    Former-commit-id: 6f723829e8bb9c0fc4ce670a9a0ec28546b04d05

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ buildscript {
         classpath("com.google.gms:google-services:4.3.10")
         classpath("com.google.firebase:perf-plugin:1.4.1")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.0")
-        classpath("com.diffplug.spotless:spotless-plugin-gradle:6.7.2")
         classpath("com.google.firebase:firebase-appdistribution-gradle:3.0.2")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.5")

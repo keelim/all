3356dcafe4d3755b3ee3499da405ab91fe8908b8
keelim <kimh00335@gmail.com>
2022-11-22 00:15:23 +0900

    update import
    Former-commit-id: 35fbd013fddfa9239685702b08997fadd9faabfd

--- app/src/main/java/com/keelim/cnubus/initialize/ComposeInitializer.kt
+++ app/src/main/java/com/keelim/cnubus/initialize/ComposeInitializer.kt
@@ -1,5 +1,10 @@
 package com.keelim.cnubus.initialize
 
+import android.content.Context
+import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.ProcessLifecycleInitializer
+import androidx.startup.Initializer
+
 class ComposeInitializer : Initializer<Unit> {
     override fun create(context: Context) {
         ComposeView(context)

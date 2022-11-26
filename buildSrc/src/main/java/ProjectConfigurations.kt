dbcc359ddab9a4886e0ed14efc8036f9b6d36da8
keelim <kimh00335@gmail.com>
2022-11-27 02:07:42 +0900

    update project version
    Former-commit-id: 7730c9054e24eee4b356b156dff75ed1b01ef0e6

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -6,8 +6,8 @@ object ProjectConfigurations {
     const val minSdk = 23
     const val targetSdk = 33
     const val buildTools = "32.0.0-rc-1"
-    const val versionCode = 7
-    const val versionName = "1.0.7"
+    const val versionCode = 8
+    const val versionName = "1.0.8"
 
     val javaVer = JavaVersion.VERSION_11
     const val javaVerString = "1.8"

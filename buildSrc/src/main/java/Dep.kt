b8b0a90863fc47bd086d06a70f71c7e2643427fa
keelim <kimh00335@gmail.com>
2022-11-27 04:11:58 +0900

    update dependencies
    Former-commit-id: e05af4d4913f0018eeaea227b414126d78d8cb59

--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -35,7 +35,7 @@ object SquareUp {
 }
 
 object Room {
-  private const val roomVersion = "2.5.0"
+  private const val roomVersion = "2.4.3"
   const val runtime = "androidx.room:room-runtime:$roomVersion"
   const val compiler = "androidx.room:room-compiler:$roomVersion"
   const val ktx = "androidx.room:room-ktx:$roomVersion"

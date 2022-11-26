da1fe6ad65f0150c33bbde1f321a5307c53e2b70
keelim <kimh00335@gmail.com>
2022-11-27 04:16:15 +0900

    add mypage2
    Former-commit-id: 276e6dcb26139c3786cbee768288d2a9bcc8bb65

--- app/src/main/java/com/keelim/comssa/ui/mypage/MyPageFragment2.kt
+++ app/src/main/java/com/keelim/comssa/ui/mypage/MyPageFragment2.kt
@@ -1,7 +1,11 @@
 package com.keelim.comssa.ui.mypage
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.fragment.app.Fragment
 import com.keelim.comssa.R
 import com.keelim.comssa.databinding.FragmentComposeViewBinding
@@ -12,6 +16,26 @@ class MyPageFragment2 : Fragment(R.layout.fragment_compose_view) {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     binding = FragmentComposeViewBinding.bind(view)
-    binding.composeView.setContent {}
+    binding.composeView.setContent {
+      MaterialTheme {
+        Surface {
+          MyPageScreen(
+            sectionClick = { packageName ->
+              runCatching {
+                  requireActivity()
+                    .packageManager
+                    .getLaunchIntentForPackage(packageName)
+                    ?.let(::startActivity)
+                }
+                .onFailure {
+                  startActivity(
+                    Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName"))
+                  )
+                }
+            }
+          )
+        }
+      }
+    }
   }
 }

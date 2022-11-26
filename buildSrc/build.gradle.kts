42a6f7ec0a2f16007845431f885c1e8cf8d71dfe
keelim <kimh00335@gmail.com>
2022-11-27 03:48:36 +0900

    update dependecies
    Former-commit-id: 3de312c511011a65dd28efcda98fa8c477667f58

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,20 +1,18 @@
-plugins {
-    `kotlin-dsl`
-}
+plugins { `kotlin-dsl` }
 
 group = "com.keelim.buildSrc"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
 }
 
 repositories {
-    google()
-    mavenCentral()
+  google()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.3.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+  implementation("com.android.tools.build:gradle:7.3.1")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
 }

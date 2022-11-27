a6e8a9d6d0c0d80ea737f3df5f2afc7b10a1ff3b
keelim <kimh00335@gmail.com>
2022-11-27 14:05:24 +0900

    update home fragment
    Former-commit-id: ba98b76ef41c10c9131284569651e62b64b23655

--- app/src/main/java/com/keelim/comssa/ui/home/HomeFragment.kt
+++ app/src/main/java/com/keelim/comssa/ui/home/HomeFragment.kt
@@ -21,6 +21,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.keelim.comssa.databinding.FragmentHomeBinding
@@ -30,110 +31,101 @@ import com.keelim.comssa.extensions.toVisible
 import com.keelim.comssa.ui.home.HomeAdapter.Companion.ITEM_VIEW_TYPE_FEATURED
 import com.keelim.comssa.ui.home.HomeAdapter.Companion.ITEM_VIEW_TYPE_SECTION_HEADER
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-    private val viewModel: HomeViewModel by viewModels()
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
-    private val homeAdapter = HomeAdapter(
-        onDataClickListener = {
-        }
-    )
+  private val viewModel: HomeViewModel by viewModels()
+  private var _binding: FragmentHomeBinding? = null
+  private val binding
+    get() = _binding!!
+  private val homeAdapter = HomeAdapter(onDataClickListener = {})
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentHomeBinding.inflate(layoutInflater)
-        return binding.root
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    _binding = FragmentHomeBinding.inflate(layoutInflater)
+    return binding.root
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    initViews()
+    initFlow()
+  }
+
+  private fun showLoadingIndicator() {
+    binding.progressBar.toVisible()
+  }
+
+  private fun hideLoadingIndicator() {
+    binding.progressBar.toGone()
+  }
+
+  private fun showErrorDescription(message: String) =
+    with(binding) {
+      recyclerView.toGone()
+      errorDescriptionTextView.toVisible()
+      errorDescriptionTextView.text = message
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+  private fun showDatas() =
+    with(binding) {
+      recyclerView.toVisible()
+      errorDescriptionTextView.toGone()
+      homeAdapter.apply {
+        addData(viewModel.randomData.value, viewModel.allData.value.orEmpty())
+        notifyDataSetChanged()
+      }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initViews()
-        fetchRandom()
-        fetchData()
-        viewModel.fetchData()
+  private fun initViews() =
+    with(binding) {
+      recyclerView.apply {
+        adapter = homeAdapter
+        val gridLayoutManager = createGridLayoutManager()
+        layoutManager = gridLayoutManager
+        addItemDecoration(GridSpacingItemDecoration(gridLayoutManager.spanCount, dip(6f)))
+      }
     }
 
-    private fun showLoadingIndicator() {
-        binding.progressBar.toVisible()
+  private fun initFlow() {
+    viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+      viewModel.allData.collectLatest { recyclerUpdate() }
+      viewModel.randomData.collectLatest { recyclerUpdate() }
     }
+  }
 
-    private fun hideLoadingIndicator() {
-        binding.progressBar.toGone()
-    }
-
-    private fun showErrorDescription(message: String) = with(binding) {
-        recyclerView.toGone()
-        errorDescriptionTextView.toVisible()
-        errorDescriptionTextView.text = message
-    }
-
-    private fun showDatas() = with(binding) {
-        recyclerView.toVisible()
-        errorDescriptionTextView.toGone()
-        homeAdapter.apply {
-            addData(viewModel.randomData.value, viewModel.allData.value.orEmpty())
-            notifyDataSetChanged()
-        }
-    }
-
-    private fun initViews() = with(binding) {
-        recyclerView.apply {
-            adapter = homeAdapter
-            val gridLayoutManager = createGridLayoutManager()
-            layoutManager = gridLayoutManager
-            addItemDecoration(GridSpacingItemDecoration(gridLayoutManager.spanCount, dip(6f)))
-        }
-    }
-
-    private fun RecyclerView.createGridLayoutManager(): GridLayoutManager = GridLayoutManager(
-        context,
-        3,
-        RecyclerView.VERTICAL,
-        false
-    ).apply {
-        spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-            override fun getSpanSize(position: Int): Int {
-                return when (adapter?.getItemViewType(position)) {
-                    ITEM_VIEW_TYPE_SECTION_HEADER, ITEM_VIEW_TYPE_FEATURED -> spanCount
-                    else -> 1
-                }
+  private fun RecyclerView.createGridLayoutManager(): GridLayoutManager =
+    GridLayoutManager(context, 3, RecyclerView.VERTICAL, false).apply {
+      spanSizeLookup =
+        object : GridLayoutManager.SpanSizeLookup() {
+          override fun getSpanSize(position: Int): Int {
+            return when (adapter?.getItemViewType(position)) {
+              ITEM_VIEW_TYPE_SECTION_HEADER,
+              ITEM_VIEW_TYPE_FEATURED -> spanCount
+              else -> 1
             }
+          }
         }
     }
 
-    private fun recyclerUpdate() {
-        try {
-            showLoadingIndicator()
-            showDatas()
-        } catch (exception: Exception) {
-            exception.printStackTrace()
-            showErrorDescription("에러가 발생했어요 😢")
-        } finally {
-            hideLoadingIndicator()
-        }
-    }
-
-    private fun fetchData() = viewModel.allData.observe(
-        viewLifecycleOwner,
-        {
-            recyclerUpdate()
-        }
-    )
-
-    private fun fetchRandom() = viewModel.randomData.observe(
-        viewLifecycleOwner,
-        {
-            recyclerUpdate()
-        }
-    )
+  private fun recyclerUpdate() {
+    runCatching {
+        showLoadingIndicator()
+        showDatas()
+      }
+      .onFailure { exception ->
+        exception.printStackTrace()
+        showErrorDescription("에러가 발생했어요 😢")
+      }
+    hideLoadingIndicator()
+  }
 }

1794112d90bfb3a80075ab6758ed235525f39307
Keel_im <kimh00335@gmail.com>
2022-12-04 17:30:30 +0900

    apply cnubus repository into all repository
    Former-commit-id: 5ac90d41f5a712b8d31abf57f93fd227e5fe0c73

--- cnubus/script/submodule_commit.sh
+++
@@ -1,7 +0,0 @@
-git submodule foreach git checkout -b newfeature
-git submodule update --remote
-git submodule foreach git add -A .
-git submodule foreach git commit -am "update common module"
-git submodule foreach git checkout master
-git submodule foreach git merge newfeature
-git submodule foreach git push

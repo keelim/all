7ed0feb29c7fc3b60bc6dfb0563d922da5ac80d8
Keel_im <kimh00335@gmail.com>
2022-12-04 13:01:23 +0900

    add build logic module

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,17 @@
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.keelim.builds.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+}
+

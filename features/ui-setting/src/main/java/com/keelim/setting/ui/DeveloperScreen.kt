a27de04d873890baec38f93d8517c80dcd6dd359
Keel_im <kimh00335@gmail.com>
2022-12-04 22:34:15 +0900

    update libs toml
    Former-commit-id: 02dbb2b9ecedcb19cd2dbb1ff1ed98b5a219b9be

--- features/ui-setting/src/main/java/com/keelim/setting/ui/DeveloperScreen.kt
+++ features/ui-setting/src/main/java/com/keelim/setting/ui/DeveloperScreen.kt
@@ -1,18 +1,5 @@
-/*
- * Designed and developed by 2020 keelim (Jaehyun Kim)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.keelim.setting.ui
 
 import androidx.compose.foundation.layout.Column
@@ -26,9 +13,11 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -39,11 +28,13 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.keelim.compose.ui.ProfileImageView
 import com.keelim.data.model.Developer
-import com.keelim.nandadiagnosis.compose.ui.ProfileImageView
-
+enum class Section {
+    Developer
+}
 @Composable
-internal fun DeveloperScreen(
+fun DeveloperScreen(
     modifier: Modifier = Modifier,
     developer: List<Developer>,
 ) {
@@ -59,8 +50,15 @@ private fun Profile(
     modifier: Modifier = Modifier,
     person: Developer,
 ) {
-    Card(modifier = modifier.aspectRatio(0.75f), shape = RoundedCornerShape(8.dp), elevation = 4.dp) {
-        Column(modifier = Modifier.padding(12.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Card(
+        modifier = modifier.aspectRatio(0.75f),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(4.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             ProfileImageView(
                 imageUrl = person.photoUrl,
                 stateColor = Color(android.graphics.Color.parseColor("#43B1B3")),
@@ -68,7 +66,8 @@ private fun Profile(
             )
 
             Column(
-                modifier = Modifier.padding(8.dp), horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier.padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
                     text = person.name,
@@ -92,6 +91,19 @@ private fun Profile(
     }
 }
 
+@Preview
+@Composable
+fun DeveloperScreenPreview() {
+    DeveloperScreen(
+        developer = listOf(
+            Developer(
+                "김재현",
+                photoUrl = "",
+                companyName = "",
+            )
+        )
+    )
+}
 @Preview(widthDp = 200)
 @Composable
 private fun ProfilePreview() {
--- features/ui-setting/src/main/java/com/keelim/setting/ui/Section.kt
+++ features/ui-setting/src/main/java/com/keelim/setting/ui/Section.kt
@@ -15,7 +15,4 @@
  */
 package com.keelim.setting.ui
 
-internal enum class Section {
-    // 같은 모듈에서만 사용 가능
-    Developer
-}
+

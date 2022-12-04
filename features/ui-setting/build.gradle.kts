6c44919aa6ce8703fbcec0f7df4c3200f4064229
Keel_im <kimh00335@gmail.com>
2022-12-04 22:47:44 +0900

    update ui-setting module
    Former-commit-id: d4996811556c6a52c2ef1db6d370fc375bf035cd

--- features/ui-setting/build.gradle.kts
+++ features/ui-setting/build.gradle.kts
@@ -16,7 +16,6 @@ dependencies {
     implementation(project(":data"))
     implementation(project(":domain"))
     implementation(project(":common"))
-    implementation(project(":compose"))
 
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)

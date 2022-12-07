73def64ee9f03d12a19c09e6562187b59a037550
Keel_im <kimh00335@gmail.com>
2022-12-07 23:11:00 +0900

    add paging runtime lib
    Former-commit-id: 9980c7f7d9f2f25d984d43cc4e76d812112927ad

--- app-comssa/build.gradle.kts
+++ app-comssa/build.gradle.kts
@@ -75,4 +75,6 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3-window-size-class")
     implementation("androidx.compose.runtime:runtime-livedata")
+
+    implementation(libs.androidx.paging.runtime)
 }

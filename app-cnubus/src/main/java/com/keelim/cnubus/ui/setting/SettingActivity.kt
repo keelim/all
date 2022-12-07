c371f2816eac7dfde239122f2aaeef74358fb41e
Keel_im <kimh00335@gmail.com>
2022-12-07 23:32:00 +0900

    add SettingScreen
    Former-commit-id: 59373188eacd59ba4d0bb8dc0ae2bdbbfcb31513

--- app-cnubus/src/main/java/com/keelim/cnubus/ui/setting/SettingActivity.kt
+++ app-cnubus/src/main/java/com/keelim/cnubus/ui/setting/SettingActivity.kt
@@ -3,11 +3,13 @@ package com.keelim.cnubus.ui.setting
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.keelim.compose.ui.screen.SettingScreen
 
 class SettingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            SettingScreen()
         }
     }
 }

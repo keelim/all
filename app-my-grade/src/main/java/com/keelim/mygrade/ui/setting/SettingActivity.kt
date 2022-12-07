fc7a9f95860a98cb3f27f26c48505d9529e07c08
Keel_im <kimh00335@gmail.com>
2022-12-07 23:28:25 +0900

    add compose dependency and Setting Activity
    Former-commit-id: 3e67c10e67150e4b4291d522a45317454e76c65c

---
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/setting/SettingActivity.kt
@@ -0,0 +1,13 @@
+package com.keelim.mygrade.ui.setting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class SettingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}

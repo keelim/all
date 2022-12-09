939a76a549cb9feedec717e64b535034deb9c923
Keel_im <kimh00335@gmail.com>
2022-12-10 02:47:43 +0900

    update lint
    Former-commit-id: d13d1815f196221d7c0af308272e2c7fbe5fb9c7

--- app-my-grade/src/main/java/com/keelim/mygrade/ui/setting/SettingActivity.kt
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/setting/SettingActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.Modifier
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.keelim.common.extensions.toast
 import com.keelim.compose.ui.screen.SettingAction
 import com.keelim.compose.ui.screen.SettingScreen
@@ -39,10 +38,8 @@ class SettingActivity : ComponentActivity() {
                 toast("아직 진행 중인 실험 기능이 없습니다. ")
             }
             SettingAction.OtherApp -> {
-
             }
             SettingAction.ThemeSetting -> {
-
             }
             SettingAction.ShowLogcat -> TODO()
         }

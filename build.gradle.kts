84930a35426c488b208241cf4299e0629cfe0187
Keel_im <kimh00335@gmail.com>
2022-12-11 00:41:33 +0900

    update plugin android code (#17)
    Former-commit-id: 7f752f111021f7a4c491a4923ce01b4d4cfde674

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,11 @@ buildscript {
 
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.secrets) apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     alias(libs.plugins.qodana) apply false
 }
 

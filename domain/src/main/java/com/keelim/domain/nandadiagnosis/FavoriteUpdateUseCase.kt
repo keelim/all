7c6a987ef6738a3ecf45f82a8bbb7628575e9327
Keel_im <kimh00335@gmail.com>
2022-12-13 01:10:44 +0900

    update cnubus module
    Former-commit-id: 8ec2981dfce233fbfb1ebf847dcbfabb3f526a53

--- domain/src/main/java/com/keelim/domain/nandadiagnosis/FavoriteUpdateUseCase.kt
+++ domain/src/main/java/com/keelim/domain/nandadiagnosis/FavoriteUpdateUseCase.kt
@@ -22,7 +22,7 @@ class FavoriteUpdateUseCase @Inject constructor(
     private val nandaIoRepository: NandaIORepository,
 ) {
 
-    operator fun invoke(favorite: Int, id: Int) {
+    suspend operator fun invoke(favorite: Int, id: Int) {
         when (favorite) {
             1 -> nandaIoRepository.updateFavorite(0, id)
             0 -> nandaIoRepository.updateFavorite(1, id)

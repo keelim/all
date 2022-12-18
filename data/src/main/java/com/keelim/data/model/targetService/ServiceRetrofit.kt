f67cb297d54f74e1dc9ba1d6fc802ba385207bb8
Keel_im <kimh00335@gmail.com>
2022-12-18 22:32:42 +0900

    update lint
    Former-commit-id: 6927b706db84fcfae9f4327ece710b7b90604e9d

--- data/src/main/java/com/keelim/data/model/targetService/ServiceRetrofit.kt
+++ data/src/main/java/com/keelim/data/model/targetService/ServiceRetrofit.kt
@@ -2,10 +2,10 @@ package com.keelim.data.model.targetService
 
 import retrofit2.Retrofit
 
-sealed class  ServiceRetrofit(val retrofit: Retrofit) {
-    class CnuBusRetrofit(retrofit: Retrofit): ServiceRetrofit(retrofit)
-    class MyGradeRetrofit(retrofit: Retrofit): ServiceRetrofit(retrofit)
-    class ComssaRetrofit(retrofit: Retrofit): ServiceRetrofit(retrofit)
-    class NandaRetrofit(retrofit: Retrofit): ServiceRetrofit(retrofit)
-    class YrRetrofit(retrofit: Retrofit): ServiceRetrofit(retrofit)
+sealed class ServiceRetrofit(val retrofit: Retrofit) {
+    class CnuBusRetrofit(retrofit: Retrofit) : ServiceRetrofit(retrofit)
+    class MyGradeRetrofit(retrofit: Retrofit) : ServiceRetrofit(retrofit)
+    class ComssaRetrofit(retrofit: Retrofit) : ServiceRetrofit(retrofit)
+    class NandaRetrofit(retrofit: Retrofit) : ServiceRetrofit(retrofit)
+    class YrRetrofit(retrofit: Retrofit) : ServiceRetrofit(retrofit)
 }

d697912f2af0ef25e15697c199f2f2e7d335cbf9
Keel_im <kimh00335@gmail.com>
2022-12-18 19:55:31 +0900

    Update FavoriteViewModel.kt
    Former-commit-id: 7f9a6b2a4d5ba13f152a287faeebc6882c1a32ee

--- app-comssa/src/main/java/com/keelim/comssa/ui/favorite/FavoriteViewModel.kt
+++ app-comssa/src/main/java/com/keelim/comssa/ui/favorite/FavoriteViewModel.kt
@@ -25,7 +25,7 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 @HiltViewModel
-class FavoriteViewModel @Inject constructor() : ViewModel() {
+class FavoriteViewModel : ViewModel() {
     fun favorite(favorite: Int, id: Int) = viewModelScope.launch {
     }
 

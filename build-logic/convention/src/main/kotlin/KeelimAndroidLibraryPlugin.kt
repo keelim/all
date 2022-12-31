37263dc0b86768f84d71eb3e952aae85c86e90e1
Keel_im <kimh00335@gmail.com>
2022-12-31 17:49:55 +0900

    udpate kts to kt

---
+++ build-logic/convention/src/main/kotlin/KeelimAndroidLibraryPlugin.kt
@@ -0,0 +1,21 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.keelim.builds.ProjectConfiguration
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+import com.keelim.builds.configureKotlinAndroid
+
+class KeelimAndroidLibraryPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        with(project) {
+            with(pluginManager) {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
+            extensions.getByType<LibraryExtension>().apply {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = ProjectConfiguration.targetSdk
+            }
+        }
+    }
+}

8e6f4ba2cdb178bae9e32b6a6fe6f38a8a9b3ff5
Keel_im <kimh00335@gmail.com>
2022-12-31 17:49:55 +0900

    udpate kts to kt

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -15,3 +15,28 @@ dependencies {
     implementation(libs.kotlin.gradlePlugin)
 }
 
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "keelim.android.application.compose"
+            implementationClass = "KeelimAndroidApplicationComposePlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "keelim.android.library.compose"
+            implementationClass = "KeelimAndroidLibraryComposePlugin"
+        }
+        register("androidTest") {
+            id = "keelim.android.test"
+            implementationClass = "KeelimAndroidTestPlugin"
+        }
+        register("androidLibrary") {
+            id = "keelim.android.library"
+            implementationClass = "KeelimAndroidLibraryPlugin"
+        }
+        register("androidApplication") {
+            id = "keelim.android.application"
+            implementationClass = "KeelimAndroidApplicationPlugin"
+        }
+    }
+}
+

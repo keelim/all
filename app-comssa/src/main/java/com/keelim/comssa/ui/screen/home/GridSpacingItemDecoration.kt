697d384de5a8ec2f9494a98296402b3069a264f7
Keel_im <kimh00335@gmail.com>
2023-02-11 17:28:44 +0900

    #24 clean package app-comssa

---
+++ app-comssa/src/main/java/com/keelim/comssa/ui/screen/home/GridSpacingItemDecoration.kt
@@ -0,0 +1,67 @@
+/*
+ * Designed and developed by 2021 keelim (Jaehyun Kim)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.keelim.comssa.ui.screen.home
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class GridSpacingItemDecoration(
+    private val spanCount: Int,
+    private val spacing: Int
+) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val adapterPosition = parent.getChildAdapterPosition(view)
+        val gridLayoutManager = parent.layoutManager as GridLayoutManager
+        val spanSize = gridLayoutManager.spanSizeLookup.getSpanSize(adapterPosition)
+
+        if (spanSize == spanCount) {
+            outRect.left = spacing
+            outRect.right = spacing
+            outRect.top = spacing
+            outRect.bottom = spacing
+            return
+        }
+
+        val column = (view.layoutParams as GridLayoutManager.LayoutParams).spanIndex
+        val itemHorizontalSpacing = ((spanCount + 1) * spacing) / spanCount.toFloat()
+        when (column) {
+            0 -> {
+                outRect.left = spacing
+                outRect.right = (itemHorizontalSpacing - spacing).toInt()
+            }
+
+            (spanCount - 1) -> {
+                outRect.left = (itemHorizontalSpacing - spacing).toInt()
+                outRect.right = spacing
+            }
+
+            else -> {
+                outRect.left = (itemHorizontalSpacing / 2).toInt()
+                outRect.right = (itemHorizontalSpacing / 2).toInt()
+            }
+        }
+        outRect.top = spacing
+        outRect.bottom = spacing
+    }
+}

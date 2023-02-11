81c14cd87c20a350f9437dcf385b6e7165f2469b
Keel_im <kimh00335@gmail.com>
2023-02-11 16:03:35 +0900

    comssa flash card state

---
+++ app-comssa/src/main/java/com/keelim/comssa/ui/main/flash/FlashCardFragment.kt
@@ -0,0 +1,22 @@
+package com.keelim.comssa.ui.main.flash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+// Flash Card 를 만드는 방법
+@AndroidEntryPoint
+class FlashCardFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setContent { FlashCardScreen() }
+    }
+}

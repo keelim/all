cda2508145154355b5fb574972beaed820b3e9c1
Keel_im <kimh00335@gmail.com>
2023-02-11 17:25:47 +0900

    #24 clean package app-cnubus

---
+++ app-cnubus/src/main/java/com/keelim/cnubus/ui/screen/setting/SettingActivity.kt
@@ -0,0 +1,10 @@
+package com.keelim.cnubus.ui.screen.setting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SettingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

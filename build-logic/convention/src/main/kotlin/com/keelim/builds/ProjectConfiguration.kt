c649152a5538d96552153eeaf408d8e6f8fc96a4
Keel_im <kimh00335@gmail.com>
2023-03-01 19:27:37 +0900

    Update ProjectConfiguration.kt

--- build-logic/convention/src/main/kotlin/com/keelim/builds/ProjectConfiguration.kt
+++ build-logic/convention/src/main/kotlin/com/keelim/builds/ProjectConfiguration.kt
@@ -6,7 +6,7 @@ object ProjectConfiguration {
     const val compileSdk = 33
     const val minSdk = 24
     const val targetSdk = 33
-    const val versionCode = 20230211
+    const val versionCode = 20230301
     const val versionName = versionCode.toString()
     val javaVer = JavaVersion.VERSION_1_8
 }

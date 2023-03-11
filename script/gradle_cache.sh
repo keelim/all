11e3bb4999242f14fd2ac1270015f4642051c645
Keel_im <kimh00335@gmail.com>
2023-03-11 16:54:13 +0900

    Update gradle_cache.sh

--- script/gradle_cache.sh
+++ script/gradle_cache.sh
@@ -1,5 +1,5 @@
-#gradle cache 문제를 해결하기 위한 스크립트 (21일 이후 안쓰는 캐시는 지우기)
-alias killgradlecache='find ~/.gradle -type f -atime +21 -delete && find ~/.gradle -mindepth 1 -type d -empty -delete'
+#gradle cache 문제를 해결하기 위한 스크립트 (30일 이후 안쓰는 캐시는 지우기)
+alias killgradlecache='find ~/.gradle -type f -atime +30 -delete && find ~/.gradle -mindepth 1 -type d -empty -delete'
 du -sh ~/.gradle
 killgradlecache
 du -sh ~/.gradle

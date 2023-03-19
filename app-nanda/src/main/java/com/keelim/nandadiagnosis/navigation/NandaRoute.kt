3fa248e81b1928e680ea44e356c4d3e51ddf2ca2
Keel_im <kimh00335@gmail.com>
2023-03-19 23:37:44 +0900

    Update NandaRoute.kt

--- app-nanda/src/main/java/com/keelim/nandadiagnosis/navigation/NandaRoute.kt
+++ app-nanda/src/main/java/com/keelim/nandadiagnosis/navigation/NandaRoute.kt
@@ -3,6 +3,11 @@ package com.keelim.nandadiagnosis.navigation
 class NandaRoute {
 }
 sealed class NandaRoutes(val route: String){
-    object Login: NandaRoutes(route = "Login")
-    object Main: NandaRoutes(route = "Main")
+    // auth
+    object Auth : NandaRoutes(route = "auth")
+    object Profile : NandaRoutes(route = "auth/profile")
+    object Login : NandaRoutes(route = "auth/login")
+
+    // main
+    object Main : NandaRoutes(route = "main")
 }

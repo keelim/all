a8b4e5190f14b7225f444cac5fe6447ef316b2b9
Keel_im <kimh00335@gmail.com>
2023-04-09 20:32:22 +0900

    Update SimpleButtonDialog.kt

--- compose/src/main/java/com/keelim/compose/component/dialog/SimpleButtonDialog.kt
+++ compose/src/main/java/com/keelim/compose/component/dialog/SimpleButtonDialog.kt
@@ -5,13 +5,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun SimpleDialog(
+    modifier: Modifier = Modifier,
     text: String,
     onDismiss: () -> Unit
 ) {
     AlertDialog(
+        modifier = modifier,
         text = {
             Text(
                 text = text,

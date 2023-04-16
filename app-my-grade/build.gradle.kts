a198579eab9e594761f29a6e18118bc7aa2bf02e
Keel_im <kimh00335@gmail.com>
2023-04-17 00:40:06 +0900

    #99 coil 의존성 추가

--- app-my-grade/build.gradle.kts
+++ app-my-grade/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.inapp.update)
     implementation(libs.coil.kt)
     implementation(libs.coil.kt.svg)
+    implementation(libs.coil.kt.compose)
     implementation(libs.androidx.metrics)
 
     // modern storage
@@ -82,6 +83,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.paging.compose)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.tooling)
 }
 
 
--- app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/center/book/BookFragment.kt
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/center/book/BookFragment.kt
@@ -1,23 +1,38 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.keelim.mygrade.ui.screen.center.book
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.keelim.common.extensions.compose.fragmentComposeView
+import com.keelim.compose.component.appbar.SearchView
 import com.keelim.data.repository.mygrade.BookRepository
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class BookViewModel @Inject constructor(val repository: BookRepository) : ViewModel() {
+class BookViewModel @Inject constructor(
+    repository: BookRepository
+) : ViewModel() {
 
     var query = mutableStateOf("")
         private set
@@ -37,5 +52,26 @@ class BookFragment : Fragment() {
     ): View = fragmentComposeView {
         val books = viewModel.bookFlow.collectAsLazyPagingItems()
         val focusManager = LocalFocusManager.current
+        MaterialTheme {
+            Scaffold(topBar = {
+
+                TopAppBar(title = {
+                    SearchView(
+                        query = viewModel.query.value,
+                        onQueryChanged = { newQuery -> viewModel.setQuery(newQuery) },
+                        onSearch = { focusManager.clearFocus() },
+                        onClearQuery = { viewModel.setQuery("") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(
+                                color = MaterialTheme.colorScheme.background,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                    )
+                })
+            }) { padding ->
+                BookScreen(books = books, modifier = Modifier.padding(padding))
+            }
+        }
     }
 }

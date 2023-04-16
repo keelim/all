4d3ce22db2e7fd0854a9ccdaa407577c18577b8f
Keel_im <kimh00335@gmail.com>
2023-04-16 17:42:26 +0900

    #99 update book fragment

--- app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/center/book/BookFragment.kt
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/center/book/BookFragment.kt
@@ -4,15 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.keelim.common.extensions.compose.fragmentComposeView
+import com.keelim.data.repository.mygrade.BookRepository
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BookViewModel @Inject constructor(val repository: BookRepository) : ViewModel() {
+
+    var query = mutableStateOf("")
+        private set
+
+    val bookFlow = repository.getBookFlow(query.value)
+
+    fun setQuery(requestQuery: String) {
+        query.value = requestQuery
+    }
+}
 
 @AndroidEntryPoint
 class BookFragment : Fragment() {
+    private val viewModel: BookViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View = fragmentComposeView {
-
+        val books = viewModel.bookFlow.collectAsLazyPagingItems()
+        val focusManager = LocalFocusManager.current
     }
 }

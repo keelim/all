d45089859a1741a5192495557ca5a456ddcd2da7
Keel_im <kimh00335@gmail.com>
2023-04-16 17:21:17 +0900

    #99 create compose book container

---
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/center/book/BookFragment.kt
@@ -0,0 +1,18 @@
+package com.keelim.mygrade.ui.screen.center.book
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.keelim.common.extensions.compose.fragmentComposeView
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BookFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View = fragmentComposeView {
+
+    }
+}

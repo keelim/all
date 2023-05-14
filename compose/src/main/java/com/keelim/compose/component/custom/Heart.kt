d0fa74ccfdcffb074ded8c47fe1e2c2470019ee4
Keel_im <kimh00335@gmail.com>
2023-05-15 01:58:04 +0900

    move package

--- compose/src/main/java/com/keelim/compose/component/custom/Heart.kt
+++ compose/src/main/java/com/keelim/compose/component/custom/Heart.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.keelim.compose.component
+package com.keelim.compose.component.custom
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi

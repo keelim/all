8634cb097e6618182e710abce208738b03ee0f24
Keel_im <kimh00335@gmail.com>
2023-05-15 01:58:04 +0900

    move package

--- compose/src/main/java/com/keelim/compose/component/widget/WidgetContent.kt
+++ compose/src/main/java/com/keelim/compose/component/widget/WidgetContent.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.keelim.compose.component
+package com.keelim.compose.component.widget
 
 import android.content.Context
 import androidx.compose.material3.MaterialTheme

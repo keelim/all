9de3cd09068572443a3c83b0fdf225156460a2ce
Keel_im <kimh00335@gmail.com>
2023-05-18 01:21:48 +0900

    modify string resource package

--- compose/src/main/java/com/keelim/composeutil/screen/setting/SettingScreen.kt
+++ compose/src/main/java/com/keelim/composeutil/screen/setting/SettingScreen.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.keelim.composeutil.BuildConfig
+import com.keelim.compose.BuildConfig
 
 sealed class SettingAction {
     object AlarmSetting : SettingAction()

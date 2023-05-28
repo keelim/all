adf0e8b33ef3114da21db5a352dc8785dd1b96f0
Keel_im <kimh00335@gmail.com>
2023-05-28 17:50:58 +0900

    update java version 17 and gradle 8.0

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 group = "com.keelim.builds.buildlogic"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {

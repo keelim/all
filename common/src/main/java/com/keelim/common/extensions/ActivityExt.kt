6bc1e68e845d27bc1f226b2aa4fd082c40cd9bf4
Keel_im <kimh00335@gmail.com>
2023-06-24 21:29:09 +0900

    Update ActivityExt.kt

--- common/src/main/java/com/keelim/common/extensions/ActivityExt.kt
+++ common/src/main/java/com/keelim/common/extensions/ActivityExt.kt
@@ -1,6 +1,7 @@
 package com.keelim.common.extensions
 
 import android.app.Activity
+import android.content.Context
 import android.graphics.Color
 import android.view.View
 import android.view.WindowManager
@@ -20,4 +21,9 @@ fun Activity.hideKeyboard() {
     val imm: InputMethodManager = getSystemService(InputMethodManager::class.java)
     val view = currentFocus ?: View(this)
     imm.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+}
+
+fun Context.getScreenWidth(): Int {
+    val displayMetrics = resources.displayMetrics
+    return displayMetrics.widthPixels
 }

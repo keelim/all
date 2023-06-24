d3c261a44c681c03cd30435922f8350f558563b6
Keel_im <kimh00335@gmail.com>
2023-06-24 20:37:09 +0900

    Update Dot.kt

--- compose/src/main/java/com/keelim/composeutil/component/box/Dot.kt
+++ compose/src/main/java/com/keelim/composeutil/component/box/Dot.kt
@@ -12,9 +12,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,4 +58,53 @@ fun RedDot(
 @Composable
 private fun PreviewRedDot() {
     RedDot()
+}
+
+@Composable
+fun GradientDot(
+    modifier: Modifier = Modifier
+) {
+    val value by rememberInfiniteTransition(label = "").animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = LinearEasing
+            )
+        ), label = ""
+    )
+    val colors = listOf(
+        Color.Red,
+        Color.Green,
+        Color.Blue,
+        Color.Black
+    )
+
+    var gradientBrush by remember {
+        mutableStateOf(
+            Brush.horizontalGradient(
+                colors = colors,
+                startX = -10.0f,
+                endX = 400.0f,
+                tileMode = TileMode.Repeated
+            )
+        )
+    }
+    Box(modifier = Modifier
+        .drawBehind {
+            rotate(value) {
+                drawCircle(
+                    gradientBrush, style = Stroke(width = 12.dp.value)
+                )
+            }
+        }
+        .size(125.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewGradientDot() {
+    GradientDot()
 }

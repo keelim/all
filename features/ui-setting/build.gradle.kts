db0ed795d0a093a558d9d7e9b769ea260ad86b38
Keel_im <kimh00335@gmail.com>
2023-06-24 21:51:44 +0900

    #238 set glance module

--- features/ui-setting/build.gradle.kts
+++ features/ui-setting/build.gradle.kts
@@ -19,7 +19,6 @@ dependencies {
 
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.glance)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.material.themAdapter)

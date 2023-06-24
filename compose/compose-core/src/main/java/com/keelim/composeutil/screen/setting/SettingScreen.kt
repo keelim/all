2e39a1e90add554f2b5087b8ee5c8992a75617f7
Keel_im <kimh00335@gmail.com>
2023-06-25 00:47:29 +0900

    #245 fix compile error

--- compose/compose-core/src/main/java/com/keelim/composeutil/screen/setting/SettingScreen.kt
+++ compose/compose-core/src/main/java/com/keelim/composeutil/screen/setting/SettingScreen.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.keelim.compose.BuildConfig
+import com.keelim.compose.core.BuildConfig
 
 sealed class SettingAction {
     object AlarmSetting : SettingAction()

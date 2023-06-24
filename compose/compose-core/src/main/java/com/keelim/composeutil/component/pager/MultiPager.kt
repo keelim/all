23890b0e0ae24af5efb20f7b6cad2c24124f1d58
Keel_im <kimh00335@gmail.com>
2023-06-25 01:12:50 +0900

    Create MultiPager.kt

---
+++ compose/compose-core/src/main/java/com/keelim/composeutil/component/pager/MultiPager.kt
@@ -0,0 +1,37 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package com.keelim.composeutil.component.pager
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MultiHorizontalPager() {
+    val state = rememberPagerState()
+    HorizontalPager(
+        pageCount = 3,
+        beyondBoundsPageCount = 3,
+        state = state,
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+            .fillMaxSize()
+    ) { page ->
+        when (page) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewMultiHorizontalPager() {
+    MultiHorizontalPager()
+
+}

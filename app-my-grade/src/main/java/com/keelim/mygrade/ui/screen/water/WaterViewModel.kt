b812d866480738fd433dc8ff5671be72aeaa386b
Keel_im <kimh00335@gmail.com>
2023-06-27 02:12:07 +0900

    update water screen

---
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/water/WaterViewModel.kt
@@ -0,0 +1,12 @@
+package com.keelim.mygrade.ui.screen.water
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WaterViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}

8773f85a53c419736e49145cd8bcbe30df6ad311
Keel_im <kimh00335@gmail.com>
2023-06-27 23:48:51 +0900

    Update ArrowBar.kt

--- compose/compose-core/src/main/java/com/keelim/composeutil/component/appbar/ArrowBar.kt
+++ compose/compose-core/src/main/java/com/keelim/composeutil/component/appbar/ArrowBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -28,6 +29,7 @@ fun NavigationBackArrowBar(title: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(horizontal = 8.dp)
             .height(50.dp)
             .clickable {
                 backDispatcher.onBackPressed()

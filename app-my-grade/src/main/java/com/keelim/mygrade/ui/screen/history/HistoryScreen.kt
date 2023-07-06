6127dcdeabf09d6f362d5116f13662a7eeca0f73
Keel_im <kimh00335@gmail.com>
2023-07-06 23:39:33 +0900

    screen update

--- app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/history/HistoryScreen.kt
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/history/HistoryScreen.kt
@@ -2,7 +2,6 @@
 
 package com.keelim.mygrade.ui.screen.history
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,12 +16,10 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -70,7 +67,7 @@ fun HistoryList(
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         items(items = histories) { history ->
-            GradeHistoryItem(
+            HistoryCard(
                 history = history,
                 onHistoryClick = onHistoryClick,
             )
@@ -85,7 +82,7 @@ private fun PreviewHistoryList() {
         modifier = Modifier.padding(horizontal = 12.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
-        GradeHistoryItem(
+        HistoryCard(
             GradeHistory(
                 date = "sociosqu",
                 grade = "placerat",
@@ -93,7 +90,7 @@ private fun PreviewHistoryList() {
                 totalStudent = 9776,
             ),
         )
-        GradeHistoryItem(
+        HistoryCard(
             GradeHistory(
                 date = "sociosqu",
                 grade = "placerat",
@@ -101,7 +98,7 @@ private fun PreviewHistoryList() {
                 totalStudent = 9776,
             ),
         )
-        GradeHistoryItem(
+        HistoryCard(
             GradeHistory(
                 date = "sociosqu",
                 grade = "placerat",
@@ -109,7 +106,7 @@ private fun PreviewHistoryList() {
                 totalStudent = 9776,
             ),
         )
-        GradeHistoryItem(
+        HistoryCard(
             GradeHistory(
                 date = "sociosqu",
                 grade = "placerat",
@@ -117,7 +114,7 @@ private fun PreviewHistoryList() {
                 totalStudent = 9776,
             ),
         )
-        GradeHistoryItem(
+        HistoryCard(
             GradeHistory(
                 date = "sociosqu",
                 grade = "placerat",
@@ -128,53 +125,66 @@ private fun PreviewHistoryList() {
     }
 }
 
+@Preview
 @Composable
-fun GradeHistoryItem(
-    history: GradeHistory,
-    onHistoryClick: (String, String) -> Unit = { _, _ -> },
-) {
-    Card(
-        onClick = { onHistoryClick(history.grade, "${history.myGrade} / ${history.totalStudent}") },
-        shape = ShapeDefaults.Large,
-        modifier = Modifier.fillMaxWidth(),
-        border = BorderStroke(1.dp, Color.Black),
-    ) {
-        Column(
-            modifier = Modifier.padding(horizontal = 12.dp, vertical = 20.dp),
-        ) {
-            Text(
-                text = "예상 학점: ${history.grade}",
-                style = MaterialTheme.typography.bodyLarge,
-            )
-            Spacer(modifier = Modifier.height(12.dp))
-            Text(
-                text = "예상 등수: ${history.myGrade} / ${history.totalStudent} 등",
-                style = MaterialTheme.typography.bodyLarge,
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Row {
-                Spacer(
-                    modifier = Modifier.weight(1f),
-                )
-                Text(
-                    text = "입력 날짜: ${history.date}",
-                    style = MaterialTheme.typography.bodySmall,
-                )
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun PreviewGradeHistoryItem() {
-    GradeHistoryItem(
-        history =
-        GradeHistory(
+private fun PreviewHistoryCard() {
+    HistoryCard(
+        history = GradeHistory(
             date = "utamur",
             grade = "suscipiantur",
             myGrade = 5812,
             totalStudent = 7712,
-        ),
+        )
     )
 }
+
+
+@Composable
+fun HistoryCard(
+    history: GradeHistory,
+    onHistoryClick: (String, String) -> Unit = { _, _ -> },
+) {
+    // val context = LocalContext.current
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        onClick = { onHistoryClick(history.grade, "${history.myGrade} / ${history.totalStudent}") },
+    ) {
+        Column {
+            Row(
+                modifier = Modifier.padding(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Column {
+                    Text(
+                        text = "예상 학점: ${history.grade}",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                    Spacer(Modifier.height(4.dp))
+                    Text(
+                        text = "예상 등수: ${history.myGrade} / ${history.totalStudent} 등",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+            Text(
+                text = "입력 날짜: ${history.date}",
+                maxLines = 4,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+            Spacer(Modifier.height(16.dp))
+            // AsyncImage(
+            //     model = ImageRequest.Builder(context)
+            //         .data("https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80")
+            //         .crossfade(true)
+            //         .build(),
+            //     modifier = Modifier
+            //         .aspectRatio(16 / 9f),
+            //     contentScale = ContentScale.Crop,
+            //     contentDescription = null
+            // )
+        }
+    }
+}
+

4a27f094b682c740d6350f6379698b676fb6aab8
Keel_im <kimh00335@gmail.com>
2023-07-07 00:45:17 +0900

    navigation 설정 및 로직 준비

---
+++ features/ui-setting/src/main/java/com/keelim/setting/screen/NotificationScreen.kt
@@ -0,0 +1,176 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.keelim.composeutil.component.layout.EmptyView
+import com.keelim.setting.screen.NotificationState
+import com.keelim.setting.screen.NotificationViewModel
+
+@Composable
+fun NotificationRoute() {
+    NotificationScreen()
+}
+
+@Composable
+private fun NotificationScreen(viewModel: NotificationViewModel = hiltViewModel()) {
+    val notificationState: NotificationState by viewModel.notificationState.collectAsStateWithLifecycle()
+    val listState = rememberLazyListState()
+    val hasScrolled by remember { derivedStateOf { listState.firstVisibleItemScrollOffset > 0 } }
+    val appBarElevation by
+    animateDpAsState(
+        targetValue =
+        if (hasScrolled) {
+            4.dp
+        } else {
+            0.dp
+        },
+        label = ""
+    )
+    val onBackPressedDispatcher =
+        checkNotNull(LocalOnBackPressedDispatcherOwner.current).onBackPressedDispatcher
+    Scaffold(
+        containerColor = MaterialTheme.colorScheme.surface,
+        contentColor = MaterialTheme.colorScheme.onSurface,
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors =
+                TopAppBarDefaults.topAppBarColors(
+                    containerColor =
+                    if (isSystemInDarkTheme()) {
+                        MaterialTheme.colorScheme.surfaceVariant.copy(alpha = if (hasScrolled) 1f else 0f)
+                    } else {
+                        MaterialTheme.colorScheme.surface
+                    },
+                ),
+                modifier = Modifier.shadow(appBarElevation),
+                title = { Text(text = "Notifications") },
+                navigationIcon = {
+                    IconButton(onClick = { onBackPressedDispatcher.onBackPressed() }) {
+                        Icon(Icons.Rounded.ArrowBack, contentDescription = "Go back")
+                    }
+                },
+                actions = {},
+            )
+        },
+    ) { padding ->
+        when (notificationState) {
+            is NotificationState.Empty -> {
+                EmptyView()
+            }
+
+            is NotificationState.Success -> {
+                LazyColumn(
+                    contentPadding = padding,
+                    state = listState,
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    items((notificationState as NotificationState.Success).items) { notification ->
+                        NotificationListCard(
+                            notificationTitle = notification.title,
+                            notificationDesc = notification.desc,
+                            notificationImageUrl = notification.imageUrl
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun NotificationListCard(
+    notificationTitle: String,
+    notificationDesc: String,
+    notificationImageUrl: String,
+) {
+    val context = LocalContext.current
+    Card {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(context).data(notificationImageUrl).crossfade(true)
+                    .build(),
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop,
+                contentDescription = "Content Description"
+            )
+            Column(verticalArrangement = Arrangement.spacedBy(6.dp)) {
+                Text(
+                    text = notificationTitle,
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 16.sp),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+                Text(
+                    text = notificationDesc,
+                    style = TextStyle(fontWeight = FontWeight.Medium, fontSize = 16.sp),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewNotificationScreen() {
+    NotificationScreen()
+}
+
+@Preview
+@Composable
+private fun PreviewNotificationListCard() {
+    NotificationListCard(
+        notificationTitle = "공지 제목",
+        notificationDesc = "공지 설명",
+        notificationImageUrl = ""
+    )
+}

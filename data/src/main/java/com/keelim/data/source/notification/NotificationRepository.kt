8a9b7c89a5f3c62b3a6a8b1b978d0e99cad55dda
Keel_im <kimh00335@gmail.com>
2023-07-11 00:35:01 +0900

    #262 notification data layer 작성

---
+++ data/src/main/java/com/keelim/data/source/notification/NotificationRepository.kt
@@ -0,0 +1,14 @@
+package com.keelim.data.source.notification
+
+
+data class Notification(
+    val date: String,
+    val title: String,
+    val desc: String
+)
+
+interface NotificationRepository {
+    suspend fun getNotification(): List<Notification>
+}
+
+

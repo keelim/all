3504e269af62de59679e9251a0d418ec1b1319eb
Keel_im <kimh00335@gmail.com>
2023-07-11 00:35:01 +0900

    #262 notification data layer 작성

---
+++ data/src/main/java/com/keelim/data/network/response/NotificationResponse.kt
@@ -0,0 +1,15 @@
+package com.keelim.data.network.response
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NotificationResponse(
+    @SerialName("majorDimension")
+    val majorDimension: String, // ROWS
+    @SerialName("range")
+    val range: String, // notifications!A1:Z1000
+    @SerialName("values")
+    val values: List<List<String>>
+)

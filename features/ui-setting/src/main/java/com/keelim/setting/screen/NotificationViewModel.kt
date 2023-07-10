db1cf6e8355c3481c0aedb7d728ed2cc5ae85d7c
Keel_im <kimh00335@gmail.com>
2023-07-11 00:35:23 +0900

    #262 update notification screen

--- features/ui-setting/src/main/java/com/keelim/setting/screen/NotificationViewModel.kt
+++ features/ui-setting/src/main/java/com/keelim/setting/screen/NotificationViewModel.kt
@@ -2,32 +2,48 @@ package com.keelim.setting.screen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.keelim.data.di.DefaultDispatcher
+import com.keelim.data.source.notification.NotificationRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
-import javax.inject.Inject
 
 @HiltViewModel
-class NotificationViewModel @Inject constructor() : ViewModel() {
-    // repository 만들 것
-    val notificationState = flow {
-        emit(NotificationState.Empty)
-    }.catch {
-        emit(NotificationState.Empty)
-    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), NotificationState.Empty)
+class NotificationViewModel
+@Inject
+constructor(
+  val notificationRepository: NotificationRepository,
+  @DefaultDispatcher val dispatcher: CoroutineDispatcher
+) : ViewModel() {
+  // repository 만들 것
+  val notificationState =
+    flow {
+        notificationRepository
+          .getNotification()
+          .map { Notification(it.date, it.title, it.desc) }
+          .let {
+            if (it.isEmpty()) {
+              emit(NotificationState.Empty)
+            } else {
+              emit(NotificationState.Success(it.toPersistentList()))
+            }
+          }
+      }
+      .flowOn(dispatcher)
+      .catch { emit(NotificationState.Empty) }
+      .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), NotificationState.Empty)
 }
 
-
-data class Notification(
-    val title: String,
-    val desc: String,
-    val imageUrl: String
-)
+data class Notification(val date: String, val title: String, val desc: String)
 
 sealed interface NotificationState {
-    object Empty : NotificationState
-    class Success(val items: PersistentList<Notification>) : NotificationState
+  object Empty : NotificationState
+  class Success(val items: PersistentList<Notification>) : NotificationState
 }

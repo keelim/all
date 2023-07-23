33ed291100342c4b5d7e0bc1b1074ba3c8d35de7
Keel_im <kimh00335@gmail.com>
2023-07-23 20:22:47 +0900

    update cnu bus logic

--- app-cnubus/src/main/java/com/keelim/cnubus/ui/screen/setting/AppSettingFragment.kt
+++ app-cnubus/src/main/java/com/keelim/cnubus/ui/screen/setting/AppSettingFragment.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.keelim.cnubus.ui.screen.setting.appsetting
+package com.keelim.cnubus.ui.screen.setting
 
 import SettingsRoute
 import android.content.Intent

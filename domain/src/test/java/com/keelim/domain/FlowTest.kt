43d35267294100a0568cec44cd1ef627eee07335
Keel_im <kimh00335@gmail.com>
2023-08-07 01:47:26 +0900

    introduce test google/truth and turbine

--- domain/src/test/java/com/keelim/domain/FlowTest.kt
+++ domain/src/test/java/com/keelim/domain/FlowTest.kt
@@ -1,10 +1,15 @@
 package com.keelim.domain
 
+import app.cash.turbine.test
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import kotlin.system.measureTimeMillis
 
@@ -38,6 +43,23 @@ class FlowTest {
             }.let {
                 println("Collected in $it ms")
             }
+        }
+    }
+
+    @Test
+    fun turbineTest() = runBlocking {
+        val number = (0..2).asFlow().onEach { delay(1) }
+        val intro = listOf("Hello").asFlow()
+
+        val combined = combine(intro, number) { one, two ->
+            "$one $two"
+        }
+
+        combined.test {
+            assertEquals("Hello 0", awaitItem())
+            assertEquals("Hello 1", awaitItem())
+            assertEquals("Hello 2", awaitItem())
+            awaitComplete()
         }
     }
 

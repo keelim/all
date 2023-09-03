cb5c2d0a7a841c174c6f42db463717d4ce22d9fa
Keel_im <kimh00335@gmail.com>
2023-09-04 02:00:29 +0900

    update compose compiler and delete kapt

--- build-logic/convention/src/main/kotlin/KeelimApplicationFirebasePlugin.kt
+++ build-logic/convention/src/main/kotlin/KeelimApplicationFirebasePlugin.kt
@@ -1,12 +1,11 @@
 
 import com.android.build.api.dsl.ApplicationExtension
 import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
+import com.keelim.builds.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 
 class KeelimApplicationFirebasePlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -17,7 +16,6 @@ class KeelimApplicationFirebasePlugin : Plugin<Project> {
                 apply("com.google.firebase.crashlytics")
             }
 
-            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
                 val bom = libs.findLibrary("firebase-bom").get()
                 add("implementation", platform(bom))

ec8569f71e318a46cb7e988ff220191889d6c531
Keel_im <kimh00335@gmail.com>
2023-09-18 02:07:46 +0900

    [all] create empty event screen

--- features/ui-setting/src/main/java/com/keelim/setting/screen/event/EventScreen.kt
+++ features/ui-setting/src/main/java/com/keelim/setting/screen/event/EventScreen.kt
@@ -1,8 +1,18 @@
 package com.keelim.setting.screen.event
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun EventRoute() {
+  EventScreen()
+}
 
+@Composable fun EventScreen(viewModel: EventViewModel = hiltViewModel()) {}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewEventScreen() {
+  EventScreen()
 }

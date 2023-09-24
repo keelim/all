cf481a0a1c308b19b7978d0e16e303e7888e4cde
Keel_im <kimh00335@gmail.com>
2023-09-24 23:24:22 +0900

    [mygrade] udpate data layer for local task
    - add date time column
    - add tomorrow task

--- data/src/main/java/com/keelim/data/source/local/LocalTask.kt
+++ data/src/main/java/com/keelim/data/source/local/LocalTask.kt
@@ -1,16 +1,16 @@
 package com.keelim.data.source.local
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.keelim.data.source.Task
-import com.keelim.data.source.network.NetworkTask
 
 @Entity(
     tableName = "task"
 )
 data class LocalTask(
     @PrimaryKey val id: String,
-    var title: String,
-    var description: String,
+    val title: String,
+    val description: String,
     var isCompleted: Boolean,
+    @ColumnInfo(name = "date", defaultValue = "" ) val date: String
 )

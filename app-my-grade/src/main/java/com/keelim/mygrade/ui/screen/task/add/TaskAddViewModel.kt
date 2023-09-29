72da9cf3c2fe8e158ee270467739b9e69a76b255
Keel_im <kimh00335@gmail.com>
2023-09-30 00:19:42 +0900

    [mygrade] add simple task screen

--- app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/task/add/TaskAddViewModel.kt
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/task/add/TaskAddViewModel.kt
@@ -6,40 +6,70 @@ import androidx.lifecycle.viewModelScope
 import com.keelim.data.di.IoDispatcher
 import com.keelim.data.source.DefaultTaskRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import javax.inject.Inject
 
 @Stable
-sealed interface TaskAddState {
-    data object Empty : TaskAddState
-    data object Loading : TaskAddState
-    data object Success : TaskAddState
+sealed interface TaskAddScreenState {
+  data object Empty : TaskAddScreenState
+
+  data object Loading : TaskAddScreenState
+
+  data object Success : TaskAddScreenState
 }
 
+@Stable
+data class TaskAddUiState(
+  val title: String = "",
+  val description: String = "",
+)
+
 @HiltViewModel
-class TaskAddViewModel @Inject constructor(
-    val defaultTaskRepository: DefaultTaskRepository,
-    @IoDispatcher val dispatcher: CoroutineDispatcher,
+class TaskAddViewModel
+@Inject
+constructor(
+  val defaultTaskRepository: DefaultTaskRepository,
+  @IoDispatcher val dispatcher: CoroutineDispatcher,
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<TaskAddState>(TaskAddState.Empty)
-    val uiState: StateFlow<TaskAddState> = _uiState.asStateFlow()
+  private val _screenState = MutableStateFlow<TaskAddScreenState>(TaskAddScreenState.Empty)
+  val screenState: StateFlow<TaskAddScreenState> = _screenState.asStateFlow()
 
-    fun addTask(title: String, description: String) {
-        viewModelScope.launch {
-            _uiState.tryEmit(TaskAddState.Loading)
-            withContext(dispatcher) {
-                val id = defaultTaskRepository.create(
-                    title = title,
-                    description = description,
-                )
-            }
-            _uiState.tryEmit(TaskAddState.Success)
-        }
+  private val _uiState = MutableStateFlow<TaskAddUiState>(TaskAddUiState())
+  val uiState: StateFlow<TaskAddUiState> = _uiState.asStateFlow()
+
+  fun updateTitle(value: String) {
+    _uiState.update { old -> old.copy(title = value) }
+  }
+
+  fun updateDescription(value: String) {
+    _uiState.update { old -> old.copy(description = value) }
+  }
+
+  fun addTask() {
+    viewModelScope.launch {
+      _screenState.tryEmit(TaskAddScreenState.Loading)
+      withContext(dispatcher) {
+        val currentUiState = _uiState.value
+        val id =
+          defaultTaskRepository.create(
+            title =
+              currentUiState.title.ifEmpty {
+                return@withContext
+              },
+            description =
+              currentUiState.description.ifEmpty {
+                return@withContext
+              },
+          )
+      }
+      _screenState.tryEmit(TaskAddScreenState.Success)
     }
+  }
 }

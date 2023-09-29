76bb2d858b7f8cb2228717398624516f7cfefbac
Keel_im <kimh00335@gmail.com>
2023-09-30 00:37:28 +0900

    [mygrade] add clear function in repository interface
    introduce clear function to use in taskScreen handling

--- data/src/main/java/com/keelim/data/source/DefaultTaskRepository.kt
+++ data/src/main/java/com/keelim/data/source/DefaultTaskRepository.kt
@@ -8,4 +8,6 @@ interface DefaultTaskRepository {
     suspend fun create(title: String, description: String): String
     suspend fun complete(taskId: String)
     suspend fun refresh()
+
+    suspend fun clear()
 }

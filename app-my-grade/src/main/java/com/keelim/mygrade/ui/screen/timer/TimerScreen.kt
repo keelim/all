7f03b2ecc6f3a41344d796afe8a846046eba1b3e
Keel_im <kimh00335@gmail.com>
2023-10-01 17:07:28 +0900

    [mygrade] add timerScreen and navigation

---
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/timer/TimerScreen.kt
@@ -0,0 +1,21 @@
+package com.keelim.mygrade.ui.screen.timer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TimerRoute() {
+    TimerScreen()
+}
+
+@Composable
+fun TimerScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewTimerScreen() {
+    TimerScreen()
+
+}

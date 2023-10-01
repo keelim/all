a580a4561cf84f7832344c64292befb31b0a6cb1
Keel_im <kimh00335@gmail.com>
2023-10-01 23:00:21 +0900

    [mygrade] add mygrade ready service string and screen

--- app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/timer/history/TimerHistoryViewModel.kt
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/timer/history/TimerHistoryViewModel.kt
@@ -1,13 +1,12 @@
 package com.keelim.mygrade.ui.screen.timer.history
 
 import androidx.lifecycle.ViewModel
+import com.keelim.data.source.HistoryRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 
 @HiltViewModel
 class TimerHistoryViewModel @Inject constructor(
-
-): ViewModel() {
-
-}
+    val historyRepository: HistoryRepository
+): ViewModel()

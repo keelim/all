f626dec89e094c475a931b34dc463ddef0c9bbb0
Keel_im <kimh00335@gmail.com>
2023-10-01 16:45:54 +0900

    [mysenior] add my senior application class and navhost

--- app-mysenior/src/main/java/com/keelim/mysenior/MainActivity.kt
+++ app-mysenior/src/main/java/com/keelim/mysenior/MainActivity.kt
@@ -1,40 +1,22 @@
+@file:OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+
 package com.keelim.mysenior
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.keelim.composeutil.setThemeContent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setThemeContent {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                Greeting2("Android")
-            }
+            MySeniorApp(
+                windowSizeClass = calculateWindowSizeClass(this),
+            )
         }
     }
-}
-
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    Greeting2("Android")
 }

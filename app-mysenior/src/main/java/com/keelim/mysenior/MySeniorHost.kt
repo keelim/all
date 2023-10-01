625ffe47205b955d59cc0eca687173622ec2e083
Keel_im <kimh00335@gmail.com>
2023-10-01 16:45:54 +0900

    [mysenior] add my senior application class and navhost

---
+++ app-mysenior/src/main/java/com/keelim/mysenior/MySeniorHost.kt
@@ -0,0 +1,45 @@
+package com.keelim.mysenior
+
+import android.content.Intent
+import androidx.compose.material3.SheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.keelim.composeutil.AppState
+import com.keelim.setting.screen.event.eventScreen
+import com.keelim.setting.screen.navigateNotification
+import com.keelim.setting.screen.notificationScreen
+import com.keelim.setting.screen.settingsRoute
+import com.keelim.setting.screen.settingsScreen
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun MySeniorHost(
+    appState: AppState,
+    coroutineScope: CoroutineScope,
+    onShowSnackbar: suspend (String, String?) -> Boolean,
+    modifier: Modifier = Modifier,
+) {
+    val navController = appState.navController
+    val context = LocalContext.current
+    NavHost(
+        navController = navController,
+        startDestination = settingsRoute,
+        modifier = modifier,
+    ) {
+        settingsScreen(
+            onNotificationsClick = {
+                navController.navigateNotification()
+            },
+            onOpenSourceClick = {
+                context.startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+            },
+            nestedGraphs = {
+                notificationScreen()
+            },
+        )
+        eventScreen()
+    }
+}

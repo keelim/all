9cdc4365948f0a3c1455f286c39087c5a0b7f807
Keel_im <kimh00335@gmail.com>
2023-10-15 22:41:57 +0900

    [all] add error delegate in AppMainDelegator

--- app-cnubus/src/main/java/com/keelim/cnubus/utils/ViewModelDelegate.kt
+++ app-cnubus/src/main/java/com/keelim/cnubus/utils/ViewModelDelegate.kt
@@ -21,16 +21,4 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import javax.inject.Inject
 
-interface ErrorViewModel {
-    val error: SharedFlow<Throwable>
-    fun emitError(throwable: Throwable)
-}
 
-class ErrorViewModelImpl @Inject constructor() : ErrorViewModel {
-    private val _error = MutableSharedFlow<Throwable>()
-    override val error: SharedFlow<Throwable> = _error
-    override fun emitError(throwable: Throwable) {
-        Firebase.crashlytics.recordException(throwable)
-        _error.tryEmit(throwable)
-    }
-}

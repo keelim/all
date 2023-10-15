89872beb18b7d88f5a237d630694c73aa81913c5
Keel_im <kimh00335@gmail.com>
2023-10-15 23:44:56 +0900

    [all] introduce compose ui util fast collection api

--- core/compose-core/src/main/java/com/keelim/composeutil/screen/tab/TabScreen.kt
+++ core/compose-core/src/main/java/com/keelim/composeutil/screen/tab/TabScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.util.fastForEachIndexed
 
 @Composable
 fun TabScreen() {
@@ -32,7 +33,7 @@ fun TabScreen() {
         ScrollableTabRow(
             selectedTabIndex = tabIndex,
         ) {
-            tabs.forEachIndexed { index, title ->
+            tabs.fastForEachIndexed { index, title ->
                 Tab(
                     text = { Text(title) },
                     selected = tabIndex == index,

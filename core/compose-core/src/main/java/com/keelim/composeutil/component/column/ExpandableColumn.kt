2c4c1fbe83c9ac5650a4bfea41c5c708da91e3a5
Keel_im <kimh00335@gmail.com>
2023-10-25 01:32:53 +0900

    ExpandableColumn

---
+++ core/compose-core/src/main/java/com/keelim/composeutil/component/column/ExpandableColumn.kt
@@ -0,0 +1,39 @@
+package com.keelim.composeutil.component.column
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ExpandableColumn(
+    modifier: Modifier = Modifier,
+    title: String,
+    content: @Composable () -> Unit,
+) {
+    var isExpanded by remember { mutableStateOf(false) }
+    Column(
+        modifier = modifier
+            .clickable { isExpanded = !isExpanded }
+            .background(color = MaterialTheme.colorScheme.primaryContainer)
+            .fillMaxWidth()
+    ) {
+        Text(
+           text = title
+        )
+        AnimatedVisibility(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.secondaryContainer)
+                .fillMaxWidth(),
+            visible = isExpanded
+        ) {
+            content()
+        }
+    }
+}

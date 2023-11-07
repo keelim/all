0e6b2dd259a0a2c74b13d9fe0b730649fd4a0429
Keel_im <kimh00335@gmail.com>
2023-11-08 00:50:30 +0900

    [all] modify Initializer

--- core/common-android/src/main/java/com/keelim/commonAndroid/initialize/TimberInitializer.kt
+++ core/common-android/src/main/java/com/keelim/commonAndroid/initialize/TimberInitializer.kt
@@ -2,16 +2,18 @@ package com.keelim.commonAndroid.initialize
 
 import android.content.Context
 import androidx.startup.Initializer
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
 import com.keelim.commonAndroid.BuildConfig
 import com.keelim.commonAndroid.util.CrashlyticsTree
 import timber.log.Timber
 
 class TimberInitializer : Initializer<Unit> {
     override fun create(context: Context) {
-        Timber.plant(
-            if (BuildConfig.DEBUG) Timber.DebugTree() else CrashlyticsTree(),
-        )
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
-    override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
+    override fun dependencies(): List<Class<out Initializer<*>>>  = listOf()
 }

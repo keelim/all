1e281ea27879f1fc67b315115097760dad77945f
Keel_im <kimh00335@gmail.com>
2023-11-20 00:21:15 +0900

    [comssa] add ComssaApp and introduce comssa activity

--- app-comssa/build.gradle.kts
+++ app-comssa/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.compose.material.iconsExtended)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.windowSizeClass)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.ui.util)

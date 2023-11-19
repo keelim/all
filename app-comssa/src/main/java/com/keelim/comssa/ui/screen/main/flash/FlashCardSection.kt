ace95a8fc6d2f881872fe0eb0a0e41c4d1917f00
Keel_im <kimh00335@gmail.com>
2023-11-19 23:59:00 +0900

    [comssa] add flashcard route, section and screen

---
+++ app-comssa/src/main/java/com/keelim/comssa/ui/screen/main/flash/FlashCardSection.kt
@@ -0,0 +1,56 @@
+package com.keelim.comssa.ui.screen.main.flash
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun FrontCardSection() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = "Front",
+            color = Color.White,
+            fontSize = 32.sp,
+        )
+    }
+}
+
+@Composable
+fun BackCardSection() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = "Back",
+            color = Color.White,
+            fontSize = 32.sp,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFrontCardSection() {
+    FrontCardSection()
+}
+
+@Preview
+@Composable
+fun PreviewBackCardSection() {
+    BackCardSection()
+}

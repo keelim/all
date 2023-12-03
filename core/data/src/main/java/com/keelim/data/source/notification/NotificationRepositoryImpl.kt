86b3d54ebc99af68c41302a65f23f4dae6147057
Keel_im <kimh00335@gmail.com>
2023-12-04 00:52:48 +0900

    update api key

--- core/data/src/main/java/com/keelim/data/source/notification/NotificationRepositoryImpl.kt
+++ core/data/src/main/java/com/keelim/data/source/notification/NotificationRepositoryImpl.kt
@@ -7,22 +7,30 @@ import com.keelim.data.network.response.NotificationResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.url
+import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
-import javax.inject.Inject
 
-class NotificationRepositoryImpl @Inject constructor(
+class NotificationRepositoryImpl
+@Inject
+constructor(
     @KtorNetworkModule.KtorAndroidClient val client: HttpClient,
     @IoDispatcher private val dispatcher: CoroutineDispatcher,
 ) : NotificationRepository {
-    override suspend fun getNotification(): List<Notification> {
-        return withContext(dispatcher) {
-            val response: NotificationResponse = client.use {
-                it.get(BuildConfig.NOTIFICATION_URL).body()
-            }
-            response.values.map { (date, title, desc) ->
-                Notification(date = date, title = title, desc = desc)
-            }
-        }
+  override suspend fun getNotification(): List<Notification> {
+    return withContext(dispatcher) {
+      client
+          .use<HttpClient, NotificationResponse> {
+            it.get {
+                  url("${BuildConfig.NOTIFICATION_URL}/notifications")
+                  parameter("key", BuildConfig.SHEET_KEY)
+                }
+                .body()
+          }
+          .values
+          .map { (date, title, desc) -> Notification(date = date, title = title, desc = desc) }
     }
+  }
 }

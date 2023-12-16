8f3f646a994053e9b00a9c63be45acb4ae11730f
Keel_im <kimh00335@gmail.com>
2023-12-16 21:38:08 +0900

    [ecol] seperate screen and section

--- features/ui-setting/src/main/java/com/keelim/ecocal/EcocalScreen.kt
+++ features/ui-setting/src/main/java/com/keelim/ecocal/EcocalScreen.kt
@@ -1,21 +1,8 @@
 package com.keelim.ecocal
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.fastForEach
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.keelim.commonAndroid.model.SealedUiState
@@ -34,27 +21,8 @@ fun EcocalScreen(state: SealedUiState<EcocalEntries>) {
   when (state) {
     is SealedUiState.Error,
     SealedUiState.Loading -> EmptyView()
-    is SealedUiState.Success -> EcocalSuccessSection(state.value)
+    is SealedUiState.Success -> EcocalMainSection(state.value)
   }
-}
-
-@Composable
-fun EcocalSuccessSection(entries: EcocalEntries) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        items(entries.entries) {entry ->
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp, horizontal = 8.dp)
-            ) {
-                Text(text = entry.toString())
-            }
-            Divider(
-                thickness = 1.dp
-            )
-        }
-    }
 }
 
 @Preview(showBackground = true)

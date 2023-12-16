c63106f76fb70b8f064631d425fb683bdc5e6a53
Keel_im <kimh00335@gmail.com>
2023-12-17 03:19:28 +0900

    [comssa] package 정리

--- app-comssa/src/main/java/com/keelim/comssa/ui/screen/main/ecocal/EcocalViewModel.kt
+++ app-comssa/src/main/java/com/keelim/comssa/ui/screen/main/ecocal/EcocalViewModel.kt
@@ -1,27 +1,32 @@
-package com.keelim.comssa.ui.screen.ecocal
+package com.keelim.comssa.ui.screen.main.ecocal
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.keelim.commonAndroid.model.SealedUiState
 import com.keelim.commonAndroid.model.asSealedUiState
-import com.keelim.data.source.firebase.EcocalEntries
 import com.keelim.data.source.firebase.FirebaseRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltViewModel
 class EcocalViewModel @Inject constructor(
-    val firebaseRepository: FirebaseRepository,
+    firebaseRepository: FirebaseRepository,
 ) : ViewModel() {
     val ref = firebaseRepository
-        .getRef("update")
+        .getRef("2023-12")
         .mapLatest { result ->
             Timber.d("result $result")
-            result.getOrNull() ?: EcocalEntries(emptyList())
+            result.getOrThrow()
+        }.catch { throwable ->
+            Timber.e(throwable)
+            emitAll(emptyFlow())
         }
         .asSealedUiState()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), SealedUiState.loading())

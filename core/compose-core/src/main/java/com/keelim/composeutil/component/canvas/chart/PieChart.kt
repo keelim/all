60bb3002bb8f0ff38631e799784c60d9c44c55b1
Keel_im <kimh00335@gmail.com>
2023-12-16 15:26:31 +0900

    [all] update pie chart

--- core/compose-core/src/main/java/com/keelim/composeutil/component/canvas/chart/PieChart.kt
+++ core/compose-core/src/main/java/com/keelim/composeutil/component/canvas/chart/PieChart.kt
@@ -1,13 +1,29 @@
 package com.keelim.composeutil.component.canvas.chart
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEachIndexed
 import com.keelim.composeutil.util.randomColor
@@ -32,21 +48,61 @@ private fun calculateStartAngles(
 @Composable
 fun PieChart(
     entries: List<PieChartEntry>,
+    radiusOuter: Dp,
+    chartBarWidth: Dp,
+    duration: Int,
     modifier: Modifier = Modifier,
 ) {
-    Canvas(
-        modifier = modifier.size(300.dp),
+    var isPlayed by remember { mutableStateOf(false) }
+    val animateSize by animateFloatAsState(
+        targetValue = if(isPlayed) radiusOuter.value *  2f else 0f,
+        animationSpec = tween(
+            durationMillis = duration,
+            delayMillis = 0,
+            easing = LinearOutSlowInEasing
+        ),
+        label = ""
+    )
+
+    val animateRotation by animateFloatAsState(
+        targetValue = if (isPlayed) 90 * 11f else 0f,
+        animationSpec = tween(
+            durationMillis = duration,
+            delayMillis = 0,
+            easing = LinearOutSlowInEasing
+        ),
+        label = ""
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        isPlayed = true
+    }
+
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val start = calculateStartAngles(entries)
-        entries.fastForEachIndexed { index, entry ->
-            drawArc(
-                color = entry.color,
-                startAngle = start[index],
-                sweepAngle = entry.percentage * 360f,
-                useCenter = true,
-                topLeft = Offset.Zero,
-                size = this.size,
-            )
+        Box(
+            modifier = Modifier.size(animateSize.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Canvas(
+                modifier = Modifier
+                    .size(radiusOuter * 2f)
+                    .rotate(animateRotation),
+            ) {
+                val start = calculateStartAngles(entries)
+                entries.fastForEachIndexed { index, entry ->
+                    drawArc(
+                        color = entry.color,
+                        startAngle = start[index],
+                        sweepAngle = entry.percentage * 360f,
+                        useCenter = true,
+                        size = this.size,
+                        style = Stroke(chartBarWidth.toPx()),
+                    )
+                }
+            }
         }
     }
 }
@@ -60,5 +116,6 @@ fun PreviewPieChart() {
             PieChartEntry(randomColor(), 0.3f),
             PieChartEntry(randomColor(), 0.2f),
         ),
+        radiusOuter =20.dp, chartBarWidth =20.dp, duration = 5892,
     )
 }

bd30ae331173ba50b1cbdc9f70622a99ceced75c
Keel_im <kimh00335@gmail.com>
2023-12-17 03:19:44 +0900

    [all] 상수에 따른 애뮬레이터 옵션 적용

--- app-comssa/src/debug/java/com/keelim/comssa/initialize/FirestoreInitializer.kt
+++ app-comssa/src/debug/java/com/keelim/comssa/initialize/FirestoreInitializer.kt
@@ -7,21 +7,24 @@ import com.google.firebase.database.database
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.firestoreSettings
 
+internal const val isEmulatorEnable = false
 class FirestoreInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
-        // 10.0.2.2 is the special IP address to connect to the 'localhost' of
-        // the host computer from an Android emulator.
-        val firestore = Firebase.firestore
-        firestore.useEmulator("10.0.2.2", 8080)
+        if(isEmulatorEnable) {
+            // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+            // the host computer from an Android emulator.
+            val firestore = Firebase.firestore
+            firestore.useEmulator("10.0.2.2", 8080)
 
-        firestore.firestoreSettings = firestoreSettings {
-            isPersistenceEnabled = false
+            firestore.firestoreSettings = firestoreSettings {
+                isPersistenceEnabled = false
+            }
+            // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+            // the host computer from an Android emulator.
+            val database = Firebase.database
+            database.useEmulator("10.0.2.2", 9000)
         }
-        // 10.0.2.2 is the special IP address to connect to the 'localhost' of
-        // the host computer from an Android emulator.
-        val database = Firebase.database
-        database.useEmulator("10.0.2.2", 9000)
     }
 
     override fun dependencies(): List<Class<Initializer<*>>> = listOf()

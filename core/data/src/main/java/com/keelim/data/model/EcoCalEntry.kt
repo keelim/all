54663efe9483678a562f9d2c1166ee0009f8ab21
Keel_im <kimh00335@gmail.com>
2023-12-17 03:18:14 +0900

    [comssa] comssa data layer 수정

---
+++ core/data/src/main/java/com/keelim/data/model/EcoCalEntry.kt
@@ -0,0 +1,9 @@
+package com.keelim.data.model
+
+data class EcoCalEntry(
+    val country: String = "",
+    val date: String = "",
+    val priority: String = "",
+    val time: String = "",
+    val title: String = "",
+)
--- core/data/src/main/java/com/keelim/data/source/firebase/FirebaseRepositoryImpl.kt
+++ core/data/src/main/java/com/keelim/data/source/firebase/FirebaseRepositoryImpl.kt
@@ -1,27 +1,45 @@
 package com.keelim.data.source.firebase
 
 import com.google.firebase.Firebase
+import com.google.firebase.database.Logger
 import com.google.firebase.database.database
+import com.keelim.data.BuildConfig
+import com.keelim.data.di.IoDispatcher
+import com.keelim.data.model.EcoCalEntry
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import timber.log.Timber
-import javax.inject.Inject
 
-class FirebaseRepositoryImpl @Inject constructor() : FirebaseRepository {
-    override fun getRef(ref: String): Flow<Result<EcocalEntries>> = callbackFlow {
-        Timber.d("[lab] 요청 들어감")
-        Firebase.database
-            .getReference(ref)
-            .get()
-            .addOnSuccessListener { snapshot ->
-                Timber.d("[lab] snapshot $snapshot")
-                trySend(
-                    runCatching { snapshot.getValue(EcocalEntries::class.java) ?: EcocalEntries(emptyList()) },
-                )
+class FirebaseRepositoryImpl
+@Inject
+constructor(@IoDispatcher val dispatcher: CoroutineDispatcher) : FirebaseRepository {
+  override fun getRef(ref: String): Flow<Result<List<EcoCalEntry>>> = callbackFlow {
+    val database =
+        Firebase.database.apply {
+          if (BuildConfig.DEBUG) {
+            setLogLevel(Logger.Level.DEBUG)
+          }
+          setPersistenceEnabled(true)
+        }
+    trySend(
+        runCatching {
+              withContext(dispatcher) {
+                database
+                    .getReference(ref)
+                    .get()
+                    .await()
+                    .takeIf { it.exists() }
+                    ?.children
+                    ?.mapNotNull { it.getValue(EcoCalEntry::class.java) } ?: emptyList()
+              }
             }
-            .addOnFailureListener { error ->
-                Timber.e(error.message)
-                trySend(Result.failure(IllegalStateException("Firebase Error ${error.message}")))
-            }
-    }
+            .onFailure { throwable ->
+              Timber.e(throwable)
+              throwable.message
+            })
+  }
 }

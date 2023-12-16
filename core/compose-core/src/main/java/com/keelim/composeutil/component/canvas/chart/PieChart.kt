d1224c9ebec26430d21c722370721408eaf1f21c
Keel_im <kimh00335@gmail.com>
2023-12-16 16:25:30 +0900

    [mygrade] modify chart entry

--- core/compose-core/src/main/java/com/keelim/composeutil/component/canvas/chart/PieChart.kt
+++ core/compose-core/src/main/java/com/keelim/composeutil/component/canvas/chart/PieChart.kt
@@ -18,9 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -30,6 +28,7 @@ import com.keelim.composeutil.util.randomColor
 
 @Stable
 data class PieChartEntry(
+    val name: String,
     val color: Color,
     val percentage: Float,
 )
@@ -37,12 +36,12 @@ data class PieChartEntry(
 private fun calculateStartAngles(
     entries: List<PieChartEntry>,
 ): List<Float> {
-    var totalPercentage = 0f
-    return entries.map { entry ->
-        val startAngle = totalPercentage * 360
-        totalPercentage += entry.percentage
-        startAngle
-    }
+  var totalPercentage = 0f
+  return entries.map { entry ->
+    val startAngle = totalPercentage * 360
+    totalPercentage += entry.percentage
+    startAngle
+  }
 }
 
 @Composable
@@ -53,69 +52,55 @@ fun PieChart(
     duration: Int,
     modifier: Modifier = Modifier,
 ) {
-    var isPlayed by remember { mutableStateOf(false) }
-    val animateSize by animateFloatAsState(
-        targetValue = if(isPlayed) radiusOuter.value *  2f else 0f,
-        animationSpec = tween(
-            durationMillis = duration,
-            delayMillis = 0,
-            easing = LinearOutSlowInEasing
-        ),
-        label = ""
-    )
+  var isPlayed by remember { mutableStateOf(false) }
+  val animateSize by
+      animateFloatAsState(
+          targetValue = if (isPlayed) radiusOuter.value * 2f else 0f,
+          animationSpec =
+              tween(durationMillis = duration, delayMillis = 0, easing = LinearOutSlowInEasing),
+          label = "")
 
-    val animateRotation by animateFloatAsState(
-        targetValue = if (isPlayed) 90 * 11f else 0f,
-        animationSpec = tween(
-            durationMillis = duration,
-            delayMillis = 0,
-            easing = LinearOutSlowInEasing
-        ),
-        label = ""
-    )
+  val animateRotation by
+      animateFloatAsState(
+          targetValue = if (isPlayed) 90 * 11f else 0f,
+          animationSpec =
+              tween(durationMillis = duration, delayMillis = 0, easing = LinearOutSlowInEasing),
+          label = "")
 
-    LaunchedEffect(key1 = Unit) {
-        isPlayed = true
-    }
+  LaunchedEffect(key1 = Unit) { isPlayed = true }
 
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = Modifier.size(animateSize.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Canvas(
-                modifier = Modifier
-                    .size(radiusOuter * 2f)
-                    .rotate(animateRotation),
-            ) {
-                val start = calculateStartAngles(entries)
-                entries.fastForEachIndexed { index, entry ->
-                    drawArc(
-                        color = entry.color,
-                        startAngle = start[index],
-                        sweepAngle = entry.percentage * 360f,
-                        useCenter = true,
-                        size = this.size,
-                        style = Stroke(chartBarWidth.toPx()),
-                    )
-                }
-            }
+  Column(modifier = modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+    Box(modifier = Modifier.size(animateSize.dp), contentAlignment = Alignment.Center) {
+      Canvas(
+          modifier = Modifier.size(radiusOuter * 2f).rotate(animateRotation),
+      ) {
+        val start = calculateStartAngles(entries)
+        entries.fastForEachIndexed { index, entry ->
+          drawArc(
+              color = entry.color,
+              startAngle = start[index],
+              sweepAngle = entry.percentage * 360f,
+              useCenter = true,
+              size = this.size,
+              style = Stroke(chartBarWidth.toPx()),
+          )
         }
+      }
     }
+  }
 }
 
 @Preview
 @Composable
 fun PreviewPieChart() {
-    PieChart(
-        listOf(
-            PieChartEntry(randomColor(), 0.5f),
-            PieChartEntry(randomColor(), 0.3f),
-            PieChartEntry(randomColor(), 0.2f),
-        ),
-        radiusOuter =20.dp, chartBarWidth =20.dp, duration = 5892,
-    )
+  PieChart(
+      listOf(
+          PieChartEntry(name = "1", randomColor(), 0.5f),
+          PieChartEntry(name = "1", randomColor(), 0.3f),
+          PieChartEntry(name = "1", randomColor(), 0.2f),
+      ),
+      radiusOuter = 20.dp,
+      chartBarWidth = 20.dp,
+      duration = 5892,
+  )
 }

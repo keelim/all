89463f1d84f9bfa4b27bd58c593a8d6c4567dd0c
Keel_im <kimh00335@gmail.com>
2023-12-23 01:54:05 +0900

    [all] apply spotless

--- core/compose-core/src/main/java/com/keelim/composeutil/demo/scaffold/ScaffoldDemo.kt
+++ core/compose-core/src/main/java/com/keelim/composeutil/demo/scaffold/ScaffoldDemo.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
--- core/data/src/main/java/com/keelim/data/source/firebase/FirebaseRepository.kt
+++ core/data/src/main/java/com/keelim/data/source/firebase/FirebaseRepository.kt
@@ -3,7 +3,6 @@ package com.keelim.data.source.firebase
 import com.keelim.data.model.EcoCalEntry
 import kotlinx.coroutines.flow.Flow
 
-
 interface FirebaseRepository {
     fun getRef(ref: String): Flow<Result<List<EcoCalEntry>>>
 }

4b9be8c5c6c31613eb87ca5151117cb81c3d4a98
Keel_im <kimh00335@gmail.com>
2023-12-22 23:58:34 +0900

    [all] delete unused function and file

--- core/common/src/main/java/com/keelim/common/extensions/ActivityExt.kt
+++ core/common/src/main/java/com/keelim/common/extensions/ActivityExt.kt
@@ -7,16 +7,6 @@ import android.view.View
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 
-fun Activity.transparentStatusBar() {
-    window.apply {
-        clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        statusBarColor = Color.TRANSPARENT
-    }
-}
-
 fun Activity.hideKeyboard() {
     val imm: InputMethodManager = getSystemService(InputMethodManager::class.java)
     val view = currentFocus ?: View(this)

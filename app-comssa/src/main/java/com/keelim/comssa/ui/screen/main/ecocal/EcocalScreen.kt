f38a2e9f52ff3210e8449ab5dab16d5eb07ef292
Keel_im <kimh00335@gmail.com>
2023-12-25 00:32:48 +0900

    [comssa] update filter

--- app-comssa/src/main/java/com/keelim/comssa/ui/screen/main/ecocal/EcocalScreen.kt
+++ app-comssa/src/main/java/com/keelim/comssa/ui/screen/main/ecocal/EcocalScreen.kt
@@ -1,36 +1,115 @@
 package com.keelim.comssa.ui.screen.main.ecocal
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.keelim.commonAndroid.model.SealedUiState
+import com.keelim.composeutil.component.fab.FabButtonItem
+import com.keelim.composeutil.component.fab.FabButtonMain
+import com.keelim.composeutil.component.fab.FabButtonState
+import com.keelim.composeutil.component.fab.FabButtonSub
+import com.keelim.composeutil.component.fab.MultiMainFab
 import com.keelim.composeutil.component.layout.EmptyView
 import com.keelim.data.model.EcoCalEntry
+import timber.log.Timber
 
 @Composable
 fun EcocalRoute(viewModel: EcocalViewModel = hiltViewModel()) {
-    val uiState by viewModel.ref.collectAsStateWithLifecycle()
-    EcocalScreen(uiState)
+    val uiState by viewModel.items.collectAsStateWithLifecycle()
+    EcocalScreen(
+        uiState = uiState,
+        updateFilter = viewModel::updateFilter,
+    )
 }
 
 @Composable
-fun EcocalScreen(state: SealedUiState<List<EcoCalEntry>>) {
-    when (state) {
+fun EcocalScreen(
+    uiState: SealedUiState<List<EcoCalEntry>>,
+    updateFilter: (FabButtonItem) -> Unit,
+) {
+    when (uiState) {
         is SealedUiState.Error,
         SealedUiState.Loading,
         -> EmptyView()
-        is SealedUiState.Success -> EcocalMainSection(state.value)
+        is SealedUiState.Success -> {
+            Scaffold(
+                floatingActionButton = {
+                    var fabState by remember { mutableStateOf<FabButtonState>(FabButtonState.Collapsed) }
+                    val items by remember {
+                        mutableStateOf(
+                            listOf(
+                                High(),
+                                Medium(),
+                                Low(),
+                                All()
+                            ),
+                        )
+                    }
+                    MultiMainFab(
+                        fabState = fabState,
+                        items = items,
+                        fabIcon = FabButtonMain(),
+                        fabOption = FabButtonSub(
+                            backgroundTint = MaterialTheme.colorScheme.primary,
+                            iconTint = MaterialTheme.colorScheme.onPrimary,
+                        ),
+                        onFabItemClicked = { item ->
+                            Timber.d("item $item")
+                            updateFilter(item)
+                            fabState = fabState.toggleValue()
+                        },
+                        stateChanged = {
+                            fabState = it
+                        },
+                    )
+                }
+            ) { paddingValues ->
+                EcocalMainSection(
+                    entries = uiState.value,
+                    modifier = Modifier.padding(paddingValues)
+                )
+            }
+        }
     }
 }
+
+data class High(
+    override val imageVector: ImageVector = Icons.Filled.List,
+    override val label: String = "상",
+) : FabButtonItem
+
+data class Medium(
+    override val imageVector: ImageVector = Icons.Filled.List,
+    override val label: String = "중",
+) : FabButtonItem
+
+data class Low(
+    override val imageVector: ImageVector = Icons.Filled.List,
+    override val label: String = "하",
+) : FabButtonItem
+
+data class All(
+    override val imageVector: ImageVector = Icons.Filled.List,
+    override val label: String = "전체",
+) : FabButtonItem
 
 @Preview(showBackground = true)
 @Composable
 private fun PreviewEcocalScreen() {
     EcocalScreen(
-        state =
-        SealedUiState.success(
+        uiState = SealedUiState.success(
             listOf(
                 EcoCalEntry(
                     country = "Congo, Democratic Republic of the",
@@ -41,5 +120,6 @@ private fun PreviewEcocalScreen() {
                 ),
             ),
         ),
+        updateFilter = {},
     )
 }

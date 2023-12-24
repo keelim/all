ebe4533996ffb0c2f7f484f14c8e881e83e499e1
Keel_im <kimh00335@gmail.com>
2023-12-25 00:32:48 +0900

    [comssa] update filter

--- app-comssa/src/main/java/com/keelim/comssa/ui/screen/main/ecocal/EcocalViewModel.kt
+++ app-comssa/src/main/java/com/keelim/comssa/ui/screen/main/ecocal/EcocalViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.keelim.commonAndroid.model.SealedUiState
 import com.keelim.commonAndroid.model.asSealedUiState
+import com.keelim.composeutil.component.fab.FabButtonItem
 import com.keelim.data.source.firebase.FirebaseRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
@@ -14,20 +15,41 @@ import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
 import timber.log.Timber
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.update
 
 @HiltViewModel
 class EcocalViewModel @Inject constructor(
     firebaseRepository: FirebaseRepository,
 ) : ViewModel() {
-    val ref = firebaseRepository
-        .getRef("2023-12")
+    private val ref = firebaseRepository
+        .getRef("ecocal")
         .mapLatest { result ->
             Timber.d("result $result")
             result.getOrThrow()
-        }.catch { throwable ->
-            Timber.e(throwable)
-            emitAll(emptyFlow())
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+
+    private val filter = MutableStateFlow<FabButtonItem>(All())
+    val items = combine(ref, filter) { data, filter ->
+        Timber.d("data $data filter $filter")
+        val sample = when (filter) {
+            is All -> data
+            is High -> data.filter { it.priority == "상" }
+            is Medium -> data.filter { it.priority == "중" }
+            is Low -> data.filter { it.priority == "하" }
+            else -> emptyList()
         }
+        sample
+    }.catch { throwable ->
+        Timber.e(throwable)
+        emitAll(emptyFlow())
+    }
         .asSealedUiState()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), SealedUiState.loading())
+
+    fun updateFilter(item: FabButtonItem) {
+        filter.update { item }
+        Timber.d("lab filter ${filter.value}")
+    }
 }

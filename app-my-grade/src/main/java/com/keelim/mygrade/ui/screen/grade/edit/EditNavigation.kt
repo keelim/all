0030e6d43cb47fa159f1451ace8252973ef5934b
Keel_im <kimh00335@gmail.com>
2024-02-09 23:26:21 +0900

    [mygrade] add note screen

---
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/grade/edit/EditNavigation.kt
@@ -0,0 +1,29 @@
+package com.keelim.mygrade.ui.screen.grade.edit
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.keelim.mygrade.ui.screen.grade.GradeRoute
+import com.keelim.mygrade.ui.screen.grade.gradeRoute
+
+const val editRoute = "$gradeRoute/edit"
+fun NavController.navigateEdit(
+    subject: String,
+    navOptions: NavOptions? = null,
+) {
+    this.navigate("$editRoute?subject=$subject", navOptions)
+}
+
+fun NavGraphBuilder.editScreen() {
+    composable(
+        route = "$editRoute?subject={subject}",
+        arguments =
+        listOf(
+            navArgument("subject") { defaultValue = "" },
+        ),
+    ) {
+
+    }
+}

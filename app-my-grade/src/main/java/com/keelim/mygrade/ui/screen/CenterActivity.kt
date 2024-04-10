dffee6cd95a72fc60c3ffa68c6a641131f040e87
Keel_im <kimh00335@gmail.com>
2024-04-10 22:27:24 +0900

    [all] delete unused app main viewmodel concept

--- app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/CenterActivity.kt
+++ app-my-grade/src/main/java/com/keelim/mygrade/ui/screen/CenterActivity.kt
@@ -3,15 +3,12 @@
 package com.keelim.mygrade.ui.screen
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.core.os.BuildCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.airbnb.deeplinkdispatch.DeepLink
-import com.keelim.commonAndroid.core.AppMainDelegator
-import com.keelim.commonAndroid.core.AppMainViewModel
 import com.keelim.composeutil.setThemeContent
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
@@ -20,8 +17,6 @@ import timber.log.Timber
 @DeepLink("all://screen/{name}")
 @AndroidEntryPoint
 class CenterActivity : AppCompatActivity() {
-    private val viewModel: AppMainViewModel by viewModels()
-    private val appMainDelegator by lazy { AppMainDelegator(this, viewModel) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()

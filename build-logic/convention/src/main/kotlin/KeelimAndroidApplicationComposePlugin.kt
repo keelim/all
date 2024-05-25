eb9a4777af7714b4fb8421e71162b68d2a3fd7ff
Keel_im <kimh00335@gmail.com>
2024-05-25 14:42:54 +0900

    #920 move compose compiler gradle plugin

--- build-logic/convention/src/main/kotlin/KeelimAndroidApplicationComposePlugin.kt
+++ build-logic/convention/src/main/kotlin/KeelimAndroidApplicationComposePlugin.kt
@@ -1,15 +1,23 @@
+
 import com.android.build.api.dsl.ApplicationExtension
+import com.keelim.builds.configureAndroidCompose
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getByType
-import com.keelim.builds.configureAndroidCompose
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 class KeelimAndroidApplicationComposePlugin : Plugin<Project> {
     override fun apply(project: Project) {
         with(project) {
-            pluginManager.apply("com.android.application")
-            extensions.getByType<ApplicationExtension>()
-                .also(::configureAndroidCompose)
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.plugin.compose")
+            }
+
+            configureAndroidCompose(
+                commonExtension = extensions.getByType<ApplicationExtension>(),
+                composeCompilerGradlePluginExtension = extensions.getByType<ComposeCompilerGradlePluginExtension>()
+            )
         }
     }
 }

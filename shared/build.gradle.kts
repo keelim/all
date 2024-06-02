6907f477416c81082cd190baf1f753c0cbe10b46
Keel_im <kimh00335@gmail.com>
2024-06-02 15:30:49 +0900

    Revert "#927 add compose resource"
    This reverts commit a0bc5b7db73edee13b4f1ff608dc6729f884a75a.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.jetbrainsCompose)
-    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.android.library)
     alias(libs.plugins.sqldelight)
     kotlin("plugin.serialization")
@@ -35,7 +33,6 @@ kotlin {
             api(libs.androidx.dataStore.preferences)
             api(libs.androidx.dataStore.core.okio)
             implementation(libs.okio)
-            implementation(compose.components.resources)
         }
         appleMain.dependencies {
             implementation(libs.sqldelight.native)
@@ -57,10 +54,4 @@ android {
     defaultConfig {
         minSdk = libs.versions.minSdk.get().toInt()
     }
-}
-
-compose.resources {
-    publicResClass = true
-    packageOfResClass = "com.keelim.res"
-    generateResClass = auto
 }

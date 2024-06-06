a7215047c3ee1a34371aa7c82076eb61bb58ebda
Keel_im <kimh00335@gmail.com>
2024-06-06 21:24:44 +0900

    [comssa] display stickyheade by date

--- app-comssa/src/main/java/com/keelim/comssa/ui/screen/main/ecocal/EcocalViewModel.kt
+++ app-comssa/src/main/java/com/keelim/comssa/ui/screen/main/ecocal/EcocalViewModel.kt
@@ -8,12 +8,15 @@ import com.keelim.commonAndroid.model.asSealedUiState
 import com.keelim.composeutil.component.fab.FabButtonItem
 import com.keelim.core.data.source.firebase.FirebaseRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -40,15 +43,17 @@ class EcocalViewModel @Inject constructor(
     private val filter = MutableStateFlow<FabButtonItem>(All())
     val items = combine(ref, filter) { data, filter ->
         Timber.d("data $data filter $filter")
-        val sample = when (filter) {
+        when (filter) {
             is All -> data
             is High -> data.filter { it.priority == EcocalPriority.HIGH }
             is Medium -> data.filter { it.priority == EcocalPriority.MEDIUM }
             is Low -> data.filter { it.priority == EcocalPriority.LOW }
             else -> emptyList()
         }
-        sample
-    }.asSealedUiState()
+    }.map {
+        it.groupBy { it.date }
+    }.flowOn(Dispatchers.Default)
+        .asSealedUiState()
         .catch { throwable ->
             Timber.e(throwable)
             emitAll(emptyFlow())

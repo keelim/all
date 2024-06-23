c76b1f0a7f0cd7bc66fbb54724c8afbe14ed724e
Keel_im <kimh00335@gmail.com>
2024-06-24 02:24:35 +0900

    feat: add main screen
    This commit introduces the main screen for the application.
    
    - Adds a new ArduConHost composable to handle navigation.
    
    - Creates a MainNavigation composable for defining the main route.
    - Updates MainActivity to use the ArduConApp composable.
    - Implements the MainScreen composable with basic functionality.

--- app-arducon/src/main/java/com/keelim/arducon/main/MainActivity.kt
+++ app-arducon/src/main/java/com/keelim/arducon/main/MainActivity.kt
@@ -1,49 +1,22 @@
 package com.keelim.arducon.main
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
-import com.keelim.arducon.ui.screen.main.MainRoute
-import com.keelim.arducon.ui.screen.main.MainViewModel
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import com.keelim.arducon.ui.ArduconApp
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.filterNot
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initViewModelCallback()
         setContent {
-            MainRoute(
-                viewModel = viewModel,
+            ArduconApp(
+                windowSizeClass = calculateWindowSizeClass(this)
             )
         }
-    }
-
-    private fun initViewModelCallback() {
-        viewModel.onClickSearch
-            .flowWithLifecycle(lifecycle)
-            .filterNot { it.isEmpty() }
-            .onEach { uri ->
-                try {
-                    Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse(uri),
-                    ).let { startActivity(it) }
-                    viewModel.clear()
-                } catch (throwable: Throwable) {
-                    Toast.makeText(this, "Exception !!!\n" + e.message.toString(), Toast.LENGTH_SHORT).show()
-                }
-            }.launchIn(lifecycleScope)
     }
 }

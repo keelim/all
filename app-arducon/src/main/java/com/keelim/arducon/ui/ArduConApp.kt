89fbe8606a4e8362603ccf65b87e4726c0315b8c
Keel_im <kimh00335@gmail.com>
2024-06-24 02:24:35 +0900

    feat: add main screen
    This commit introduces the main screen for the application.
    
    - Adds a new ArduConHost composable to handle navigation.
    
    - Creates a MainNavigation composable for defining the main route.
    - Updates MainActivity to use the ArduConApp composable.
    - Implements the MainScreen composable with basic functionality.

---
+++ app-arducon/src/main/java/com/keelim/arducon/ui/ArduConApp.kt
@@ -0,0 +1,63 @@
+package com.keelim.arducon.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import com.keelim.composeutil.AppState
+import com.keelim.composeutil.rememberAppState
+
+@Composable
+fun ArduconApp(
+    windowSizeClass: WindowSizeClass,
+    appState: AppState = rememberAppState(windowSizeClass = windowSizeClass),
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    val bottomSheetState = rememberModalBottomSheetState()
+    Scaffold(
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+    ) { padding ->
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .consumeWindowInsets(padding)
+                .windowInsetsPadding(
+                    WindowInsets.safeDrawing.only(
+                        WindowInsetsSides.Horizontal,
+                    ),
+                ),
+        ) {
+            ArduConHost(
+                appState = appState,
+                coroutineScope = coroutineScope,
+                bottomSheetState = bottomSheetState,
+                onShowSnackbar = { message, action ->
+                    snackbarHostState.showSnackbar(
+                        message = message,
+                        actionLabel = action,
+                        duration = SnackbarDuration.Short,
+                    ) == SnackbarResult.ActionPerformed
+                },
+            )
+        }
+    }
+}
--- app-arducon/src/main/java/com/keelim/arducon/ui/screen/main/MainScreen.kt
+++ app-arducon/src/main/java/com/keelim/arducon/ui/screen/main/MainScreen.kt
@@ -1,5 +1,8 @@
 package com.keelim.arducon.ui.screen.main
 
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,16 +19,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.keelim.composeutil.resource.space8
 import com.keelim.core.database.model.DeepLink
+
 
 @Composable
 fun MainRoute(
@@ -34,6 +40,27 @@ fun MainRoute(
     val items by viewModel.deepLinkList.collectAsStateWithLifecycle(
         lifecycleOwner = androidx.compose.ui.platform.LocalLifecycleOwner.current,
     )
+    val isSearched = viewModel.onClickSearch.collectAsStateWithLifecycle(
+        lifecycleOwner = androidx.compose.ui.platform.LocalLifecycleOwner.current,
+    )
+
+    val context = LocalContext.current
+    LaunchedEffect(isSearched) {
+        try {
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(isSearched.value),
+            ).let { context.startActivity(it) }
+            viewModel.clear()
+        } catch (throwable: Throwable) {
+            Toast.makeText(
+                context,
+                "Exception !!!\n" + throwable.message.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     MainScreen(
         items = items,
         onSearch = viewModel::onClickSearch,

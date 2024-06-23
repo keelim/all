3899044e3204b402898d4ee14f8c6debe6cbc9a0
Keel_im <kimh00335@gmail.com>
2024-06-24 02:24:35 +0900

    feat: add main screen
    This commit introduces the main screen for the application.
    
    - Adds a new ArduConHost composable to handle navigation.
    
    - Creates a MainNavigation composable for defining the main route.
    - Updates MainActivity to use the ArduConApp composable.
    - Implements the MainScreen composable with basic functionality.

---
+++ app-arducon/src/main/java/com/keelim/arducon/ui/screen/main/MainNavigation.kt
@@ -0,0 +1,23 @@
+package com.keelim.arducon.ui.screen.main
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+
+const val mainRoute = "main"
+
+fun NavController.navigateMain(navOptions: NavOptions? = null) {
+    this.navigate(mainRoute, navOptions)
+}
+
+fun NavGraphBuilder.mainScreen(
+    nestedGraphs: NavGraphBuilder.() -> Unit,
+) {
+    composable(
+        route = mainRoute,
+    ) {
+        MainRoute()
+    }
+    nestedGraphs()
+}

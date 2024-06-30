efd843016751bacf880d63bab415dd69a94d1f14
Keel_im <kimh00335@gmail.com>
2024-06-30 20:33:32 +0900

    feat(data): Add updateDeepLinkUrl function
    This commit adds a new function `updateDeepLinkUrl` to the `ArduconRepository` and
     its implementation. This function allows updating an existing deep link URL in the database.
    
    The following changes were made:
    
    - Added `updateDeepLinkUrl` function to `ArduconRepository` interface.
    - Implemented `updateDeepLinkUrl` function in `ArduconRepositoryImpl`.
    - Added
     `updateDeepLinkUrl` function to `ArduconDataSource` interface.
    - Implemented `updateDeepLinkUrl` function in `ArduconDataSourceImpl`.

--- core/data/src/main/java/com/keelim/core/data/source/local/ArduconDataSource.kt
+++ core/data/src/main/java/com/keelim/core/data/source/local/ArduconDataSource.kt
@@ -7,4 +7,5 @@ interface ArduconDataSource {
     suspend fun insertDeepLinkUrl(deepLink: DeepLink)
     fun getDeepLinkUrls(): Flow<List<DeepLink>>
     suspend fun deleteDeepLinkUrl(deepLink: DeepLink)
+    suspend fun updateDeepLinkUrl(deepLink: DeepLink)
 }

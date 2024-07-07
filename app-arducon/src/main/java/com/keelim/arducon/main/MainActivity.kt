e8d03b15435916a607f074e8e763dbff2e7f79b3
Keel_im <kimh00335@gmail.com>
2024-07-07 11:14:38 +0900

    Refactor: Use setThemeContent from composeutil
    This commit refactors the MainActivity to use the `setThemeContent` function from the `composeutil`
     library. This simplifies the code and provides a consistent way to set the theme for composable content.

--- app-arducon/src/main/java/com/keelim/arducon/main/MainActivity.kt
+++ app-arducon/src/main/java/com/keelim/arducon/main/MainActivity.kt
@@ -2,10 +2,10 @@ package com.keelim.arducon.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import com.keelim.arducon.ui.ArduconApp
+import com.keelim.composeutil.util.setThemeContent
 import dagger.hilt.android.AndroidEntryPoint
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
@@ -13,7 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setThemeContent {
             ArduconApp(
                 windowSizeClass = calculateWindowSizeClass(this),
             )

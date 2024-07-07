900f82c75bf79a9f8dda9954837867016de0aeb4
Keel_im <kimh00335@gmail.com>
2024-07-08 00:21:53 +0900

    feat: Generate SVG dependency graph for app-arducon
    This commit adds the generation of an SVG dependency graph for the app-arducon module.
    
    The
     generated SVG file is placed in the app-arducon directory.

--- script/gradle_dependencies_graph.sh
+++ script/gradle_dependencies_graph.sh
@@ -1,8 +1,10 @@
+./gradlew generateModulesGraphvizText -Pmodules.graph.output.gv=app-arducon.gv -Pmodules.graph.of.module=:app-arducon
 ./gradlew generateModulesGraphvizText -Pmodules.graph.output.gv=app-cnubus.gv -Pmodules.graph.of.module=:app-cnubus
 ./gradlew generateModulesGraphvizText -Pmodules.graph.output.gv=app-comssa.gv -Pmodules.graph.of.module=:app-comssa
 ./gradlew generateModulesGraphvizText -Pmodules.graph.output.gv=app-my-grade.gv -Pmodules.graph.of.module=:app-my-grade
 ./gradlew generateModulesGraphvizText -Pmodules.graph.output.gv=app-nanda.gv -Pmodules.graph.of.module=:app-nanda
 
+dot -Tsvg app-arducon.gv > ./app-arducon/app-arducon.svg
 dot -Tsvg app-cnubus.gv > ./app-cnubus/app-cnubus.svg
 dot -Tsvg app-comssa.gv > ./app-comssa/app-comssa.svg
 dot -Tsvg app-my-grade.gv > ./app-my-grade/app-my-grade.svg

da7561e56a133a33539f0149b72602f0ef54f1a5
Keel_im <kimh00335@gmail.com>
2024-07-17 01:16:33 +0900

    feat: Add Compose Compiler Plugin
    This commit adds the Compose Compiler Plugin to the shared module's build configuration. This plugin is necessary for enabling certain Compose features
     and optimizations.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.android.library)
     alias(libs.plugins.sqldelight)
     kotlin("plugin.serialization")

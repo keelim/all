201df3c91f95c2f692cef9ff0d2b932036a9ccf7
Keel_im <kimh00335@gmail.com>
2024-07-21 16:24:00 +0900

    Refactor: Migrate to core navigation module
    This commit refactors the navigation logic to utilize the core navigation module.
    
    - Introduces the `CnuBus
    Route` sealed interface in the core navigation module.
    - Replaces hardcoded route strings with `CnuBusRoute` constants.
    - Updates navigation calls to use `CnuBusRoute` for type safety and consistency.

--- core/navigation/src/main/java/com/keelim/core/navigation/RouteModel.kt
+++ core/navigation/src/main/java/com/keelim/core/navigation/RouteModel.kt
@@ -5,5 +5,13 @@ import kotlinx.serialization.Serializable
 
 sealed interface ArduconRoute {
     @Serializable
-    data object Main: ArduconRoute
+    data object Main : ArduconRoute
+}
+
+sealed interface CnuBusRoute {
+    @Serializable
+    data object Main : CnuBusRoute
+
+    @Serializable
+    data object Map : CnuBusRoute
 }

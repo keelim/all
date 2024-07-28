e5eebf3eca5a5e528189bde04cde86ad9c02adcf
Keel_im <kimh00335@gmail.com>
2024-07-29 01:28:09 +0900

    feat: add search screen
    Adds a new search screen to the application.
    
    - Created a new SearchRoute composable function.
    - Created a new Search
    Screen composable function.
    - Added a search icon to the MainScreen which navigates to the SearchScreen.
    - Added navigation logic for the search screen.
    - Updated the ArduconHost to include the search screen in the navigation graph.

---
+++ app-arducon/src/main/java/com/keelim/arducon/ui/screen/search/SearchScreen.kt
@@ -0,0 +1,20 @@
+package com.keelim.arducon.ui.screen.search
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SearchRoute() {
+
+}
+
+@Composable
+fun SearchScreen() {
+
+}
+
+@Preview
+@Composable
+private fun PreviewSearchScreen() {
+    SearchScreen()
+}

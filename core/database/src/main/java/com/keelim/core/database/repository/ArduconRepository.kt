5bee7da0881a1f14acf111eb1de8a5304db23316
Keel_im <kimh00335@gmail.com>
2024-07-29 01:37:46 +0900

    feat: add search function to deeplink
    This commit adds a search function to the deeplink list.
    
    - Modified `ArduconRepository` to accept
     a keyword for filtering deeplinks.
    - Added `getDeepLinkUrlsFiltered` to `ArduconDataSource` to retrieve filtered deeplinks.
    - Implemented `getDeepLinkUrlsFiltered` in `ArduconDataSourceImpl` using the DAO.
    - Added `getDeepLinkUrlsFiltered` to `
    ArduconDao` with a SQL query for keyword search.
    - Updated `ArduconRepositoryImpl` to use the new filtering mechanism.

--- core/database/src/main/java/com/keelim/core/database/repository/ArduconRepository.kt
+++ core/database/src/main/java/com/keelim/core/database/repository/ArduconRepository.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface ArduconRepository {
     suspend fun insertDeepLinkUrl(deepLink: DeepLink)
-    fun getDeepLinkUrls(): Flow<List<DeepLink>>
+    fun getDeepLinkUrls(keyword: String): Flow<List<DeepLink>>
     suspend fun deleteDeepLinkUrl(deepLink: DeepLink)
     suspend fun updateDeepLinkUrl(deepLink: DeepLink)
 }

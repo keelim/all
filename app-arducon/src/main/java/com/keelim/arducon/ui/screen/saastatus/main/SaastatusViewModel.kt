bc64e258359e8ad82418b35fbc371c881165014f
Keel_im <kimh00335@gmail.com>
2024-09-01 16:29:03 +0900

    feat: add saastatus screen
    This commit adds a new screen for saastatus.
    
    - Added `SaastatusRoute` for navigation.
    
    - Added `SaastatusScreen` composable.
    - Added `SaastatusViewModel`.
    - Added `saastatusScreen` to the `ArduconHost`.
    - Added `navigateSaastatus` function for navigation.

---
+++ app-arducon/src/main/java/com/keelim/arducon/ui/screen/saastatus/main/SaastatusViewModel.kt
@@ -0,0 +1,12 @@
+package com.keelim.arducon.ui.screen.saastatus.main
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SaastatusViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}

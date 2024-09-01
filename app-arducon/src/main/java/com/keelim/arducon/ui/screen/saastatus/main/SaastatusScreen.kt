85a226123f5fdea8fc8cccf8e8a49303b5db539b
Keel_im <kimh00335@gmail.com>
2024-09-01 17:09:38 +0900

    Refactor: Replace width modifier with height modifier in SaastatusScreen
    This commit replaces the `width` modifier with `height` modifier for Spacers
     in the `SaastatusScreen` composable to ensure correct spacing between elements.

--- app-arducon/src/main/java/com/keelim/arducon/ui/screen/saastatus/main/SaastatusScreen.kt
+++ app-arducon/src/main/java/com/keelim/arducon/ui/screen/saastatus/main/SaastatusScreen.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -57,13 +57,13 @@ fun SaastatusScreen(
             )
         }
         Spacer(
-            modifier = Modifier.width(space12),
+            modifier = Modifier.height(space12),
         )
         HorizontalDivider(
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(
-            modifier = Modifier.width(space24),
+            modifier = Modifier.height(space24),
         )
         when (state) {
             SaastatusState.Loading -> Loading()

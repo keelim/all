43d5520d8af7cbc9207ba544879937ce64495d83
Keel_im <kimh00335@gmail.com>
2024-09-03 22:17:55 +0900

    feat: add faq screen
    This commit adds a simple FAQ screen with expandable rows.
    
    - Created a basic FAQ screen with a LazyColumn.
    - Implemented
     an ExpandableRow composable for displaying questions and answers.
    - Added animations for expanding and collapsing rows.
    - Included a placeholder FAQ item for now.

--- features/ui-setting/src/main/java/com/keelim/setting/screen/faq/FaqScreen.kt
+++ features/ui-setting/src/main/java/com/keelim/setting/screen/faq/FaqScreen.kt
@@ -1,20 +1,107 @@
 package com.keelim.setting.screen.faq
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun FaqRoute(
     viewModel: FaqViewModel = hiltViewModel(),
 ) {
-
+    FaqScreen()
 }
 
 @Composable
 fun FaqScreen() {
-
+    LazyColumn {
+        item {
+            ExpandableRow(
+                title = "현재 faq 준비 중입니다. ",
+                subtitle = "원할한 안내를 위한 준비 중입니다. "
+            )
+        }
+    }
 }
+
+@Composable
+private fun ExpandableRow(
+    title: String,
+    subtitle: String,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .clickable { expanded = expanded.not() }
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            val degrees by animateFloatAsState(if (expanded) -180f else 0f, label = "")
+            Icon(
+                imageVector = Icons.Filled.KeyboardArrowDown,
+                contentDescription = null,
+                modifier = Modifier.rotate(degrees)
+            )
+        }
+        AnimatedVisibility(
+            visible = expanded,
+            enter = expandVertically(
+                spring(
+                    stiffness = Spring.StiffnessMediumLow,
+                    visibilityThreshold = IntSize.VisibilityThreshold
+                )
+            ),
+            exit = shrinkVertically()
+        ) {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = subtitle,
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            }
+        }
+    }
+}
+
 
 @Preview
 @Composable

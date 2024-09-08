c04bcbfb051ca361ae97e9d7fce8a0e79ab10ee9
Keel_im <kimh00335@gmail.com>
2024-09-08 17:08:51 +0900

    ios app init

---
+++ allIos/all/allApp.swift
@@ -0,0 +1,17 @@
+//
+//  allApp.swift
+//  all
+//
+//  Created by 김재현 on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct allApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

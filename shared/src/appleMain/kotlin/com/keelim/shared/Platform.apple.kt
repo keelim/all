770c87dc10bd8853d8a7910d792408fd9c13f36a
Keel_im <kimh00335@gmail.com>
2024-09-08 16:38:48 +0900

    feat: add locale information to Platform
    This commit adds a `locale` property to the `Platform` interface and its implementations to provide information about the device'
    s current locale.
    
    - Adds `locale` string to `Platform` interface.
    - Implements `locale` in `DesktopPlatform`, `ApplePlatform`, and `AndroidPlatform` using platform-specific APIs.
    - Adds a default locale string "kr" to `strings.xml`.

--- shared/src/appleMain/kotlin/com/keelim/shared/Platform.apple.kt
+++ shared/src/appleMain/kotlin/com/keelim/shared/Platform.apple.kt
@@ -1,9 +1,16 @@
 package com.keelim.shared
 
+import platform.Foundation.NSLocale
+import platform.Foundation.preferredLanguages
 import platform.UIKit.UIDevice
 
 class ApplePlatform : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    override val locale: String
+        get() = NSLocale
+            .preferredLanguages()
+            .first()
+            .toString()
 }
 
 actual fun getPlatform(): Platform = ApplePlatform()
--- shared/src/desktopMain/kotlin/com/keelim/shared/Platform.desktop.kt
+++ shared/src/desktopMain/kotlin/com/keelim/shared/Platform.desktop.kt
@@ -1,7 +1,11 @@
 package com.keelim.shared
 
+import java.util.Locale
+
 class DesktopPlatform : Platform {
     override val name: String = "Desktop"
+    override val locale: String
+        get() = Locale.getDefault().language
 }
 
 actual fun getPlatform(): Platform = DesktopPlatform()

3af935a21d559cb51d3277970c32996ba4366ca5
Keel_im <kimh00335@gmail.com>
2024-09-29 22:46:16 +0900

    feat: implement onMessageReceived in AllFCMService
    This commit implements the `onMessageReceived` method in `AllFCMService` to handle incoming FCM
     messages.
    
    - Logs the message sender and data payload.
    - Logs the notification body if present.
    - Adds a placeholder for generating custom notifications based on the received message.

--- core/common-android/src/main/java/com/keelim/commonAndroid/message/AllFCMService.kt
+++ core/common-android/src/main/java/com/keelim/commonAndroid/message/AllFCMService.kt
@@ -1,11 +1,30 @@
 package com.keelim.commonAndroid.message
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import timber.log.Timber
 
 class AllFCMService: FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Timber.d("newToken: $token")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Timber.d( "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Timber.d("Message data payload: ${remoteMessage.data}")
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Timber.d("Message Notification Body: ${it.body}")
+        }
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
     }
 }

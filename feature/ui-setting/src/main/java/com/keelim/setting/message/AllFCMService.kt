9aeaf7c40ad278c226ad30dca25ab1771af93964
Keel_im <kimh00335@gmail.com>
2024-09-30 00:33:15 +0900

    feat: move fcm service to ui-setting module
    This commit moves the AllFCMService from the common-android module to the ui-setting module.
    
    - Moves AllFCMService to ui-setting module.
    - Adds dependency on firebase-messaging to ui-setting module.
    - Removes dependency on firebase-messaging from common-android module.
    - Updates AndroidManifest.xml to reflect the new location of AllFCMService.
    - Updates AlarmRepository to take
     title and subtitle as parameters for inserting alarms.
    - Updates AlarmRepositoryImpl to use the new AlarmRepository method.
    - Updates AllFCMService to insert alarms using the new AlarmRepository method.
    - Adds receiveDate to the Alarm data class.

--- feature/ui-setting/src/main/java/com/keelim/setting/message/AllFCMService.kt
+++ feature/ui-setting/src/main/java/com/keelim/setting/message/AllFCMService.kt
@@ -1,10 +1,28 @@
-package com.keelim.commonAndroid.message
+package com.keelim.setting.message
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.keelim.common.di.ApplicationScope
+import com.keelim.data.repository.AlarmRepository
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AllFCMService: FirebaseMessagingService() {
+
+    @Inject
+    lateinit var alarmRepository: AlarmRepository
+
+    @Inject
+    @ApplicationScope
+    lateinit var applicationScope: CoroutineScope
+
+    private var alarmActionJob: Job? = null
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Timber.d("newToken: $token")
@@ -26,5 +44,21 @@ class AllFCMService: FirebaseMessagingService() {
         }
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+        alarmActionJob = applicationScope.launch {
+            alarmRepository.insertAlarm(
+                title = remoteMessage.notification?.title ?: "title",
+                subTitle = remoteMessage.notification?.body ?: "body"
+            )
+        }.apply {
+            invokeOnCompletion {
+                alarmActionJob = null
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        alarmActionJob?.cancel()
+        alarmActionJob = null
     }
 }

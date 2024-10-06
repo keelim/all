a46e6b4200834112712bc62b1e05a6e39a85e4cb
Keel_im <kimh00335@gmail.com>
2024-10-06 18:32:36 +0900

    Refactor: Migrate to setContent and update theme handling
    This commit refactors the application to use `setContent` for composing UI and updates theme handling using a shared
     `DataStore`.
    
    - Removes `SetThemeContent.kt` which previously applied the theme wrapper.
    - Updates all UI modules to use `setContent` and apply the theme from `DataStore` using `collectAsStateWithLifecycle`.
    - Updates the default theme type to `LIGHT` in `AppDataStore`.
    
    - Renames `Main2Activity` to `MainActivity` in the Nanda module.
    - Renames `CenterActivity` to `MainActivity` in the MyGrade module.
    - Adds the shared module as a dependency to the Cnubus and Arducon modules.
    - Updates `ThemeViewModel` and `ThemeScreen` to use the new `ThemeType` enum location.
    - Updates the notification launcher intent in the Nanda module to use the renamed `MainActivity`.
    - Adds a new file `ThemeType.kt` in the shared module to define the theme types.
    - Updates the application ID in the MyGrade module
    's shortcut XML.
    - Updates the default theme type to `LIGHT` in `AppDataStore` and update the `themeTypeFlow`.

--- feature/ui-setting/src/main/java/com/keelim/setting/screen/theme/ThemeScreen.kt
+++ feature/ui-setting/src/main/java/com/keelim/setting/screen/theme/ThemeScreen.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.util.fastForEach
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.keelim.composeutil.resource.space8
-import com.keelim.shared.data.ThemeType
+import com.keelim.shared.data.model.ThemeType
 
 @Composable
 fun ThemeRoute(

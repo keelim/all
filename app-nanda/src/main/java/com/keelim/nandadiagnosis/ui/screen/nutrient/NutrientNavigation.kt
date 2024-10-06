8ad175af46505dfaef183159221ac20a47f37f34
Keel_im <kimh00335@gmail.com>
2024-10-06 22:21:06 +0900

    refactor: rename nutrients to nutrient
    This commit renames the `nutrients` package to `nutrient` for better clarity and consistency.
    
    This
     change involves renaming files and updating imports in the following locations:
    
    - `src/main/java/com/keelim/nandadiagnosis/ui/screen/nutrients` to `src/main/java/com/keelim/nandadiagnosis/ui/screen/nutrient`
    - Related
     files within these directories were also updated to reflect the package name change.
    - Imports in other files using the `nutrients` package were updated to `nutrient`.

--- app-nanda/src/main/java/com/keelim/nandadiagnosis/ui/screen/nutrient/NutrientNavigation.kt
+++ app-nanda/src/main/java/com/keelim/nandadiagnosis/ui/screen/nutrient/NutrientNavigation.kt
@@ -1,4 +1,4 @@
-package com.keelim.nandadiagnosis.ui.screen.nutrients
+package com.keelim.nandadiagnosis.ui.screen.nutrient
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder

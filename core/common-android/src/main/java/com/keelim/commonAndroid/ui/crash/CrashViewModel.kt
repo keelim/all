79a419663910958f3960ce407fd5ab7a1593311d
Keel_im <kimh00335@gmail.com>
2024-10-13 01:14:24 +0900

    feat: add CrashReportActivity and CrashRoute
    This commit introduces a new `CrashReportActivity` and `CrashRoute` for handling crashes.
    
    - Adds
     `CrashReportActivity` to display error information and provide an option to restart the app.
    - Adds `CrashRoute` to display error information with restart button.
    - Adds a new dependency on `composeCore` in `common-android` build configuration.
    - Updates `AndroidManifest.xml` to include
     the new activity.

---
+++ core/common-android/src/main/java/com/keelim/commonAndroid/ui/crash/CrashViewModel.kt
@@ -0,0 +1,12 @@
+package com.keelim.commonAndroid.ui.crash
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CrashViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}

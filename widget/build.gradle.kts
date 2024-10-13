785d898dcca03525e56ec5dc185ad3ce31c613d8
Keel_im <kimh00335@gmail.com>
2024-10-13 17:33:13 +0900

    chore: add Glance library as api dependency and separate appwidget
    This commit updates the widget module to depend on Glance as an API dependency and introduces a separate
     dependency for the appwidget library.
    
    - Adds `androidx.glance:glance` and `androidx.glance:glance-appwidget` as API dependencies in `widget/build.gradle.kts`.
    - Removes `androidx.compose.glance`, `androidx.compose.glance.
    material3` and `glance.tools.appwidget.host` from `widget/build.gradle.kts` dependencies.
    - Adds separate entries for `androidx-compose-glance-widget` and `androidx-compose-glance` in `libs.versions.toml`.

--- widget/build.gradle.kts
+++ widget/build.gradle.kts
@@ -8,7 +8,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.compose.glance)
-    implementation(libs.androidx.compose.glance.material3)
-    implementation(libs.glance.tools.appwidget.host)
+    api(libs.androidx.compose.glance.widget)
+    api(libs.androidx.compose.glance)
 }

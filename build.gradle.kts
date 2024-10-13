26a009bc94fc2a6adfea106a606ded12f0d0864f
Keel_im <kimh00335@gmail.com>
2024-10-13 17:09:11 +0900

    chore: apply dependency analysis plugin
    This commit applies the dependency analysis plugin to all Android application modules.
    
    - Adds the `dependency-analysis` plugin to
     the `build-logic/convention/src/main/kotlin/KeelimAndroidApplicationPlugin.kt` file.
    - Adds the `dependency-analysis` plugin dependency to the `gradle/libs.versions.toml` file.
    - Reorders plugin dependencies in the root `build.gradle.kts`
     file to be more consistent.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,21 +14,22 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.baselineprofile) apply false
     alias(libs.plugins.cacheFixPlugin) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
+    alias(libs.plugins.dependency.analysis) apply false
+    alias(libs.plugins.dependencyGuard) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.firebase.perf) apply false
+    alias(libs.plugins.gms.googleServices) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.secrets) apply false
-    alias(libs.plugins.gms.googleServices) apply false
-    alias(libs.plugins.firebase.crashlytics) apply false
-    alias(libs.plugins.firebase.perf) apply false
-    alias(libs.plugins.room) apply false
-    alias(libs.plugins.dependencyGuard) apply false
     alias(libs.plugins.moduleGraphAssertion) apply false
-    alias(libs.plugins.baselineprofile) apply false
+    alias(libs.plugins.room) apply false
+    alias(libs.plugins.secrets) apply false
 }

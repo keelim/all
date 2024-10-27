c2ebc2df9e7e4213200203b9f010421eb1579ff0
Keel_im <kimh00335@gmail.com>
2024-10-27 23:00:26 +0900

    feat: add food screen
    This commit introduces a new screen for food information.
    
    - Adds a new `FoodRoute` and `FoodScreen` composables.
    - Adds navigation to the new screen from the home screen using `CategoriesType.NUTRIENT`.
    - Adds `FoodViewModel` to handle the screen's logic.
    - Updates `app-nanda/build.gradle.kts` to use the debug signing config for release builds.

---
+++ app-nanda/src/main/java/com/keelim/nandadiagnosis/ui/screen/food/FoodScreen.kt
@@ -0,0 +1,24 @@
+package com.keelim.nandadiagnosis.ui.screen.food
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun FoodRoute(
+    viewModel: FoodViewModel = hiltViewModel(),
+) {
+
+}
+
+@Composable
+fun FoodScreen() {
+
+}
+
+@Preview
+@Composable
+private fun PreviewFoodScreen() {
+    FoodScreen()
+}
---
+++ app-nanda/src/main/java/com/keelim/nandadiagnosis/ui/screen/food/FoodViewModel.kt
@@ -0,0 +1,12 @@
+package com.keelim.nandadiagnosis.ui.screen.food
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FoodViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}

8a77d292fac555abb55d18972c5b51d6c4d5bf2f
Keel_im <kimh00335@gmail.com>
2025-01-29 02:55:07 +0900

    refactor: downgrade JVM target to version 1.8 and simplify Word object

--- core/string/build.gradle.kts
+++ core/string/build.gradle.kts
@@ -2,17 +2,16 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.compose.multiplatform)
-    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.android.library)
 }
 
 kotlin {
     androidTarget {
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_17
+            jvmTarget = JvmTarget.JVM_1_8
         }
     }
+    
     jvm("desktop")
 
     listOf(
@@ -23,12 +22,6 @@ kotlin {
         iosTarget.binaries.framework {
             baseName = "ALL"
             isStatic = true
-        }
-    }
-
-    sourceSets {
-        commonMain.dependencies {
-            implementation(compose.components.resources)
         }
     }
 }

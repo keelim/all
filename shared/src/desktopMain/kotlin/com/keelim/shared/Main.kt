6147dafe387a112f1b8b6d837f60c3be32626faf
Keel_im <kimh00335@gmail.com>
2025-01-29 02:55:44 +0900

    fix: update window title to use Word.project directly

--- shared/src/desktopMain/kotlin/com/keelim/shared/Main.kt
+++ shared/src/desktopMain/kotlin/com/keelim/shared/Main.kt
@@ -8,7 +8,7 @@ fun main() {
     application {
         Window(
             onCloseRequest = ::exitApplication,
-            title = Word.project.key,
+            title = Word.project,
         ) {
         }
     }

1d579db7c90d455f2d829137a20dd197532891c8
Keel_im <kimh00335@gmail.com>
2025-01-29 02:55:07 +0900

    refactor: downgrade JVM target to version 1.8 and simplify Word object

--- core/string/src/commonMain/kotlin/com/keelim/core/string/Word.kt
+++ core/string/src/commonMain/kotlin/com/keelim/core/string/Word.kt
@@ -1,12 +1,5 @@
 package com.keelim.core.string
 
-import all.core.string.generated.resources.Res
-import all.core.string.generated.resources.locale_default
-import all.core.string.generated.resources.project
-
 object Word {
-    val project
-        get() = Res.string.project
-    val locale_default
-        get() = Res.string.locale_default
+    val project = "All"
 }

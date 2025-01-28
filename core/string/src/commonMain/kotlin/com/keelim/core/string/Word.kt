d1207ff27b6dc68d05cab62599053f800990be15
Keel_im <kimh00335@gmail.com>
2025-01-29 02:16:10 +0900

    refactor: create core string module and update dependencies for improved resource management

---
+++ core/string/src/commonMain/kotlin/com/keelim/core/string/Word.kt
@@ -0,0 +1,12 @@
+package com.keelim.core.string
+
+import all.core.string.generated.resources.Res
+import all.core.string.generated.resources.locale_default
+import all.core.string.generated.resources.project
+
+object Word {
+    val project
+        get() = Res.string.project
+    val locale_default
+        get() = Res.string.locale_default
+}

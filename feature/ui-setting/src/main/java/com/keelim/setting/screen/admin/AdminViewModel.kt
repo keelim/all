2a25a3b42b238b09e50a839dcdd46c625302ab13
Keel_im <kimh00335@gmail.com>
2025-02-16 20:34:14 +0900

    feat: add Admin screen and ViewModel with basic structure

---
+++ feature/ui-setting/src/main/java/com/keelim/setting/screen/admin/AdminViewModel.kt
@@ -0,0 +1,10 @@
+package com.keelim.setting.screen.admin
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AdminViewModel @Inject constructor(
+) : ViewModel() {
+}

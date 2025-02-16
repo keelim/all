9ff063d3e6fe23e6e95d50354fe3079d70cde67c
Keel_im <kimh00335@gmail.com>
2025-02-17 00:42:19 +0900

    feat: restructure AdminScreen layout with Column and add NavigationBackArrowBar

--- feature/ui-setting/src/main/java/com/keelim/setting/screen/admin/AdminScreen.kt
+++ feature/ui-setting/src/main/java/com/keelim/setting/screen/admin/AdminScreen.kt
@@ -2,8 +2,12 @@ package com.keelim.setting.screen.admin
 
 import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,6 +16,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.util.trace
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.core.net.toUri
+import com.keelim.composeutil.component.appbar.NavigationBackArrowBar
+import com.keelim.composeutil.resource.space8
 
 @Composable
 fun AdminRoute(
@@ -23,23 +29,30 @@ fun AdminRoute(
 @Composable
 fun AdminScreen(
 ) = trace("AdminScreen") {
-    val context = LocalContext.current
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = space8),
     ) {
-        item {
-            SchemeTestSection(
-                onClick = { uri ->
-                    context.startActivity(
-                        Intent(
-                            Intent.ACTION_VIEW,
-                            uri.toUri()
+        NavigationBackArrowBar(title = "알림 확인")
+        Spacer(
+            modifier = Modifier.height(space8),
+        )
+        val context = LocalContext.current
+        LazyColumn {
+            item {
+                SchemeTestSection(
+                    onClick = { uri ->
+                        context.startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW,
+                                uri.toUri()
+                            )
                         )
-                    )
-                },
-                modifier = Modifier.fillMaxWidth()
-            )
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
 }

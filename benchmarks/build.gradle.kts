3edc0f59dd7641cbdd19ceec49cc19160ea836bc
Keel_im <kimh00335@gmail.com>
2025-02-24 01:15:48 +0900

    feat: add build configuration and include benchmarks module

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -1,7 +1,33 @@
+import kotlinx.benchmark.gradle.JvmBenchmarkTarget
+
 plugins {
     alias(libs.plugins.keelim.jvm.library)
+    // please migrate version catalog
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.13"
+    kotlin("plugin.allopen") version "2.1.10"
 }
 
 dependencies {
-    //
+    implementation(libs.kotlin.becnhmark.runtime)
+}
+
+allOpen {
+    annotation("org.openjdk.jmh.annotations.State")
+}
+
+benchmark {
+    configurations {
+        named("main") {
+            warmups = 20
+            iterations = 10
+            iterationTime = 3
+            iterationTimeUnit = "s"
+        }
+    }
+    targets {
+        register("main") {
+            this as JvmBenchmarkTarget
+            jmhVersion = "1.21"
+        }
+    }
 }

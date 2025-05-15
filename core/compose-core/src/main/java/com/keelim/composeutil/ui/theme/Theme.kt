5b41abe4928657c7f6255a2b2d2190dcb55a8be1
Keel_im <kimh00335@gmail.com>
2025-05-16 00:33:43 +0900

    Use MaterialExpressiveTheme in Theme.kt
    Replaced MaterialTheme with MaterialExpressiveTheme to leverage experimental Material 3 expressive APIs. Added the necessary @OptIn annotation for ExperimentalMaterial3ExpressiveApi.

--- core/compose-core/src/main/java/com/keelim/composeutil/ui/theme/Theme.kt
+++ core/compose-core/src/main/java/com/keelim/composeutil/ui/theme/Theme.kt
@@ -1,9 +1,13 @@
+@file:OptIn(ExperimentalMaterial3ExpressiveApi::class)
+
 package com.keelim.composeutil.ui.theme
 
 import android.app.Activity
 import android.os.Build
 import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -67,7 +71,7 @@ fun KeelimTheme(
         }
     }
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         typography = keelimTypography,
         colorScheme = colorScheme,
         content = content,

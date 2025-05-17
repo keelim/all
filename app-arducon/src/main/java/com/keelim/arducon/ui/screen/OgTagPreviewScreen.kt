42f48d83ff74c569dfeabc26b8a8d8cd54af1f08
Keel_im <kimh00335@gmail.com>
2025-05-17 22:20:56 +0900

    Add AdBannerView to OgTagPreviewScreen
    Introduced an AdBannerView component at the bottom of the OgTagPreviewScreen with a full-width modifier and added spacing to separate it from other content.

--- app-arducon/src/main/java/com/keelim/arducon/ui/screen/OgTagPreviewScreen.kt
+++ app-arducon/src/main/java/com/keelim/arducon/ui/screen/OgTagPreviewScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.keelim.arducon.ui.component.AdBannerView
 
 data class OgTagData(
     val title: String = "",
@@ -97,6 +98,12 @@ fun OgTagPreviewScreen(
                 }
             }
         }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        AdBannerView(
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 }
 

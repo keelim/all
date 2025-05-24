6d5e954fb9e525ce075733f83f4d8bc2b18f89ab
Keel_im <kimh00335@gmail.com>
2025-05-25 01:48:34 +0900

    Refactor settings navigation and update dependencies
    Replaced `NavGraphBuilder` with `EntryProviderBuilder` in `SettingsNavigation.kt` to improve navigation structure. Updated `build.gradle.kts` to include new dependencies for androidx.navigation3 runtime and UI.

--- feature/ui-setting/src/main/java/com/keelim/setting/screen/settings/SettingsNavigation.kt
+++ feature/ui-setting/src/main/java/com/keelim/setting/screen/settings/SettingsNavigation.kt
@@ -1,50 +1,77 @@
 package com.keelim.setting.screen.settings
 
+import android.content.Context
+import android.content.Intent
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.core.net.toUri
 import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
-import androidx.navigation.compose.composable
+import androidx.navigation3.runtime.EntryProviderBuilder
+import androidx.navigation3.runtime.entry
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.keelim.core.navigation.FeatureRoute
-import com.keelim.setting.screen.admin.adminScreen
-import com.keelim.setting.screen.alarm.alarmScreen
-import com.keelim.setting.screen.faq.faqScreen
-import com.keelim.setting.screen.lab.labScreen
-import com.keelim.setting.screen.notification.notificationScreen
-import com.keelim.setting.screen.theme.themeScreen
+import com.keelim.setting.screen.admin.AdminRoute
+import com.keelim.setting.screen.alarm.AlarmRoute
+import com.keelim.setting.screen.faq.FaqRoute
+import com.keelim.setting.screen.lab.LabRoute
+import com.keelim.setting.screen.notification.NotificationRoute
+import com.keelim.setting.screen.theme.ThemeRoute
 
 fun NavController.navigateSettings(navOptions: NavOptions? = null) {
     this.navigate(FeatureRoute.Settings, navOptions)
 }
 
-fun NavGraphBuilder.settingsScreen(
-    onThemeChangeClick: () -> Unit,
-    onNotificationsClick: () -> Unit,
-    onAlarmsClick: () -> Unit,
-    onFaqClick: () -> Unit,
-    onOpenSourceClick: () -> Unit,
-    onLabClick: () -> Unit,
-    onAppUpdateClick: () -> Unit,
-    onAdminClick: () -> Unit,
-    nestedGraphs: NavGraphBuilder.() -> Unit = {
-        faqScreen(nestedGraphs = {})
-        themeScreen()
-        notificationScreen()
-        labScreen()
-        alarmScreen()
-        adminScreen(nestedGraphs = {})
-    },
+fun EntryProviderBuilder<Any>.settingsEntry(
+    context: Context,
+    backStack: SnapshotStateList<Any>,
 ) {
-    composable<FeatureRoute.Settings> {
+    entry<FeatureRoute.Settings>() {
         SettingsRoute(
-            onThemeChangeClick = onThemeChangeClick,
-            onNotificationsClick = onNotificationsClick,
-            onAlarmsClick = onAlarmsClick,
-            onFaqClick = onFaqClick,
-            onOpenSourceClick = onOpenSourceClick,
-            onLabClick = onLabClick,
-            onAppUpdateClick = onAppUpdateClick,
-            onAdminClick = onAdminClick,
+            onThemeChangeClick = { backStack.add(FeatureRoute.Theme) },
+            onNotificationsClick = {
+                backStack.add(FeatureRoute.Notification)
+            },
+            onAlarmsClick = {
+                backStack.add(FeatureRoute.Alarm)
+            },
+            onFaqClick = {
+                backStack.add(FeatureRoute.Faq)
+            },
+            onOpenSourceClick = {
+                context.startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+            },
+            onLabClick = {
+                backStack.add(FeatureRoute.Lab)
+            },
+            onAppUpdateClick = {
+                context.startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        "https://play.google.com/store/apps/details?id=${context.packageName}".toUri(),
+                    ),
+                )
+            },
+            onAdminClick = {
+                backStack.add(FeatureRoute.Admin)
+            }
         )
     }
-    nestedGraphs()
+    entry<FeatureRoute.Faq> {
+        FaqRoute()
+    }
+    entry<FeatureRoute.Theme> {
+        ThemeRoute()
+    }
+    entry<FeatureRoute.Notification> {
+        NotificationRoute()
+    }
+    entry<FeatureRoute.Lab> {
+        LabRoute()
+    }
+    entry<FeatureRoute.Alarm> {
+        AlarmRoute()
+    }
+    entry<FeatureRoute.Admin> {
+        AdminRoute()
+    }
 }

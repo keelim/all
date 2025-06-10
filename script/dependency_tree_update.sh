e7b37bcb0b4eecc7fa520796747f260b20c6836e
Keel_im <kimh00335@gmail.com>
2025-06-10 22:46:15 +0900

    Initial commit
    Project scaffolding and initial setup files added.

--- script/dependency_tree_update.sh
+++ script/dependency_tree_update.sh
@@ -1,6 +1,8 @@
 modules=("app-arducon" "app-cnubus" "app-comssa" "app-my-grade" "app-nanda")
+tasks=""
 
 # Loop through each module
 for module in "${modules[@]}"; do
-    ./gradlew "$module":dependencyGuardBaseline
+    tasks+=":$module:dependencyGuardBaseline "
 done
+./gradlew $tasks

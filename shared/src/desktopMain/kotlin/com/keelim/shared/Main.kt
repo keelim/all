c3660622ccfa40a54cb0b95a7913f9ff8ce9f754
Keel_im <kimh00335@gmail.com>
2025-08-04 10:02:25 +0900

    Integrate Compose resources and update project title usage
    Added Compose Multiplatform and resources plugins to build scripts and configured resource generation. Removed the Word object and updated Main.kt to use stringResource for the project title, improving resource management and localization support.

--- shared/src/desktopMain/kotlin/com/keelim/shared/Main.kt
+++ shared/src/desktopMain/kotlin/com/keelim/shared/Main.kt
@@ -2,13 +2,15 @@ package com.keelim.shared
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.keelim.core.string.Word
+import com.keelim.core.resource.Res
+import com.keelim.core.resource.project
+import org.jetbrains.compose.resources.stringResource
 
 fun main() {
     application {
         Window(
             onCloseRequest = ::exitApplication,
-            title = Word.project,
+            title = stringResource(Res.string.project),
         ) {
         }
     }

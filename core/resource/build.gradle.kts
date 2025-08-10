d16d3288af44e47f79816d5aa190fcfa4baffe85
Keel_im <kimh00335@gmail.com>
2025-08-10 23:53:32 +0900

    Revert "Integrate Compose resources and update project title usage"
    This reverts commit 84622282

--- core/resource/build.gradle.kts
+++ core/resource/build.gradle.kts
@@ -1,9 +1,8 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.android.library)
 }
 
 kotlin {
@@ -25,21 +24,6 @@ kotlin {
             isStatic = true
         }
     }
-    sourceSets {
-        commonMain.dependencies {
-            implementation(compose.components.resources)
-        }
-    }
-}
-
-compose.resources {
-    publicResClass = true
-    packageOfResClass = "com.keelim.core.resource"
-    generateResClass = auto
-    customDirectory(
-        sourceSetName = "desktopMain",
-        directoryProvider = provider { layout.projectDirectory.dir("desktopResources") }
-    )
 }
 
 android {

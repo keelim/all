2e6c5dd628451873a263d77ebfbca20acd49c69e
Keel_im <kimh00335@gmail.com>
2025-10-08 00:43:43 +0900

    Move Android source and dependencies to core.component
    Android-specific source files and dependencies were relocated from core.compose-core to core.component. This refactor consolidates platform-specific code and dependencies, improving project structure and maintainability.

--- core/compose-core/build.gradle.kts
+++ core/compose-core/build.gradle.kts
@@ -13,21 +13,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.accompanist.permissions)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.emoji2.emojipicker)
-    implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.androidx.lifecycle.runtime.compose)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.inapp.update)
-    implementation(libs.material.themAdapter)
-    implementation(platform(libs.coil.bom))
-    implementation(libs.bundles.coil)
-    implementation(libs.androidx.media.compose)
-    implementation(libs.androidx.media.exoplayer)
-
-    debugImplementation(libs.androidx.compose.ui.testManifest)
-    androidTestImplementation(libs.androidx.compose.ui.test)
 
     api(projects.core.component)
 }

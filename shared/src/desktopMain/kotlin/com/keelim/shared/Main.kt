e4ed134b8b6f987d0434e3d4e98890cc8b27eb48
Keel_im <kimh00335@gmail.com>
2025-10-09 22:10:59 +0900

    Comment out DesktopWindow usage and implementation
    The DesktopWindow composable and its usage in Main.kt have been commented out due to issues with compose for desktop. This is a temporary change, as indicated by the FIXME comment, to prevent build or runtime errors until a proper solution is implemented.

--- shared/src/desktopMain/kotlin/com/keelim/shared/Main.kt
+++ shared/src/desktopMain/kotlin/com/keelim/shared/Main.kt
@@ -1,13 +1,12 @@
 package com.keelim.shared
 
 import androidx.compose.ui.window.application
-import com.keelim.shared.ui.DesktopWindow
 
 fun main() {
     application {
-        DesktopWindow(
-            onCloseRequest = ::exitApplication,
-        ) {
-        }
+        // DesktopWindow(
+        //     onCloseRequest = ::exitApplication,
+        // ) {
+        // }
     }
 }

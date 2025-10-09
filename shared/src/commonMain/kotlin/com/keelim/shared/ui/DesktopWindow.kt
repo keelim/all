18a0b92ae2181555c022d4a1cabc1da3591f5810
Keel_im <kimh00335@gmail.com>
2025-10-09 22:10:59 +0900

    Comment out DesktopWindow usage and implementation
    The DesktopWindow composable and its usage in Main.kt have been commented out due to issues with compose for desktop. This is a temporary change, as indicated by the FIXME comment, to prevent build or runtime errors until a proper solution is implemented.

--- shared/src/commonMain/kotlin/com/keelim/shared/ui/DesktopWindow.kt
+++ shared/src/commonMain/kotlin/com/keelim/shared/ui/DesktopWindow.kt
@@ -1,20 +1,14 @@
 package com.keelim.shared.ui
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.window.FrameWindowScope
-import androidx.compose.ui.window.Window
-import com.keelim.core.resource.Res
-import com.keelim.core.resource.project
-import org.jetbrains.compose.resources.stringResource
-
-@Composable
-fun DesktopWindow(
-    onCloseRequest: () -> Unit,
-    content: @Composable FrameWindowScope.() -> Unit,
-) {
-    Window(
-        onCloseRequest = onCloseRequest,
-        title = stringResource(Res.string.project),
-        content = content,
-    )
-}
+// FIXME: Window is compose for desktop 
+// @Composable
+// fun DesktopWindow(
+//     onCloseRequest: () -> Unit,
+//     content: @Composable FrameWindowScope.() -> Unit,
+// ) {
+//     Window(
+//         onCloseRequest = onCloseRequest,
+//         title = stringResource(Res.string.project),
+//         content = content,
+//     )
+// }
